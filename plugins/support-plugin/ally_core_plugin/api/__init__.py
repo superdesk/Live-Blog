fca9c82669975f298944ceea198344f7bb0af5f3
Gabriel Nistor <gabriel.nistor@sourcefabric.org>
2012-01-24 12:33:41 +0200

    NR-29 : URI requests introspection Implemented the basic services in the request introspection plugin.

---
+++ plugins/support-plugin/ally_core_plugin/api/__init__.py
@@ -0,0 +1,10 @@
+'''
+Created on May 26, 2011
+
+@package ally core plugins
+@copyright 2011 Sourcefabric o.p.s.
+@license http://www.gnu.org/licenses/gpl-3.0.txt
+@author: Gabriel Nistor
+
+Specifications for plugin APIs.
+'''
