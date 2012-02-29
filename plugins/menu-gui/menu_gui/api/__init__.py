e0786955ff7458e18249f3279d3dd9bbf10d9fb6
Mihai Balaceanu <gizm0bill@gmail.com>
2012-02-29 18:01:00 +0200

    NR-32 : Implement support for UI resources in the plugin API
    Actions api plugin and implementation

--- plugins/menu-gui/menu_gui/api/__init__.py
+++
@@ -1,7 +0,0 @@
-
-from functools import partial
-from ally.api.config import model
-
-# --------------------------------------------------------------------
-
-modelGui = partial(model, domain='GUI')
