0ba270907d3cf0d39577df75564c321f57cdd11b
Mihai Balaceanu <gizm0bill@gmail.com>
2012-03-07 13:07:33 +0200

    NR-40 : GUI core plugin
    Untility methods for published files

--- plugins/introspection-request/__plugin__/introspection_request/gui.py
+++ plugins/introspection-request/__plugin__/introspection_request/gui.py
@@ -9,31 +9,13 @@ Created on Feb 2, 2012
 Contains the GUI configuration setup for the node presenter plugin.
 '''
 
-from ..gui_action.defaults import actionManagerService, menuAction
-from ..gui_core.gui_core import publishGui, gui_folder_format
-from ..plugin.registry import cdmGUI
 from ally.container import ioc
-from ally.internationalization import N_, textdomain
-from gui.action.api.action import Action
-
-# --------------------------------------------------------------------
-
-textdomain('action')
+from ..gui_core.gui_core import publishGui
 
 # --------------------------------------------------------------------
 
 @ioc.start
 def publishJS():
-    publishGui('app')
-    
-@ioc.entity    
-def requestAction():
-    a = Action('request', N_('Request'), Parent=menuAction())
-    a.ScriptPath = cdmGUI().getURI(gui_folder_format() % 'app/scripts/js/request.js')
-    return a
-    
-@ioc.start
-def actionRegister():
-    actionManagerService().add(requestAction())
-    actionManagerService().add(Action('demo', N_('Demo'), Parent=menuAction()))
+    publishGui('superdesk/request')
+
     
