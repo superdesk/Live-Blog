8ce65793ef53daf896080618cad45a9ba6d705fb
Gabriel Nistor <gabriel.nistor@sourcefabric.org>
2012-03-07 22:45:35 +0200

    NR-14 : Internationalization
    Small fixes.

---
+++ plugins/internationalization/internationalization/meta/file.py
@@ -0,0 +1,34 @@
+'''
+Created on Mar 5, 2012
+
+@package: internationalization
+@copyright: 2011 Sourcefabric o.p.s.
+@license: http://www.gnu.org/licenses/gpl-3.0.txt
+@author: Gabriel Nistor
+
+Contains the SQL alchemy meta for source API.
+'''
+
+from . import meta
+from ..api.file import File, QFile
+from ally.support.sqlalchemy.mapper import mapperModel, mapperQuery
+from sqlalchemy.schema import Table, Column, UniqueConstraint
+from sqlalchemy.types import String, DateTime
+from sqlalchemy.dialects.mysql.base import INTEGER
+
+# --------------------------------------------------------------------
+
+component = Column('component', String(255), nullable=True, key='Component')
+plugin = Column('plugin', String(255), nullable=True, key='Plugin')
+path = Column('path', String(255), nullable=False, key='Path')
+              
+table = Table('inter_file', meta,
+              Column('id', INTEGER(unsigned=True), primary_key=True, key='Id'),
+              component, plugin, path,
+              Column('last_modified', DateTime, nullable=False, key='LastModified'),
+              UniqueConstraint(component, plugin, path, name='component_plugin_path_UNIQUE'),
+              mysql_engine='InnoDB'
+              )
+
+File = mapperModel(File, table)
+QFile = mapperQuery(QFile, File)
