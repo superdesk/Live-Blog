18be56584fe3900a98bfb30d1725cc40c6df6fde
Gabriel Nistor <gabriel.nistor@sourcefabric.org>
2012-03-28 19:29:36 +0300

    NR-76 : Enhanced REST operations (multiple operations per request)
    Small fix for the meta creator and resource manager.

--- superdesk/plugins/superdesk-user/superdesk/user/api/user.py
+++ superdesk/plugins/superdesk-user/superdesk/user/api/user.py
@@ -3,6 +3,7 @@ Created on Mar 6, 2012
 
 @author: Mihai Balaceanu
 '''
+
 from superdesk.api import modelSuperDesk
 from ally.api.config import service, query
 from sql_alchemy.api.entity import Entity, IEntityService, QEntity
@@ -16,13 +17,17 @@ class User(Entity):
     Provides the user model.
     '''
     Name = str
-    
+
+# --------------------------------------------------------------------
+
 @query
 class QUser(QEntity):
     '''
     Query for user service
     '''
     name = AsLike
+
+# --------------------------------------------------------------------
 
 @service((Entity, User), (QEntity, QUser))
 class IUserService(IEntityService):
