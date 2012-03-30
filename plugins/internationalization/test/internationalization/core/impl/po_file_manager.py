b1c2e63b94854203bf3f8be6645a59cf61d02672
Mugur Rus <mugur.rus@sourcefabric.org>
2012-03-30 16:24:18 +0300

    NR-21 : Translation management plugin implementation

--- plugins/internationalization/test/internationalization/core/impl/po_file_manager.py
+++ plugins/internationalization/test/internationalization/core/impl/po_file_manager.py
@@ -20,6 +20,7 @@ from internationalization.core.impl.po_file_manager import POFileManagerDB
 from internationalization.api.message import IMessageService, Message
 from internationalization.api.source import ISourceService, Source
 from datetime import datetime
+from introspection.api.component import Component
 
 class TestMessageService(IMessageService):
     '''
@@ -32,67 +33,69 @@ class TestMessageService(IMessageService):
 
     _pluginMessages = 8
 
-    def getMessagesCount(self, sourceId, q):
+    def getMessagesCount(self, sourceId=None, q=None):
         '''
         Provides the total count of messages searched based on the given parameters.
         '''
         return self._components * self._componentMessages + self._plugins * self._pluginMessages
 
-    def getMessages(self, sourceId, offset, limit, q):
+    def getMessages(self, sourceId=None, offset=None, limit=None, q=None):
         '''
         Provides the messages searched based on the given parameters.
         '''
         messages = []
         for c in range(self._components):
-            messages.extend(self.getComponentMessages(c))
+            messages.extend(self.getComponentMessages(str(c)))
         for p in range(self._plugins):
-            messages.extend(self.getPluginMessages(p))
+            messages.extend(self.getPluginMessages(str(p)))
         return messages
 
-    def getComponentMessagesCount(self, component, q):
+    def getComponentMessagesCount(self, component, q=None):
         '''
         Provides the total count of messages for the given component.
         '''
         return self._componentMessages
 
-    def getComponentMessages(self, component, offset, limit, q):
+    def getComponentMessages(self, component, offset=None, limit=None, q=None):
         '''
         Provides the messages for the given component.
         '''
         messages = []
         for m in range(self._componentMessages):
             msg = Message()
+            msg.Id = int(component) * 10 + m
             if m > 2:
-                msg.Id = 'component %i message %i' % component, m
+                msg.Singular = 'component %s message %d' % (component, m)
                 msg.Context = 'component'
             else:
-                msg.Id = 'message %i' % m
+                msg.Singular = 'message %i' % m
                 msg.Context = ''
-            msg.Source = 'src/component%i.py' % m
+            msg.Source = int(component)
             msg.LineNumber = 100 + 2 * m
             messages.append(msg)
         return messages
 
-    def getPluginMessagesCount(self, plugin, q):
+    def getPluginMessagesCount(self, plugin, q=None):
         '''
         Provides the total count of messages for the given plugin.
         '''
         return self._pluginMessages
 
-    def getPluginMessages(self, plugin, offset, limit, q):
+    def getPluginMessages(self, plugin, offset=None, limit=None, q=None):
         '''
         Provides the messages for the given plugin.
         '''
         messages = []
         for m in range(self._pluginMessages):
             msg = Message()
+            msg.Id = int(plugin) * 10 + m
             if m > 3:
-                msg.Id = 'plugin %i message %i' % plugin, m
+                msg.Singular = 'plugin %s message %d' % (plugin, m)
                 msg.Context = 'plugin'
             else:
-                msg.Id = 'message %i' % m
+                msg.Singular = 'message %i' % m
                 msg.Context = ''
-            msg.Source = 'src/plugin%i.py' % m
+            msg.Source = int(plugin)
             msg.LineNumber = 100 + 2 * m
             messages.append(msg)
         return messages
@@ -120,7 +123,7 @@ class TestSourceService(ISourceService):
 
     def getAll(self, offset=None, limit=None, q=None):
         src = Source()
-        src.LastModified = datetime.now()
+        src.LastModified = datetime(2012, 4, 1, 12, 15, 10)
         return [src]
 
     def insert(self, entity):
@@ -141,6 +144,18 @@ class TestHTTPDelivery(unittest.TestCase):
         poManager.sourceService = TestSourceService()
         poRepDir = TemporaryDirectory()
         poManager.locale_dir_path = poRepDir.name
+
+        srcService = TestSourceService()
+        self.assertEqual(srcService.getAll()[0].LastModified,
+                         poManager.getGlobalPOTimestamp())
+        self.assertEqual(srcService.getAll()[0].LastModified,
+                         poManager.getComponentPOTimestamp('1'))
+        self.assertEqual(srcService.getAll()[0].LastModified,
+                         poManager.getPluginPOTimestamp('1'))
+
+        poFile = poManager.getGlobalPOFile()
+        print(poFile)
+
         src = poManager.sourceService.getAll()[0]
         print(src)
 
