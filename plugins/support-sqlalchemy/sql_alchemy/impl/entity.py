ca2039b89947f453a2756b492edd19dc97993a86
Gabriel Nistor <gabriel.nistor@sourcefabric.org>
2012-04-09 10:15:32 +0300

    NR-89 : Small bug fixes
    Added the method override, and fixed a sql alchemy session bug (it was not closing the session for requests that ended the chain execution).

--- plugins/support-sqlalchemy/sql_alchemy/impl/entity.py
+++ plugins/support-sqlalchemy/sql_alchemy/impl/entity.py
@@ -213,6 +213,7 @@ class EntityCRUDServiceAlchemy(EntitySupportAlchemy):
         try:
             return self.session().query(self.Entity).filter(self.Entity.Id == id).delete() > 0
         except OperationalError:
+            assert log.debug('Could not delete entity %s with id \'%s\'', self.Entity, id, exc_info=True) or True
             raise InputError(Ref(_('Cannot delete because is in use'), model=self.model))
 
 class EntityGetCRUDServiceAlchemy(EntityGetServiceAlchemy, EntityCRUDServiceAlchemy):
