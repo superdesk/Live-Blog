b4e7249ea2aa90ef7257111ba0530b72acbab5ed
Mihai Balaceanu <gizm0bill@gmail.com>
2012-04-11 18:13:33 +0300

    NR-75 : GUI core plugin refactoring
    integration for require.js and dust.js throughout the whole UI base.

--- plugins/gui-core/gui-resources/scripts/js/views/menu.js
+++ plugins/gui-core/gui-resources/scripts/js/views/menu.js
@@ -1,28 +1,36 @@
 define([
-  'jquery','jquery.superdesk','jquery.tmpl','jquery.rest',
-  'tmpl!layouts/dashboard',
-  'tmpl!navbar'
-  ], function($, superdesk){ 
-var MenuView = function() {
-	superdesk.apiUrl = config.api_url;
-	var menu = new $.rest(superdesk.apiUrl + '/resources/GUI/Action?path=menu')
-	.done(function(menu)
-	{
-		var displayMenu = []
-		$(menu).each(function()
-		{ 
-			if( this.Path == 'menu' ) return true;
-			displayMenu.push($.extend({}, this, { Path: this.Path.split('.'), DisplayName: this.Path.replace('.', '-') })) 
-		});
-		$('#navbar-top')
-		.tmpl( 'navbar', {superdesk: {menu: displayMenu}} )
-		.on('click', '.nav a', function()
-		{
-			require([superdesk.apiUrl+'/'+$(this).attr('href')]);
-			return false 
-		});
-	});
-	$('#area-main').tmpl( 'layouts/dashboard' );
-}
-return MenuView;
+    'jquery','jquery.superdesk','jquery.tmpl','jquery.rest', 
+    'tmpl!layouts/dashboard',
+    'tmpl!navbar'
+], function($, superdesk)
+{
+    var MenuView = function() 
+    {
+        superdesk.apiUrl = config.api_url;
+        
+        var menu = new $.rest(superdesk.apiUrl + '/resources/GUI/Action?path=menu.*')
+        .done(function(menu)
+        {  
+    		var displayMenu = []
+    		$(menu).each(function()
+    		{ 
+    			displayMenu.push($.extend({}, this, { Path: this.Path.split('.'), DisplayName: this.Path.replace('.', '-') }));
+    		});
+    		
+    		$('#navbar-top')
+    		.tmpl( 'navbar', {superdesk: {menu: displayMenu}} )
+    		.on('click', '.nav a', function(event)
+    		{
+    		    superdesk.navigation.bind($(this).attr('href'), function()
+    		    {
+    		        require([superdesk.apiUrl+'/'+$(this).attr('href')]);
+    		    });
+    			event.preventDefault(); 
+    		});
+    		
+        });
+        $('#area-main').tmpl( 'layouts/dashboard' );
+    }
+
+    return MenuView;
 });
