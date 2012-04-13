414af5bdfae267ae51c4d857e76b36b7062846e7
ubuntu <ubuntu@ubuntu.(none)>
2012-04-13 08:01:41 -0700

    removed tmpl.js and renamed dustjs.js to tmpl.js from require folder Change paths

--- plugins/gui-core/gui-resources/scripts/js/views/menu.js
+++ plugins/gui-core/gui-resources/scripts/js/views/menu.js
@@ -19,7 +19,7 @@ define([
     		.tmpl( 'navbar', {superdesk: {menu: displayMenu}} )
     		.on('click', '.nav a', function(event)
     		{
-    		    superdesk.navigation.bind( $(this).attr('href'), require([config.api_url+'/'+$(this).attr('script-path')]) );
+    		    superdesk.navigation.bind( $(this).attr('href'), require([config.api_url + $(this).attr('script-path')]) );
     			event.preventDefault(); 
     		});
     		
