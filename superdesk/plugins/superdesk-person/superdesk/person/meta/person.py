577973f7331cae1dd3cbebc44ca8f8c56be88c44
Gabriel Nistor <gabriel.nistor@sourcefabric.org>
2012-04-17 11:48:06 +0300

    NR-89 : Small bug fixes
    Remaped the user/person relations.

--- superdesk/plugins/superdesk-person/superdesk/person/meta/person.py
+++ superdesk/plugins/superdesk-person/superdesk/person/meta/person.py
@@ -1,29 +1,27 @@
 '''
 Created on Aug 23, 2011
 
-@package superdesk
-@copyright 2011 Sourcefabric o.p.s.
-@license http://www.gnu.org/licenses/gpl-3.0.txt
+@package: superdesk person
+@copyright: 2012 Sourcefabric o.p.s.
+@license: http://www.gnu.org/licenses/gpl-3.0.txt
 @author: Mihai Balaceanu
 
-Contains the SQL alchemy meta for language API.
+Contains the SQL alchemy meta for person API.
 '''
 
 from ally.support.sqlalchemy.mapper import mapperModel
 from sqlalchemy.dialects.mysql.base import INTEGER
-from sqlalchemy.schema import Table, Column, ForeignKey
+from sqlalchemy.schema import Table, Column
 from sqlalchemy.types import String
 from superdesk.meta import meta
 from ..api.person import Person
-from superdesk.user.meta.user import User
 
 # --------------------------------------------------------------------
 
 table = Table('person', meta,
                Column('id', INTEGER(unsigned=True), primary_key=True, key='Id'),
-               Column('fk_user_id', ForeignKey(User.Id), nullable=True, key='User'),
-               Column('firstname', String(20), nullable=True, unique=False, key='FirstName'),
-               Column('lastname', String(20), nullable=True, unique=False, key='LastName'),
+               Column('first_name', String(20), nullable=True, unique=False, key='FirstName'),
+               Column('last_name', String(20), nullable=True, unique=False, key='LastName'),
                Column('address', String(20), nullable=True, unique=False, key='Address'),
                mysql_engine='InnoDB', mysql_charset='utf8')
 
