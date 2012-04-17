d6a22ac28ea0f12e8cc92de9a06da759b98d1fb1
Gabriel Nistor <gabriel.nistor@sourcefabric.org>
2012-04-17 11:48:06 +0300

    NR-89 : Small bug fixes
    Remaped the user/person relations.

--- superdesk/plugins/superdesk-country/superdesk/country/api/__init__.py
+++ superdesk/plugins/superdesk-country/superdesk/country/api/__init__.py
@@ -1,8 +1,8 @@
 '''
 Created on Jul 15, 2011
 
-@package superdesk language
-@copyright 2011 Sourcefabric o.p.s.
+@package: superdesk country
+@copyright: 2012 Sourcefabric o.p.s.
 @license http://www.gnu.org/licenses/gpl-3.0.txt
 @author: Gabriel Nistor
 
--- superdesk/plugins/superdesk-user/superdesk/user/meta/user.py
+++ superdesk/plugins/superdesk-user/superdesk/user/meta/user.py
@@ -1,27 +1,27 @@
 '''
 Created on Aug 23, 2011
 
-@package superdesk
-@copyright 2011 Sourcefabric o.p.s.
-@license http://www.gnu.org/licenses/gpl-3.0.txt
+@package: superdesk user
+@copyright: 2012 Sourcefabric o.p.s.
+@license: http://www.gnu.org/licenses/gpl-3.0.txt
 @author: Mihai Balaceanu
 
 Contains the SQL alchemy meta for language API.
 '''
 
+from ..api.user import User
 from ally.support.sqlalchemy.mapper import mapperModel
-from sqlalchemy.dialects.mysql.base import INTEGER
-from sqlalchemy.schema import Table, Column
+from sqlalchemy.schema import Table, Column, ForeignKey
 from sqlalchemy.types import String
 from superdesk.meta import meta
-from ..api.user import User
+from superdesk.person.meta.person import Person
 
 # --------------------------------------------------------------------
 
 table = Table('user', meta,
-               Column('id', INTEGER(unsigned=True), primary_key=True, key='Id'),
+               Column('fk_person_id', ForeignKey(Person.Id), nullable=False, primary_key=True, key='Id'),
                Column('name', String(20), nullable=False, unique=True, key='Name'),
                mysql_engine='InnoDB', mysql_charset='utf8')
 
 # map User entity to defined table (above)
-User = mapperModel(User, table)
+User = mapperModel(User, table, inherits=Person)
