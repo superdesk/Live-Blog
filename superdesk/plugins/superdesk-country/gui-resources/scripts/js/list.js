fdf83f92eb903a9b7b64cba387eaeda0bb026b55
Mihai Balaceanu <gizm0bill@gmail.com>
2012-04-27 12:09:46 +0200

    NR-80 : UI functionality for countries management
    fixed a bug with rendering datatable elements more than 1 time

--- superdesk/plugins/superdesk-country/gui-resources/scripts/js/list.js
+++ superdesk/plugins/superdesk-country/gui-resources/scripts/js/list.js
@@ -5,7 +5,8 @@
  * @copyright 2012 Sourcefabric o.p.s.
  * @license http://www.gnu.org/licenses/gpl.txt
  */
-define([
+define
+([
   'jquery','jquery/superdesk','jquery/tmpl','jquery/rest', 'jqueryui/datatable', 
   'tmpl!layouts/list',
   'tmpl!country>add',
@@ -13,65 +14,70 @@ define([
   'tmpl!country>list-head',
   'tmpl!country>list-body',
   'tmpl!country>list-footer',
-  ], function($, superdesk){ 
-var ListApp = function( paths ) {
-		$('#area-main').tmpl('list');
-		$('#area-content').html( $('<table class="table table-bordered table-striped country-list" />').datatable
-		({
-			tpl: 
-			{
-				header: 'list-head',
-				footer: 'list-footer',
-				body: 'list-body'
-			},
-			resource: new $.rest(superdesk.apiUrl + '/resources/Superdesk/Country').xfilter('Code, Name')
-		}));
-		$.tmpl('add', {}, function(error, output){
-			$('#area-content').append(output);
-		});
-		//presentation.view.load('country/templates/list.html').done(app);
-
-	// details button functionality 
-	$(document)
-	.off('click.superdesk-country-list', '.country-list .btn-info')
-	.on('click.superdesk-country-list', '.country-list .btn-info', function(event)
-	{
-		new $.rest($(this).attr('href')).done(function(data)
-		{
-			require(['tmpl!country>details','jqueryui/dialog'], function() {
-				$.tmpl('details', data, function(error, output) {
-					$(output)
-					.dialog
-					({ 
-						draggable: false,
-						resizable: false,
-						modal: true,
-						width: "40.1709%",
-						close: function(){ $(this).dialog('destroy').remove(); },
-						buttons: 
-						[{
-							text: "Close",
-							click: function(){ $(this).dialog('close'); },
-							class: "btn btn-primary"
-						}]
-					});
-
-				});
-			});
-		});
-		event.preventDefault();
-	});
-	$(document)
-	.off('click.superdesk-country-list', '.country-add.btn')
-	.on('click.superdesk-country-list', '.country-add.btn', function(event)
-	{
-		superdesk.navigation.bind('/country/add', function()
-		{
-			console.log('add country');
-		});
-		event.preventDefault();
-	});	
-}
+], 
+function($, superdesk)
+{
+    var datatable = $('<table class="table table-bordered table-striped country-list" />').datatable
+    ({
+        tpl: 
+        {
+            header: 'list-head',
+            footer: 'list-footer',
+            body: 'list-body'
+        },
+        resource: new $.rest(superdesk.apiUrl + '/resources/Superdesk/Country').xfilter('Code, Name')
+    });
+    
+    var ListApp = function( paths ) 
+    {
+        $('#area-main').tmpl('list', {ui:{ content: 'is-content=1'}});
+        $('#area-main [is-content]').html( datatable );
+        
+    	$.tmpl('add', {}, function(error, output){ $('#area-content').append(output); });
+    
+    	// details button functionality 
+    	$(document)
+    	.off('click.superdesk-country-list', '.country-list .btn-info')
+    	.on('click.superdesk-country-list', '.country-list .btn-info', function(event)
+    	{
+    		new $.rest($(this).attr('href')).done(function(data)
+    		{
+    			require(['tmpl!country>details','jqueryui/dialog'], function() {
+    				$.tmpl('details', data, function(error, output) {
+    					$(output)
+    					.dialog
+    					({ 
+    						draggable: false,
+    						resizable: false,
+    						modal: true,
+    						width: "40.1709%",
+    						close: function(){ $(this).dialog('destroy').remove(); },
+    						buttons: 
+    						[{
+    							text: "Close",
+    							click: function(){ $(this).dialog('close'); },
+    							class: "btn btn-primary"
+    						}]
+    					});
+    
+    				});
+    			});
+    		});
+    		event.preventDefault();
+    	});
+    	$(document)
+    	.off('click.superdesk-country-list', '.country-add.btn')
+    	.on('click.superdesk-country-list', '.country-add.btn', function(event)
+    	{
+    		superdesk.navigation.bind('/country/add', function()
+    		{
+    			console.log('add country');
+    		});
+    		event.preventDefault();
+    	});	
+    };
+    
 return ListApp;
+
 });
 
