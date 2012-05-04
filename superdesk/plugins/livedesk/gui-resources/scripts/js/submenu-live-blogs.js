7b3968da163bcc6fe5206a1f4988ff1077e94a5a
Mihai Balaceanu <gizm0bill@gmail.com>
2012-05-04 16:23:48 +0300

    NR-37 : Live Blogging Admin Screen
    Edit screen wip

--- superdesk/plugins/livedesk/gui-resources/scripts/js/submenu-live-blogs.js
+++ superdesk/plugins/livedesk/gui-resources/scripts/js/submenu-live-blogs.js
@@ -1,14 +1,42 @@
 define
 ([
-  'jquery', 'jquery/tmpl', 'jquery/rest',
+  'jquery', 'jquery/superdesk', 'jquery/tmpl', 'jquery/rest',
   'tmpl!livedesk>submenu',
-], function($)
+  'tmpl!livedesk>layouts/livedesk'
+], function($, superdesk)
 {
-    app = {
-        init: function(submenu) {
-            setTimeout(function(){ 
-                $(submenu).tmpl('submenu');
-            }, 3000)
+    app = 
+    {
+        init: function(submenu) 
+        {
+            setTimeout(function()
+            { 
+                $(submenu).tmpl('submenu', function()
+                { 
+                    var createBtn = $(this).find('#submenu-liveblogs-create');
+                    createBtn.off('click.livedesk')
+                        .on('click.livedesk', function()
+                        { 
+                            // add layout
+                            $('#area-main').tmpl('layouts/livedesk', function()
+                            {
+                                // get modules.* actions
+                                superdesk.getActions('modules.livedesk.*')
+                                .done(function(actions)
+                                {
+                                    $(actions).each(function()
+                                    {
+                                        // and display add action
+                                        if(this.Path == 'modules.livedesk.add' && this.ScriptPath)
+                                            require([superdesk.apiUrl+this.ScriptPath], function(AddApp) {
+                                                listApp = new AddApp();
+                                            });
+                                    });
+                                });
+                            });
+                        });
+                });
+            }, 3000);
         }
     };
     return app;
