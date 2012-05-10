9dd7e733b0bd7f7d0c0ac0221cc7be571d2e4875
Mihai Balaceanu <gizm0bill@gmail.com>
2012-05-10 18:39:34 +0300

    NR-37 : Live Blogging Admin Screen
    templating/css
    also working on some functionality for texteditor
    made a method to drop the client demo file on cdm on api startup

--- plugins/gui-core/__plugin__/gui_core/gui_lib.py
+++ plugins/gui-core/__plugin__/gui_core/gui_lib.py
@@ -14,6 +14,7 @@ from .gui_core import getGuiPath, lib_folder_format, publishLib
 from ally.container import ioc
 from ally.support.util_io import openURI
 from io import BytesIO
+from __plugin__.gui_core.gui_core import getPublishedLib
 
 # --------------------------------------------------------------------
 
@@ -32,6 +33,15 @@ def js_bootstrap_file():
     ''' The javascript core libraries '''
     return 'scripts/js/startup.js'
 
+@ioc.config()
+def ui_demo_file():
+    ''' the demo client html file '''
+    return 'start.html'
+
+@ioc.config()
+def server_url():
+    ''' for demo file update... '''
+    return 'localhost:8080'
 # --------------------------------------------------------------------
 
 @ioc.start
@@ -51,3 +61,17 @@ def updateStartup():
     cdmGUI().publishFromFile(bootPath + js_bootstrap_file(), BytesIO(b'\n'.join([fi.read() for fi in fileList])))
 
     for f in fileList: f.close()
+    
+@ioc.after(publish)
+def updateDemoFile():
+    try:
+        bootPath = lib_folder_format() % 'core/'
+        with openURI(getGuiPath(ui_demo_file())) as f:
+            out = f.read().replace(b'{server_url}', bytes(server_url(), 'utf-8'))
+            cdmGUI().publishFromFile(bootPath + ui_demo_file(), BytesIO(out))
+    except Exception as e: 
+        print('Error publishing demo client file:', e)
+        return
+    print('Client demo script published:', server_url()+getPublishedLib('core/'+ui_demo_file()) )
+    
+    
