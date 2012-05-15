1290748926d0cca8bc350d5109797d2cad9f5c1d
Mugur Rus <mugur.rus@sourcefabric.org>
2012-05-15 18:21:26 +0300

    NR-114 : Live Blog back end

--- superdesk/plugins/superdesk-person/superdesk/person/meta/person.py
+++ superdesk/plugins/superdesk-person/superdesk/person/meta/person.py
@@ -10,19 +10,42 @@ Contains the SQL alchemy meta for person API.
 '''
 
 from ..api.person import Person
-from ally.support.sqlalchemy.mapper import mapperModel
 from sqlalchemy.dialects.mysql.base import INTEGER
-from sqlalchemy.schema import Table, Column
+from sqlalchemy.schema import Column
 from sqlalchemy.types import String
-from superdesk.meta.metadata_superdesk import meta
+from superdesk.meta.metadata_superdesk import Base
+from sqlalchemy.ext.hybrid import hybrid_property
+from ally.container.binder_op import validateManaged
+from ally.support.sqlalchemy.mapper import registerValidation
+from sqlalchemy.orm.mapper import reconstructor
 
 # --------------------------------------------------------------------
 
-table = Table('person', meta,
-               Column('id', INTEGER(unsigned=True), primary_key=True, key='Id'),
-               Column('first_name', String(255), key='FirstName'),
-               Column('last_name', String(255), key='LastName'),
-               Column('address', String(255), key='Address'),
-               mysql_engine='InnoDB', mysql_charset='utf8')
+class PersonMapped(Base, Person):
+    '''
+    Provides the mapping for Person entity.
+    '''
+    __tablename__ = 'person'
+    __table_args__ = dict(mysql_engine='InnoDB', mysql_charset='utf8')
 
-Person = mapperModel(Person, table)
+    Id = Column('id', INTEGER(unsigned=True), primary_key=True)
+    FirstName = Column('first_name', String(255))
+    LastName = Column('last_name', String(255))
+    Address = Column('address', String(255))
+
+    # Calculated model attributes ------------------------------------
+    @hybrid_property
+    def Name(self):
+        name = '%s %s' % ('' if self.FirstName is None else self.FirstName,
+                          '' if self.LastName is None else self.LastName)
+        print('name: %s' % name)
+        return name.strip()
+
+#    @reconstructor
+#    def init_on_load(self):
+#        self.Name = '%s %s' % ('' if self.FirstName is None else self.FirstName,
+#                               '' if self.LastName is None else self.LastName)
+#        self.Name = self.Name.strip()
+
+#validateManaged(PersonMapped.Name)
+#registerValidation(PersonMapped)
