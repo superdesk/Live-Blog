840cbada58e5ea8fd02eaf25305ba6d39f027616
Mihai Balaceanu <gizm0bill@gmail.com>
2012-05-16 19:13:44 +0300

    NR-37 : Live Blogging Admin Screen
    Init collaborator left sidebar

--- superdesk/plugins/livedesk/gui-resources/scripts/js/edit-live-blogs.js
+++ superdesk/plugins/livedesk/gui-resources/scripts/js/edit-live-blogs.js
@@ -1,9 +1,21 @@
-define(['jquery', 'jqueryui/texteditor',
-        'tmpl!livedesk>layouts/livedesk', 'tmpl!livedesk>edit', 'tmpl!livedesk>edit-timeline'], 
-function($) 
+requirejs.config
+({
+    paths: 
+    { 
+        providers:'gui/superdesk/livedesk/scripts/js/providers' 
+    } 
+});
+define([
+        'providers/enabled',
+        'jquery', 'jqueryui/texteditor','jquery/splitter', 
+        'tmpl!livedesk>layouts/livedesk',
+        'tmpl!livedesk>layouts/blog',
+        'tmpl!livedesk>edit', 'tmpl!livedesk>edit-timeline'
+], 
+function(providers, $) 
 {
-    // wrap image command
-    var editorImageControl = function()
+    var providers = $.arrayValues(providers), 
+        editorImageControl = function()
         {
             // call super
             var command = $.ui.texteditor.prototype.plugins.controls.image.apply(this, arguments);
@@ -17,27 +29,54 @@ function($)
             return command;
         },
         editorTitleControls = $.extend({}, $.ui.texteditor.prototype.plugins.controls, { image : editorImageControl }),
-        initEditBlog = function()
+        initEditBlog = function(theBlog)
         {
             var content = $(this).find('[is-content]'),
                 h2ctrl = $.extend({}, $.ui.texteditor.prototype.plugins.controls);
             delete h2ctrl.justifyRight;
             delete h2ctrl.justifyLeft;
             delete h2ctrl.justifyCenter; 
+            delete h2ctrl.html;
+            delete h2ctrl.image;
+            delete h2ctrl.link;
             content.find('section header h2').texteditor
             ({
                 plugins: {controls: h2ctrl},
                 floatingToolbar: 'top'
             });
             content.find('article#blog-intro').texteditor({floatingToolbar: 'top', plugins:{ controls: editorTitleControls }});
+            
+            $('.tabbable').on('show','a[data-toggle="tab"]', function(e) 
+            {
+                var el = $(e.target);
+                var idx = parseInt(el.attr('data-idx'));
+                providers[idx].el = $(el.attr('href'));
+                providers[idx].init(theBlog);
+            });
         };
     
     var EditApp = function(theBlog)
     {
         var blog = new $.rest(theBlog).xfilter('Creator.Name, Creator.Id').done(function(blogData)
         { 
-            var data = $.extend({}, blogData, {ui: {content: 'is-content=1', side: 'is-side=1'}}),
-                content = $.superdesk.applyLayout('livedesk>edit', data, initEditBlog);
+            
+            var data = $.extend({}, blogData, {ui: {content: 'is-content=1', side: 'is-side=1'}, providers: providers}),
+                content = $.superdesk.applyLayout('livedesk>edit', data, function(){ initEditBlog(theBlog); });
+            
+            $("#MySplitter").splitter({
+                type: "v",
+                outline: true,
+                sizeLeft: 470,
+                minLeft: 470,
+                minRight: 600,
+                resizeToWidth: true,
+                //dock: "left",
+                dockSpeed: 100,
+                cookie: "docksplitter",
+                dockKey: 'Z',   // Alt-Shift-Z in FF/IE
+                accessKey: 'I'  // Alt-Shift-I in FF/IE
+            }); 
+            
             
             $('.collapse-title-page', content).off('click.livedesk')
             .on('click.livedesk', function()
