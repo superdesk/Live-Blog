ae5ae2470a56d916ecf75d0b19faf9e3476bfbef
Mihai Nistor <Mihai.Nistor@sourcefabric.org>
2012-06-06 01:32:03 -0700

    NR-208: Implement Meta object for providers

--- superdesk/plugins/livedesk/gui-resources/scripts/js/providers/google/adaptor.js
+++ superdesk/plugins/livedesk/gui-resources/scripts/js/providers/google/adaptor.js
@@ -22,25 +22,42 @@ define('providers/google/adaptor', [
                         }
                     });
             },
+			universal: function(obj) {
+				var meta =  jQuery.extend(true, {}, obj);
+				delete meta.content;
+				return {
+                    Content: obj.content,
+                    Type: 'normal',
+                    Author: this.author,
+					Meta: meta
+                };
+            },
+			// Todo remove this stuff
             web: function(obj) {
-                return {
+                delete obj['$idx'];
+				delete obj['$len'];
+				delete obj['GsearchResultClass'];
+				return {
                     Content: str.format('<h3><a href="%(url)s">%(title)s</a></h3><p class="result-text">%(content)s</p><i class="source-icon"><img src="http://g.etfv.co/%(url)s" style="max-width: 16px" border="0"></i><a class="author-name" href="%(url)s">%(visibleUrl)s</a>',obj),
                     Type: 'normal',
-                    Author: this.author
+                    Author: this.author,
+					Meta: obj
                 };
             },
             news: function(obj) {
                 return {
                     Content: str.format('<h3><a href="%(url)s">%(title)s</a></h3><p class="result-text">%(content)s</p><i class="source-icon"><img src="http://g.etfv.co/%(url)s" style="max-width: 16px" border="0"></i><a class="author-name" href="%(unescapedUrl)s">%(publisher)s</a>',obj),
                     Type: 'normal',
-                    Author: this.author
+                    Author: this.author,
+					Meta: obj
                 };
             },
             images: function(obj) {
                 return {
                     Content: str.format('<p class="result-text">%(content)s</p><a href="%(url)s"><img src="%(tbUrl)s"/></a>', obj),
                     Type: 'normal',
-                    Author: this.author
+                    Author: this.author,
+					Meta: obj
                 };
             }
         }
