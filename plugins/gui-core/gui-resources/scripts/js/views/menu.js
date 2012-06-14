023964bf66453ab7bdceff5c5a21fa183da34be6
Mihai Balaceanu <gizm0bill@gmail.com>
2012-06-14 18:43:06 +0300

    refactoring wiht models and collections
    added gizmo.js and some scripts to test

--- plugins/gui-core/gui-resources/scripts/js/views/menu.js
+++ plugins/gui-core/gui-resources/scripts/js/views/menu.js
@@ -27,7 +27,7 @@ function($, superdesk, dust)
         			    { 
         			        $(subs).each(function()
         			        { 
-        			            require([config.api_url + this.ScriptPath], function(x){ x.init(Subz); }); 
+        			            require([config.api_url + this.ScriptPath], function(x){ x && x.init && x.init(Subz); }); 
         			        });
         			    });
         			}
