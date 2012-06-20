2bf6ad79a2844d01f2020abadbfd5dac51d3bf51
Mihai Nistor <Mihai.Nistor@sourcefabric.org>
2012-06-20 11:41:15 +0300

    Revert "Revert "NR-209: UI Optimization & Fixes""
    This reverts commit 2e65a4a3fd979fe7b2d9536af5ea77cbcdc579c2.

---
+++ plugins/gui-core/gui-resources/scripts/js/utils/class.js
@@ -0,0 +1,40 @@
+define('utils/class',function(){
+(function(){
+  var initializing = false;
+  // The base Class implementation (does nothing)
+  this.Class = function(){};
+  
+  // Create a new Class that inherits from this class
+  Class.extend = function(prop, options) {   
+    // Instantiate a base class (but only create the instance,
+    // don't run the init constructor)
+    initializing = true;
+    var prototype = new this();
+    initializing = false;
+    
+    // Copy the properties over onto the new prototype
+    for (var name in prop) {
+      // Check if we're overwriting an existing function
+      prototype[name] = prop[name];
+    }
+    
+    // The dummy class constructor
+    function Class() {
+      // All construction is actually done in the init method
+      if ( !initializing && this._constructor )
+        return this._constructor.apply(this, arguments);
+    }
+    
+    // Populate our constructed prototype object
+    Class.prototype = prototype;
+    
+    // Enforce the constructor to be what we expect
+    Class.prototype.constructor = Class;
+
+    // And make this class extendable
+    Class.extend = arguments.callee;
+    
+    return Class;
+  };
+})();
+});
