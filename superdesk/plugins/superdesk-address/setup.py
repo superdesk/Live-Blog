8862c0a48194f1dfc905ef1ffb623357bca320e8
Mugur Rus <mugur.rus@sourcefabric.org>
2012-06-21 18:29:37 +0300

    NR-210 : Implement native egg build support

--- superdesk/plugins/superdesk-address/setup.py
+++ superdesk/plugins/superdesk-address/setup.py
@@ -14,19 +14,23 @@ from setuptools import setup, find_packages
 # --------------------------------------------------------------------
 
 setup(
-    name="gui_action",
+    name="superdesk_address",
     version="1.0",
     packages=find_packages(),
-    install_requires=['ally_api >= 1.0'],
+    install_requires=['ally_api >= 1.0', 'internationalization >= 1.0', 'gui_action >= 1.0',
+                      'gui_core >= 1.0'],
     platforms=['all'],
     zip_safe=True,
+    package_data={
+        '': ['*.gif', '*.png', '*.jpg', '*.jpeg', '*.js', '*.css', '*.less', '*.dust', '*.html'],
+    },
 
     # metadata for upload to PyPI
     author="Gabriel Nistor",
     author_email="gabriel.nistor@sourcefabric.org",
-    description="Ally framework - GUI actions plugin",
-    long_description='The plugin that implements that actions management service',
+    description="Superdesk address plugin",
+    long_description='Address management functionality',
     license="GPL v3",
-    keywords="Ally REST framework plugin GUI",
+    keywords="Ally REST framework plugin Livedesk",
     url="http://www.sourcefabric.org/en/superdesk/", # project home page
 )
