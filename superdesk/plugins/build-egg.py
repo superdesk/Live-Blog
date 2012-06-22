1faaf195f52091a270d6bc7aa410688a60eba89a
Mugur Rus <mugur.rus@sourcefabric.org>
2012-06-22 15:17:16 +0300

    NR-210 : Implement native egg build support

--- superdesk/plugins/build-egg.py
+++ superdesk/plugins/build-egg.py
@@ -12,14 +12,41 @@ from os.path import join, dirname, sep, split
 import imp
 import sys
 from os import chdir, getcwd
+from os.path import isdir, normpath, basename
+from shutil import move, copy2
 
 # --------------------------------------------------------------------
 
+distComponents = '../../distribution/components'
+distPlugins = '../../distribution/plugins'
+sdComponents = '../distribution/components'
+sdPlugins = '../distribution/plugins'
+
+def copyDir(srcDir, dstDir, filter=None):
+    if isdir(srcDir):
+        files = glob(join(srcDir, filter)) if isinstance(filter, str) else srcDir
+        for file in files:
+            fName = basename(file)
+            newPath = join(dstDir, fName)
+            print("* copy %s\n  to   %s" % (file, newPath))
+            copy2(file, newPath + '.new')
+            move(newPath + '.new', newPath)
+
 if __name__ == '__main__':
+    sys.argv[0] = 'setup.py'
     sys.argv.insert(1, 'bdist_egg')
-    currentDir = getcwd()
-    setupScripts = glob(join(dirname(__file__), '*', 'setup.py'))
+    currentDir, fileDir = getcwd(), dirname(__file__)
+    setupScripts = glob(join(fileDir, '*', 'setup.py'))
     for script in setupScripts:
         chdir(dirname(script))
         module = imp.load_source('setup', script)
+        print("\n".rjust(79, '-'))
     chdir(currentDir)
+
+    componentsDir = normpath(join(fileDir, distComponents))
+    sdComponentsPath = normpath(join(fileDir, sdComponents))
+    copyDir(componentsDir, sdComponentsPath, '*.egg')
+
+    pluginsDir = normpath(join(fileDir, distPlugins))
+    sdPluginsPath = normpath(join(fileDir, sdPlugins))
+    copyDir(pluginsDir, sdPluginsPath, '*.egg')
