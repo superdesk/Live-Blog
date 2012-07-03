43b437447e3eb33f215bb1d16ccac4a169940dee
Mihai Nistor <Mihai.Nistor@sourcefabric.org>
2012-07-03 12:01:11 +0300

    Gizmo implementation or edit provider

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
 setlocal
-set PYTHONPATH=superdesk/plugins/livedesk;plugins/gui-core;plugins/test-dev
+set PYTHONPATH=superdesk/plugins/livedesk;plugins/gui-core;plugins/test-dev;plugins/gui-action;
 python.exe superdesk/distribution/application.py
