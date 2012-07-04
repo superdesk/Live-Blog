6f171a075bc465ea66751e46e3d9f5f594edd82c
Mihai Balaceanu <gizm0bill@gmail.com>
2012-07-04 11:27:28 +0300

    NR-216 : UI MVC - Collection implementation
    models for livedesk and some changes to the core

--- plugins/gui-core/gui-resources/scripts/js/utils/class.js
+++ plugins/gui-core/gui-resources/scripts/js/utils/class.js
@@ -1,40 +1,47 @@
-define('utils/class',function(){
-(function(){
-  var initializing = false;
-  // The base Class implementation (does nothing)
-  this.Class = function(){};
-  
-  // Create a new Class that inherits from this class
-  Class.extend = function(prop, options) {   
-    // Instantiate a base class (but only create the instance,
-    // don't run the init constructor)
-    initializing = true;
-    var prototype = new this();
-    initializing = false;
+define('utils/class',
+function()
+{
+    (function(){
+      var initializing = false;
+      // The base Class implementation (does nothing)
+      this.Class = function(){};
+      
+      // Create a new Class that inherits from this class
+      Class.extend = function(prop, options) {   
+        // Instantiate a base class (but only create the instance,
+        // don't run the init constructor)
+        initializing = true;
+        var prototype = new this();
+        initializing = false;
+        
+        // Copy the properties over onto the new prototype
+        for (var name in prop) {
+          // Check if we're overwriting an existing function
+          prototype[name] = prop[name];
+        }
+        
+        // The dummy class constructor
+        function Class() {
+          // All construction is actually done in the init method
+          if ( !initializing && ( this._constructor || this._construct ) )
+              try
+              { 
+                  var constructor = this._construct || this._constructor; 
+                  return constructor.apply(this, arguments);
+              }
+              catch(e){}
+        }
+        
+        // Populate our constructed prototype object
+        Class.prototype = prototype;
+        
+        // Enforce the constructor to be what we expect
+        Class.prototype.constructor = Class;
     
-    // Copy the properties over onto the new prototype
-    for (var name in prop) {
-      // Check if we're overwriting an existing function
-      prototype[name] = prop[name];
-    }
-    
-    // The dummy class constructor
-    function Class() {
-      // All construction is actually done in the init method
-      if ( !initializing && this._constructor )
-        return this._constructor.apply(this, arguments);
-    }
-    
-    // Populate our constructed prototype object
-    Class.prototype = prototype;
-    
-    // Enforce the constructor to be what we expect
-    Class.prototype.constructor = Class;
-
-    // And make this class extendable
-    Class.extend = arguments.callee;
-    
-    return Class;
-  };
-})();
+        // And make this class extendable
+        Class.extend = arguments.callee;
+        
+        return Class;
+      };
+    })();
 });
