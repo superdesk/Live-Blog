d771caa659c4c887269d5ed268f13907f044b830
Mihai Balaceanu <gizm0bill@gmail.com>
2012-07-04 11:27:28 +0300

    NR-216 : UI MVC - Collection implementation
    models for livedesk and some changes to the core

--- superdesk/plugins/livedesk/gui-resources/scripts/js/providers/colabs.old.js
+++ superdesk/plugins/livedesk/gui-resources/scripts/js/providers/colabs.old.js
@@ -1,117 +1,15 @@
-define( 'providers/colabs', 
-[ 'providers', 'jquery', 'gizmo/superdesk', 
-  
-  'gui/superdesk/livedesk/scripts/js/models/blog',
-  
-  'jquery/rest',
-  'jquery/avatar',
+define('providers/colabs', 
+[ 'providers', 'jquery', 'jquery/rest', 'jquery/avatar',
   'providers/colabs/adaptor',
   'tmpl!livedesk>providers/colabs',
-  'tmpl!livedesk>providers/colabs/items' ],
-function(providers, $, giz, blog)
+  'tmpl!livedesk>providers/colabs/items'
+],
+function(providers, $)
 {
     var config = { updateInterval: 30 },
         colabsList = [], 
         updateInterval = 0,
         intervalRunning = false;
-    
-    
-    var ColabView = giz.View.extend
-    ({
-        init: function()
-        {
-            $('.search-result-list', this.el).html('')
-            colabsList = [];
-            
-            var self = this,
-            setupHeader = function()
-            {
-                $('.collaborators-header', self.el)
-                .off('click', 'span[data-colab-id]')
-                .on('click', 'span[data-colab-id]', function()
-                {
-                    var colabId = $(this).attr('data-colab-id'),
-                        posts = $('.collaborators-header', self.el).nextAll('.search-result-list').find('li[data-colab-id='+colabId+']');
-                    
-                    if($(this).data('is-off'))
-                    {
-                        posts.show();
-                        $(this).addClass('label-info').data('is-off', false);
-                    }
-                    else
-                    {
-                        posts.hide(); 
-                        $(this).removeClass('label-info').data('is-off', true);
-                    }
-                });  
-                $('.new-results', self.el).off('update.livedesk').on('update.livedesk', function(e, count, callback)
-                {
-                    var self = $(this);
-                    $(this).removeClass('hide')
-                        .text(count+" "+_('new items! Update')).one('click.livedesk', function()
-                        { 
-                            self.addClass('hide'); 
-                            callback.apply(this);
-                        });
-                });
-            },
-            updateItemCount = 0,
-            update = function()
-            {
-                $(colabsList).each(function()
-                {
-                    var colab = this,
-                        updateRequest = new $.rest(colab).resetData()
-                        .request({data: {'startEx.cId': colab._latestPost}})
-                        .get('PostPublished').resetData().xfilter('CreatedOn,Content,PublishedOn,Type,Id,CId')
-                        .get('PostUnpublished').resetData().xfilter('CreatedOn,Content,PublishedOn,Type,Id,CId')
-                    .done(function(published, unpublished)
-                    {
-                        published = this.extractListData(published[0]);
-                        unpublished = this.extractListData(unpublished[0]);
-                        var postList = published.concat(unpublished);
-                        
-                        var appendPosts = [];
-                        for(var i=0; i<postList.length; i++)
-                        {
-                            if(parseInt(postList[i].CId) > colab._latestPost)
-                            {
-                                appendPosts.push(postList[i]);
-                                colab._latestPost = postList[i].CId;
-                            }
-                        }
-                        updateItemCount += appendPosts.length; 
-                        appendPosts.length &&
-                        $('.new-results', self.el).trigger('update.livedesk', [updateItemCount, function()
-                        {
-                            $.tmpl('livedesk>providers/colabs/items', {Person: colab.Person, Posts: appendPosts}, function(e, o)
-                            {
-                                $('<ul>'+ o + '</ul>').find('li').each(function(){
-                                    var postId = $(this).attr('data-post-id');
-                                    $('.search-result-list li[data-post-id="' + postId + '"]', self.el).remove();
-                                    $('.search-result-list', self.el).scrollTop(0).prepend($(this));
-                                });
-                                //$('.search-result-list', self.el).scrollTop(0).prepend(o);
-                                $('.search-result-list li.draggable', self.el).draggable
-                                ({
-                                    helper: 'clone',
-                                    appendTo: 'body',
-                                    zIndex: 2700,
-                                    start: function() 
-                                    {
-                                        $(this).data('post', self.adaptor.universal(this));
-                                    }
-                                });
-                                updateItemCount -= appendPosts.length;
-                            });
-                        }]);
-                    });
-                });
-            };
-        }
-    });
-    
-    
     
     $.extend(providers.colabs,  
     {
@@ -206,7 +104,6 @@ function(providers, $, giz, blog)
                 });
             };
 
-            
             new $.restAuth(theBlog).get('Collaborator').resetData().done(function(listData)
             {
 
