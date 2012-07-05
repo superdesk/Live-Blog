619ed66b60d890166066c01c6f1a353db2b282fa
Mihai Balaceanu <gizm0bill@gmail.com>
2012-07-05 18:17:54 +0300

    NR-216 : UI MVC - Collection implementation
    implementation on collaborators work in progress

--- superdesk/plugins/livedesk/gui-resources/scripts/js/models/blog-collaborator.js
+++ superdesk/plugins/livedesk/gui-resources/scripts/js/models/blog-collaborator.js
@@ -2,6 +2,8 @@ define(['gizmo/superdesk',
         'gui/superdesk/livedesk/scripts/js/models/collaborator'], 
 function(giz, Collaborator)
 {
+    var BlogCollaborator = giz.Collection.extend({ model: Collaborator });  
     // Blog Collaborator list
-    return giz.Collection.extend({ model: Collaborator });
+    return BlogCollaborator;
+    
 });
