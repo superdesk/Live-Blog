0d0cf71af269e51cd0064640f05ff7b0688e08a8
Mihai Nistor <Mihai.Nistor@sourcefabric.org>
2012-07-12 15:42:26 +0300

    NR-213: Core implementation of MVC framework
    Added Register functionality
    Implemented edit own posts.

--- superdesk/plugins/livedesk/gui-resources/scripts/js/models/blog-collaborator.js
+++
@@ -1,9 +0,0 @@
-define(['gizmo/superdesk', 
-        'gui/superdesk/livedesk/scripts/js/models/collaborator'], 
-function(giz, Collaborator)
-{
-    var BlogCollaborator = giz.Collection.extend({ model: Collaborator });  
-    // Blog Collaborator list
-    return BlogCollaborator;
-    
-});
