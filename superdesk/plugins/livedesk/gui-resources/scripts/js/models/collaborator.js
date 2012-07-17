2a0578669ada3b201cc941a9926c40c2650e4814
Mihai Balaceanu <gizm0bill@gmail.com>
2012-07-17 12:11:04 +0300

    NR-213: Core implementation of MVC framework
    Reconfig

--- superdesk/plugins/livedesk/gui-resources/scripts/js/models/collaborator.js
+++ superdesk/plugins/livedesk/gui-resources/scripts/js/models/collaborator.js
@@ -1,19 +1,19 @@
 define(['gizmo/superdesk',
-        'livedesk/models/post',
-        'livedesk/models/source',
-        'livedesk/models/person'], 
-function(Gizmo)
+    config.guiJs('livedesk', 'models/posts'),
+    config.guiJs('livedesk', 'models/source'),
+    config.guiJs('livedesk', 'models/person')], 
+function(giz, Posts, Source, Person)
 {
     // Collaborator
-    return Gizmo.Model.extend
+    return giz.Model.extend
     ({ 
         defaults:
         { 
-            Post: Gizmo.Collection.extend({ model: Gizmo.Model.Post }),
-            PostPublished: Gizmo.Collection.extend({ model: Gizmo.Model.Post }),
-            PostUnpublished: Gizmo.Collection.extend({ model: Gizmo.Model.Post }),
-            Source: Gizmo.Model.Source,
-            Person: Gizmo.Model.Person
+            Post: Posts,
+            PostPublished: Posts,
+            PostUnpublished: Posts,
+            Source: Source,
+            Person: Person
         }
     }, { register: 'Collaborator' } );
 });
