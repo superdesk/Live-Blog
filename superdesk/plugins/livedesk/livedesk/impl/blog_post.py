571edb5cc4376784deb292579a01c35528309046
Gabriel Nistor <gabriel.nistor@sourcefabric.org>
2012-07-18 16:18:44 +0300

    NR-227 : Add order change to the blog posts
    Added ordering to the blog posts.

--- superdesk/plugins/livedesk/livedesk/impl/blog_post.py
+++ superdesk/plugins/livedesk/livedesk/impl/blog_post.py
@@ -83,7 +83,7 @@ class BlogPostServiceAlchemy(SessionSupport, IBlogPostService):
         sql = self._buildQuery(blogId, typeId, creatorId, authorId, q)
         sql = sql.filter(BlogPostMapped.PublishedOn == None)
 
-        sql = sql.order_by(desc_op(BlogPostMapped.Order))
+        sql = sql.order_by(desc_op(BlogPostMapped.ordering))
         sql = buildLimits(sql, offset, limit)
         return self._trimmDeleted(sql.all())
 
@@ -98,7 +98,7 @@ class BlogPostServiceAlchemy(SessionSupport, IBlogPostService):
             else: sql = sql.filter(BlogPostMapped.PublishedOn == None)
         sql = sql.filter(BlogPostMapped.Author == None)
 
-        sql = sql.order_by(desc_op(BlogPostMapped.Order))
+        sql = sql.order_by(desc_op(BlogPostMapped.ordering))
         sql = buildLimits(sql, offset, limit)
         return self._trimmDeleted(sql.all())
 
@@ -109,7 +109,7 @@ class BlogPostServiceAlchemy(SessionSupport, IBlogPostService):
         assert q is None or isinstance(q, QBlogPost), 'Invalid query %s' % q
         sql = self._buildQuery(blogId, typeId, creatorId, authorId, q)
 
-        sql = sql.order_by(desc_op(BlogPostMapped.Order))
+        sql = sql.order_by(desc_op(BlogPostMapped.ordering))
         sql = buildLimits(sql, offset, limit)
         return self._trimmDeleted(sql.all())
 
@@ -175,22 +175,22 @@ class BlogPostServiceAlchemy(SessionSupport, IBlogPostService):
         '''
         @see: IBlogPostService.reorder
         '''
-        sql = self.session().query(BlogPostMapped.Order)
+        sql = self.session().query(BlogPostMapped.ordering)
         sql = sql.filter(BlogPostMapped.Blog == blogId)
         sql = sql.filter(BlogPostMapped.Id == refPostId)
         order = sql.scalar()
 
         if not order: raise InputError(Ref(_('Invalid before post')))
 
-        sql = self.session().query(BlogPostMapped.Order)
+        sql = self.session().query(BlogPostMapped.ordering)
         sql = sql.filter(BlogPostMapped.Blog == blogId)
         sql = sql.filter(BlogPostMapped.Id != postId)
         if before:
-            sql = sql.filter(BlogPostMapped.Order > order)
-            sql = sql.order_by(BlogPostMapped.Order)
+            sql = sql.filter(BlogPostMapped.ordering > order)
+            sql = sql.order_by(BlogPostMapped.ordering)
         else:
-            sql = sql.filter(BlogPostMapped.Order < order)
-            sql = sql.order_by(desc_op(BlogPostMapped.Order))
+            sql = sql.filter(BlogPostMapped.ordering < order)
+            sql = sql.order_by(desc_op(BlogPostMapped.ordering))
 
         sql = sql.limit(1)
 
@@ -206,8 +206,7 @@ class BlogPostServiceAlchemy(SessionSupport, IBlogPostService):
         post = self.getById(blogId, postId)
         assert isinstance(post, BlogPostMapped)
 
-        post.Order = order
-        post.CId = self._nextCId()
+        post.ordering = order
         self.session().merge(post)
 
     def delete(self, id):
@@ -273,6 +272,6 @@ class BlogPostServiceAlchemy(SessionSupport, IBlogPostService):
         '''
         Provides the next ordering.
         '''
-        max = self.session().query(fn.max(BlogPostMapped.Order)).filter(BlogPostMapped.Blog == blogId).scalar()
+        max = self.session().query(fn.max(BlogPostMapped.ordering)).filter(BlogPostMapped.Blog == blogId).scalar()
         if max: return max + 1
         return 1
