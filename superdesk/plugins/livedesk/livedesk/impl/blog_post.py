4cbb90999a529846f8607dd88ccd8fb636981061
Gabriel Nistor <gabriel.nistor@sourcefabric.org>
2012-07-18 16:18:44 +0300

    NR-227 : Add order change to the blog posts
    Added ordering to the blog posts.

--- superdesk/plugins/livedesk/livedesk/impl/blog_post.py
+++ superdesk/plugins/livedesk/livedesk/impl/blog_post.py
@@ -30,6 +30,7 @@ from superdesk.collaborator.meta.collaborator import CollaboratorMapped
 from superdesk.person.meta.person import PersonMapped
 from superdesk.post.api.post import IPostService, Post, QPostUnpublished
 from superdesk.post.meta.type import PostTypeMapped
+from sqlalchemy.sql.operators import desc_op
 
 # --------------------------------------------------------------------
 
@@ -70,10 +71,26 @@ class BlogPostServiceAlchemy(SessionSupport, IBlogPostService):
         sql = self._buildQuery(blogId, typeId, creatorId, authorId, q)
         sql = sql.filter(BlogPostMapped.PublishedOn != None)
 
+<<<<<<< HEAD
         sql = sql.order_by(BlogPostMapped.PublishedOn.desc())
         sqlLimit = buildLimits(sql, offset, limit)
         if detailed: return IterPart(sqlLimit.all(), sql.count(), offset, limit)
         return sqlLimit.all()
+=======
+        sql = sql.order_by(desc_op(BlogPostMapped.ordering))
+        sql = buildLimits(sql, offset, limit)
+        return self._trimmDeleted(sql.all())
+
+    def getPublishedCount(self, blogId, typeId=None, creatorId=None, authorId=None, q=None):
+        '''
+        @see: IBlogPostService.getPublishedCount
+        '''
+        assert q is None or isinstance(q, QBlogPostPublished), 'Invalid query %s' % q
+        sql = self._buildQuery(blogId, typeId, creatorId, authorId, q)
+        sql = sql.filter(BlogPostMapped.PublishedOn != None)
+
+        return sql.count()
+>>>>>>> NR-227 : Add order change to the blog posts
 
     def getUnpublished(self, blogId, typeId=None, creatorId=None, authorId=None, offset=None, limit=None, q=None):
         '''
@@ -83,7 +100,11 @@ class BlogPostServiceAlchemy(SessionSupport, IBlogPostService):
         sql = self._buildQuery(blogId, typeId, creatorId, authorId, q)
         sql = sql.filter(BlogPostMapped.PublishedOn == None)
 
+<<<<<<< HEAD
         sql = sql.order_by(desc_op(BlogPostMapped.Order))
+=======
+        sql = sql.order_by(desc_op(BlogPostMapped.ordering))
+>>>>>>> NR-227 : Add order change to the blog posts
         sql = buildLimits(sql, offset, limit)
         return self._trimmDeleted(sql.all())
 
@@ -98,7 +119,11 @@ class BlogPostServiceAlchemy(SessionSupport, IBlogPostService):
             else: sql = sql.filter(BlogPostMapped.PublishedOn == None)
         sql = sql.filter(BlogPostMapped.Author == None)
 
+<<<<<<< HEAD
         sql = sql.order_by(desc_op(BlogPostMapped.Order))
+=======
+        sql = sql.order_by(desc_op(BlogPostMapped.ordering))
+>>>>>>> NR-227 : Add order change to the blog posts
         sql = buildLimits(sql, offset, limit)
         return self._trimmDeleted(sql.all())
 
@@ -109,7 +134,11 @@ class BlogPostServiceAlchemy(SessionSupport, IBlogPostService):
         assert q is None or isinstance(q, QBlogPost), 'Invalid query %s' % q
         sql = self._buildQuery(blogId, typeId, creatorId, authorId, q)
 
+<<<<<<< HEAD
         sql = sql.order_by(desc_op(BlogPostMapped.Order))
+=======
+        sql = sql.order_by(desc_op(BlogPostMapped.ordering))
+>>>>>>> NR-227 : Add order change to the blog posts
         sql = buildLimits(sql, offset, limit)
         return self._trimmDeleted(sql.all())
 
@@ -175,13 +204,18 @@ class BlogPostServiceAlchemy(SessionSupport, IBlogPostService):
         '''
         @see: IBlogPostService.reorder
         '''
+<<<<<<< HEAD
         sql = self.session().query(BlogPostMapped.Order)
+=======
+        sql = self.session().query(BlogPostMapped.ordering)
+>>>>>>> NR-227 : Add order change to the blog posts
         sql = sql.filter(BlogPostMapped.Blog == blogId)
         sql = sql.filter(BlogPostMapped.Id == refPostId)
         order = sql.scalar()
 
         if not order: raise InputError(Ref(_('Invalid before post')))
 
+<<<<<<< HEAD
         sql = self.session().query(BlogPostMapped.Order)
         sql = sql.filter(BlogPostMapped.Blog == blogId)
         sql = sql.filter(BlogPostMapped.Id != postId)
@@ -191,6 +225,17 @@ class BlogPostServiceAlchemy(SessionSupport, IBlogPostService):
         else:
             sql = sql.filter(BlogPostMapped.Order < order)
             sql = sql.order_by(desc_op(BlogPostMapped.Order))
+=======
+        sql = self.session().query(BlogPostMapped.ordering)
+        sql = sql.filter(BlogPostMapped.Blog == blogId)
+        sql = sql.filter(BlogPostMapped.Id != postId)
+        if before:
+            sql = sql.filter(BlogPostMapped.ordering > order)
+            sql = sql.order_by(BlogPostMapped.ordering)
+        else:
+            sql = sql.filter(BlogPostMapped.ordering < order)
+            sql = sql.order_by(desc_op(BlogPostMapped.ordering))
+>>>>>>> NR-227 : Add order change to the blog posts
 
         sql = sql.limit(1)
 
@@ -206,8 +251,12 @@ class BlogPostServiceAlchemy(SessionSupport, IBlogPostService):
         post = self.getById(blogId, postId)
         assert isinstance(post, BlogPostMapped)
 
+<<<<<<< HEAD
         post.Order = order
         post.CId = self._nextCId()
+=======
+        post.ordering = order
+>>>>>>> NR-227 : Add order change to the blog posts
         self.session().merge(post)
 
     def delete(self, id):
@@ -273,6 +322,10 @@ class BlogPostServiceAlchemy(SessionSupport, IBlogPostService):
         '''
         Provides the next ordering.
         '''
+<<<<<<< HEAD
         max = self.session().query(fn.max(BlogPostMapped.Order)).filter(BlogPostMapped.Blog == blogId).scalar()
+=======
+        max = self.session().query(fn.max(BlogPostMapped.ordering)).filter(BlogPostMapped.Blog == blogId).scalar()
+>>>>>>> NR-227 : Add order change to the blog posts
         if max: return max + 1
         return 1
