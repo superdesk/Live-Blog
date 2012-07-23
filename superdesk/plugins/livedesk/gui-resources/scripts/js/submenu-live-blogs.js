e88a526ac83fba73ab5e093ae5ff3b2db8ded2f6
Mihai Nistor <Mihai.Nistor@sourcefabric.org>
2012-07-23 19:30:00 +0300

    NR-221: Published posts reordering
    And more implementation on the timeline

--- superdesk/plugins/livedesk/gui-resources/scripts/js/submenu-live-blogs.js
+++ superdesk/plugins/livedesk/gui-resources/scripts/js/submenu-live-blogs.js
@@ -2,7 +2,7 @@ requirejs.config({
 	paths: { 
 		'providers': 'lib/livedesk/scripts/js/providers', 
 		'livedesk/models': 'lib/livedesk/scripts/js/models', 
-		'collections_livedesk': 'lib/livedesk/scripts/js/collections/' 
+		'livedesk/collections': 'lib/livedesk/scripts/js/collections/' 
 } });
 define
 ([
@@ -42,7 +42,7 @@ define
                             .done(function(action)
                             {
                                 action.ScriptPath && 
-                                    require([superdesk.apiUrl+action.ScriptPath], function(EditApp){ new EditApp(theBlog).render(); });
+                                    require([superdesk.apiUrl+action.ScriptPath], function(EditApp){ EditApp(theBlog); });
                             });
                         });
                 });
