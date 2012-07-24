47efad3556649bb250da1badc72b473316fb40fe
vlad.nicoara <vlad.nicoara@sourcefabric.org>
2012-07-24 18:19:41 +0300

    NR-201 : Liveblog embedding API
    Parsing of content to treat provider differences + misc

--- superdesk/plugins/livedesk/gui-resources/scripts/js/embed/livedeskembed.js
+++ superdesk/plugins/livedesk/gui-resources/scripts/js/embed/livedeskembed.js
@@ -1,39 +1,39 @@
 var livedeskEmbed = {
     jqueryLoaded : false,
+    gizmoLoaded : false,
     waited : 0,
     waitTime : 20, //miliseconds
     init : function() {
+        
+        //make sure jsLib is loaded
         if (typeof jQuery == 'undefined') {
             if(this.jqueryLoaded == false) {
-                this.loadJquery(document, 'script', 'livedesk-jquery');
-            }
-            console.log('Waiting ' + this.waitTime);
-            console.log(this.waited);
-            this.waited += 1;
-            
-            if(this.waited > 50) {
-                throw('waited too much');
+                this.loadScript(document, "http://code.jquery.com/jquery-1.7.2.min.js", 'livedesk-jquery');
             }
             setTimeout('livedeskEmbed.init()', this.waitTime);
-        } else {
-            this.startLoading();
+        } else {   
+            if (typeof $.gizmo == 'undefined') {
+                if(this.gizmoLoaded == false) {
+                    this.loadScript(document, "gizmo.js", 'livedesk-gizmo');
+                }
+                setTimeout('livedeskEmbed.init()', this.waitTime);
+            } else {
+                this.startLoading();
+            }
         }
     },
-    loadJquery : function(d, s, id) {
-            var js, fjs = d.getElementsByTagName(s)[1];
+    loadScript : function(d, src, id) {
+            
+            var js, fjs = d.getElementsByTagName('script')[1];
             if (d.getElementById(id)) return;
-            js = d.createElement(s);js.id = id;
-            js.src = "http://code.jquery.com/jquery-1.7.2.min.js";
+            js = d.createElement('script');js.id = id;
+            js.src = src;
             fjs.parentNode.insertBefore(js, fjs);
             this.jqueryLoaded = true;
-            console.log('loading jQuery...');
+            console.log('loading ' + src);
             this.init();
     },
-    
     startLoading : function() {
-        //console.log('starting to load');
-        //$('#loader-smack').html('merge jQuery');
-        
         var 
         User = $.gizmo.Model.extend({}),
         Post = $.gizmo.Model.extend
@@ -41,6 +41,98 @@ var livedeskEmbed = {
             defaults:
             {
                 Creator: User
+            },
+            services: {
+                'flickr': true,
+                'google': true,
+                'twitter': true,
+                'facebook': true,
+                'youtube': true
+            },
+            /**
+            * Get css class based on type
+            *
+            * @return {string}
+            */
+            getClass: function() {
+                switch (this.get('Type').Key) {
+                    case 'wrapup':
+                        return 'wrapup';
+                        break;
+
+                    case 'quote':
+                        return 'quotation';
+                        break;
+
+                    case 'advertisement':
+                        return 'advertisement';
+                        break;
+
+                    default:
+                        if (this.isService()) {
+                            return 'service';
+                        }
+
+                        return 'tw';
+                }
+            },
+            /**
+            * Test if post is from service
+            *
+            * @return {bool}
+            */
+            isService: function() {
+                return this.get('AuthorName') in this.services;
+            },
+
+            /**
+            * Test if post is quote
+            *
+            * @return {bool}
+            */
+            isQuote: function() {
+                return this.getClass() == 'quotation';
+            },
+            twitter: {
+                link: {
+                    anchor: function(str) 
+                    {
+                        return str.replace(/[A-Za-z]+:\/\/[A-Za-z0-9-_]+\.[A-Za-z0-9-_:%&\?\/.=]+/g, function(m) 
+                        {
+                            m = m.link(m);
+                            m = m.replace('href="','target="_blank" href="');
+                            return m;
+                        });
+                    },
+                    user: function(str) 
+                    {
+                        return str.replace(/[@]+[A-Za-z0-9-_]+/g, function(us) 
+                        {
+                            var username = us.replace("@","");
+				
+                            us = us.link("http://twitter.com/"+username);
+                            us = us.replace('href="','target="_blank" onclick="loadProfile(\''+username+'\');return(false);"  href="');
+                            return us;
+                        });
+                    },
+                    tag: function(str) 
+                    {
+                        return str.replace(/[#]+[A-Za-z0-9-_]+/g, function(t) 
+                        {
+                            var tag = t.replace(" #"," %23");
+                            t = t.link("http://summize.com/search?q="+tag);
+                            t = t.replace('href="','target="_blank" href="');
+                            return t;
+                        });
+                    },
+                    all: function(str)
+                    {
+                        str = this.anchor(str);
+                        str = this.user(str);
+                        str = this.tag(str);
+                        return str;
+                    }
+                }
             }
         }),
         
@@ -56,19 +148,37 @@ var livedeskEmbed = {
             }
         });
         
-        var 
+        var i=0,
         PostItemView = $.gizmo.View.extend
         ({
-            init: function(href)
+            init: function()
             {
-                this.post = new Post(href);
-                return this.render();
+                var self = this;
+                this.post.on('update', this.render, this);
+                this.post.on('read', this.render, this);
+                this.post.sync();
             },
             render: function()
             {
-                // pus in ceva html 
-                console.log(this.post.get('Content') );
-                return '<div style="display:block">' + this.post.get('Content') + '</div>';
+                var content = this.post.get('Content');                
+                var style= '';                
+                if (this.post.getClass() == 'wrapup') {
+                    //$(this.el).addClass('open');
+                    style += ' open';
+                }
+                if (this.post.isService()) {
+                    style += ' ' + this.post.get('AuthorName');
+                    if (this.post.get('AuthorName') == 'flickr') {
+                        var paddedContent = '<span>' + content + '</span>';
+                        var jqo = $(paddedContent);
+                        jqo.find('img').attr('src', jqo.find('a').attr('href'));
+                        content = jqo.html();
+                    } else if (this.post.get('AuthorName') == 'twitter') {
+                        content = this.post.twitter.link.all(content);
+                    }
+                }
+                var template = '<li class="'+ style +'">' + content + '</li>';
+                this.el.replaceWith( template );
             }
         }),
         TimelineView = $.gizmo.View.extend
@@ -77,14 +187,13 @@ var livedeskEmbed = {
             {
                 '[uberclick="ceva-click-shucar"]': {'click': 'altceva'}
             },
-            init: function(el)
+            init: function()
             {
-                this.el = el;
                 this.blog = new Blog('http://localhost:8080/resources/LiveDesk/Blog/1');
                 var self = this;
                 this.blog.on('read', function()
                 { 
-                    self.blog.get('Post').on('read', function(){ self.render(); })
+                    self.blog.get('Post').on('read', function(){self.render();})
                     self.blog.get('Post').xfilter('*').sync();
                 });
                 
@@ -92,16 +201,18 @@ var livedeskEmbed = {
             },
             render: function()
             {
-                var self = this;
+                var self = this,i=0;
                 this.blog.get('Post').each(function()
                 {
-                    self.el.append( (new PostItemView).init(this.hash()) )
+                    
+                    var post = new Post(this.hash()), view = new PostItemView({ post: post }); 
+                    self.el.append( view.el );
                 })
             }
             
         });
         
-        (new TimelineView).init($('#livedesk-root'));
+        new TimelineView({ el: '#livedesk-root'});
     }
 };
 livedeskEmbed.init();
