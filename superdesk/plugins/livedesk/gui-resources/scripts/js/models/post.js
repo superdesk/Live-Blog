44b53a38bb8f7c5e9ca3793f5999c53c737c75d1
Mihai Nistor <Mihai.Nistor@sourcefabric.org>
2012-07-26 18:26:47 +0300

    NR-221: Published posts reordering
    Added Order default
    Made little changes to the request changest to gizmo!

--- superdesk/plugins/livedesk/gui-resources/scripts/js/models/post.js
+++ superdesk/plugins/livedesk/gui-resources/scripts/js/models/post.js
@@ -2,6 +2,19 @@ define([ 'gizmo/superdesk'],
 function(Gizmo)
 {
     // Post
-    return Gizmo.Model.extend({});
-    
+	return Gizmo.AuthModel.extend({
+		url: new Gizmo.Url('/Post')
+	}, { register: 'Post' } );
+	/*
+    return Gizmo.AuthModel.extend
+    ({ 
+		url: new Gizmo.Url('/Post'),
+		defaults:
+        { 
+            Author: Collaborator,
+            Blog: Blog,
+            Creator: User,
+            PostType: PostType
+        }
+    }, { register: 'Post' } );*/
 });
