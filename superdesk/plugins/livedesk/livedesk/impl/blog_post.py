21a7b24e11179e0fd1c157f158b590100d14ff37
Mihai Nistor <Mihai.Nistor@sourcefabric.org>
2012-07-26 18:26:47 +0300

    NR-221: Published posts reordering
    Added Order default
    Made little changes to the request changest to gizmo!

--- superdesk/plugins/livedesk/livedesk/impl/blog_post.py
+++ superdesk/plugins/livedesk/livedesk/impl/blog_post.py
@@ -146,7 +146,7 @@ class BlogPostServiceAlchemy(SessionSupport, IBlogPostService):
 
         postEntry = BlogPostEntry(Blog=blogId, blogPostId=post.Id)
         postEntry.CId = self._nextCId()
-        postEntry.ordering = self._nextOrdering(blogId)
+        postEntry.Order = self._nextOrdering(blogId)
         self.session().merge(postEntry)
 
         return postId
@@ -161,7 +161,7 @@ class BlogPostServiceAlchemy(SessionSupport, IBlogPostService):
 
         postEntry = BlogPostEntry(Blog=blogId, blogPostId=self.postService.insert(post))
         postEntry.CId = self._nextCId()
-        postEntry.ordering = self._nextOrdering(blogId)
+        postEntry.Order = self._nextOrdering(blogId)
         self.session().add(postEntry)
 
         return postEntry.blogPostId
