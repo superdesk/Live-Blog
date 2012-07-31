aa1fc066b1bdac42f1142ab4465af9e3faff9ff0
Mihai Nistor <Mihai.Nistor@sourcefabric.org>
2012-07-31 13:26:28 +0300

    NR-226: Create login functionality in Ally framework
    Bugs fixed and many other!

--- superdesk/plugins/livedesk/gui-resources/scripts/js/models/post.js
+++ superdesk/plugins/livedesk/gui-resources/scripts/js/models/post.js
@@ -3,7 +3,16 @@ function(Gizmo)
 {
     // Post
 	return Gizmo.AuthModel.extend({
-		url: new Gizmo.Url('/Post')
+		url: new Gizmo.Url('/Post'),
+		order: function(id, before){
+			var reorderHref = this.href+'/Post/'+id+'/Reorder?before='+before;
+			console.log(reorderHref);
+			var
+				self = this,  
+				dataAdapter = function(){ return self.syncAdapter.request.apply(self.syncAdapter, arguments); },
+                ret = dataAdapter(reorderHref).update();
+			return ret;
+		}
 	}, { register: 'Post' } );
 	/*
     return Gizmo.AuthModel.extend
