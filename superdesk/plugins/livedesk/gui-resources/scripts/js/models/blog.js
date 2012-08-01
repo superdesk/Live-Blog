838423c7585dd2d11db355bad27f9e5ceb43b73c
Mihai Balaceanu <gizm0bill@gmail.com>
2012-08-01 12:39:46 +0300

    NR-230 : Collaborators provider
    w.i.p. + gizmo.js edits + eggs rebuilt

--- superdesk/plugins/livedesk/gui-resources/scripts/js/models/blog.js
+++ superdesk/plugins/livedesk/gui-resources/scripts/js/models/blog.js
@@ -1,15 +1,15 @@
 define([ 'gizmo/superdesk', 
-/*    config.guiJs('livedesk', 'models/user'),
+    config.guiJs('livedesk', 'models/user'),
     config.guiJs('livedesk', 'models/language'),
     config.guiJs('livedesk', 'models/posts'),
-    config.guiJs('livedesk', 'models/collaborators')*/],
-    
-function(Gizmo, User, Language, Posts, Collaborators) {
+    config.guiJs('livedesk', 'models/collaborators')],
+function(Gizmo, User, Language, Posts, Collaborators) 
+{
     // Blog
-    return Gizmo.AuthModel.extend
+    return Gizmo.Model.extend
     ({
 		url: new Gizmo.Url('Blog'),
-/*        defaults:
+        defaults:
         { 
             Creator: User,
             Language: Language,
@@ -18,7 +18,6 @@ function(Gizmo, User, Language, Posts, Collaborators) {
             PostPublished: Posts,
             PostUnpublished: Posts
         }
-*/		
     }, 
     { register: 'Blog' } );
 });
