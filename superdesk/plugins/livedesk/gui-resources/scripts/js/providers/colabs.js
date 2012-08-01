6551068a248a70fe5fae2cf27be75021b322f2f2
Mihai Balaceanu <gizm0bill@gmail.com>
2012-08-01 18:21:14 +0300

    NR-230 : Collaborators provider
    Changes to some models, now with auth decorator

--- superdesk/plugins/livedesk/gui-resources/scripts/js/providers/colabs.js
+++ superdesk/plugins/livedesk/gui-resources/scripts/js/providers/colabs.js
@@ -154,10 +154,10 @@ function(providers, $, giz, Blog, Collaborator)
                     colab.get('Post').xfilter('*')
                         .on('read', function()
                         { 
+                            // list of new posts to append
                             var appendPosts = [];
                             this.each(function()
                             {
-                                
                                 if( $.inArray( this.get('Id'), colab._viewModels ) === -1 )
                                 {
                                     appendPosts.push(this);
@@ -166,7 +166,6 @@ function(providers, $, giz, Blog, Collaborator)
                                 //console.log(this, Math.max(colab._latestPost, parseInt(this.get('CId'))));
                                 colab._latestPost = Math.max(colab._latestPost, parseInt(this.get('CId')));
                             });
-                            
                             updateItemCount += appendPosts.length;
                             
                             appendPosts.length && $('.new-results', self.el).trigger('update.livedesk', [updateItemCount, function()
