f3fe77b57488fd01e9f0f8730796c7d4656f487f
Mihai Nistor <Mihai.Nistor@sourcefabric.org>
2012-08-02 13:58:57 +0300

    NR-226: Create login functionality in Ally framework
    Added events on edit js and auth models!

--- superdesk/plugins/livedesk/gui-resources/scripts/js/providers/colabs.js
+++ superdesk/plugins/livedesk/gui-resources/scripts/js/providers/colabs.js
@@ -21,10 +21,8 @@ function(providers, $, giz, Blog, Collaborator)
     ({
         init: function()
         {
-            var self = this;
-			console.log(this.model._clientId);
-            this.model.on('update', function(){ self.render(true); });
-			this.model.on('read', function(){ console.log('read');self.render(true); });
+            this.model.on('update', this.render, this);
+			this.model.on('read', this.render, this);
         },
         render: function()
         {
