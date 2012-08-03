b10e016da13ae08c850f606da45f90d7baf0b7b6
Gabriel Nistor <gabriel.nistor@sourcefabric.org>
2012-08-03 22:36:08 +0300

    NR-148 : Meta refactor
    Made the extension for iterators and also the redirect. Made all the plugins to propertly work with the refactored version. Still need to finalize decoding.

--- plugins/internationalization/internationalization/impl/message.py
+++ plugins/internationalization/internationalization/impl/message.py
@@ -14,6 +14,7 @@ from ..meta.message import Message
 from internationalization.meta.source import Source
 from sql_alchemy.impl.entity import EntityGetCRUDServiceAlchemy
 from ally.support.sqlalchemy.util_service import buildQuery, buildLimits
+from ally.api.extension import IterPart
 
 # --------------------------------------------------------------------
 
@@ -25,61 +26,36 @@ class MessageServiceAlchemy(EntityGetCRUDServiceAlchemy, IMessageService):
     def __init__(self):
         EntityGetCRUDServiceAlchemy.__init__(self, Message, QMessage)
 
-    def getMessagesCount(self, sourceId=None, qm=None, qs=None):
-        '''
-        @see: IMessageService.getMessagesCount
-        '''
-        sqlQuery = self.session().query(Message)
-        if sourceId: sqlQuery = sqlQuery.filter(Message.Source == sourceId)
-        if qm: sqlQuery = buildQuery(sqlQuery, qm, Message)
-        if qs: sqlQuery = buildQuery(sqlQuery.join(Source), qs, Source)
-        return sqlQuery.count()
-
-    def getMessages(self, sourceId=None, offset=None, limit=None, qm=None, qs=None):
+    def getMessages(self, sourceId=None, offset=None, limit=None, detailed=False, qm=None, qs=None):
         '''
         @see: IMessageService.getMessages
         '''
-        sqlQuery = self.session().query(Message)
-        if sourceId: sqlQuery = sqlQuery.filter(Message.Source == sourceId)
-        if qm: sqlQuery = buildQuery(sqlQuery, qm, Message)
-        if qs: sqlQuery = buildQuery(sqlQuery.join(Source), qs, Source)
-        sqlQuery = buildLimits(sqlQuery, offset, limit)
-        return sqlQuery.all()
+        sql = self.session().query(Message)
+        if sourceId: sql = sql.filter(Message.Source == sourceId)
+        if qm: sql = buildQuery(sql, qm, Message)
+        if qs: sql = buildQuery(sql.join(Source), qs, Source)
+        sqlLimit = buildLimits(sql, offset, limit)
+        if detailed: return IterPart(sqlLimit.all(), sql.count(), offset, limit)
+        return sqlLimit.all()
 
-    def getComponentMessagesCount(self, component, qm=None, qs=None):
-        '''
-        @see: IMessageService.getComponentMessagesCount
-        '''
-        sqlQuery = self.session().query(Message).join(Source).filter(Source.Component == component)
-        if qm: sqlQuery = buildQuery(sqlQuery, qm, Message)
-        if qs: sqlQuery = buildQuery(sqlQuery, qs, Source)
-        return sqlQuery.count()
-
-    def getComponentMessages(self, component, offset=None, limit=None, qm=None, qs=None):
+    def getComponentMessages(self, component, offset=None, limit=None, detailed=False, qm=None, qs=None):
         '''
         @see: IMessageService.getComponentMessages
         '''
-        sqlQuery = self.session().query(Message).join(Source).filter(Source.Component == component)
-        if qm: sqlQuery = buildQuery(sqlQuery, qm, Message)
-        if qs: sqlQuery = buildQuery(sqlQuery, qs, Source)
-        sqlQuery = buildLimits(sqlQuery, offset, limit)
-        return sqlQuery.all()
+        sql = self.session().query(Message).join(Source).filter(Source.Component == component)
+        if qm: sql = buildQuery(sql, qm, Message)
+        if qs: sql = buildQuery(sql, qs, Source)
+        sqlLimit = buildLimits(sql, offset, limit)
+        if detailed: return IterPart(sqlLimit.all(), sql.count(), offset, limit)
+        return sqlLimit.all()
 
-    def getPluginMessagesCount(self, plugin, qm=None, qs=None):
-        '''
-        @see: IMessageService.getPluginMessagesCount
-        '''
-        sqlQuery = self.session().query(Message).join(Source).filter(Source.Plugin == plugin)
-        if qm: sqlQuery = buildQuery(sqlQuery, qm, Message)
-        if qs: sqlQuery = buildQuery(sqlQuery, qs, Source)
-        return sqlQuery.count()
-
-    def getPluginMessages(self, plugin, offset=None, limit=None, qm=None, qs=None):
+    def getPluginMessages(self, plugin, offset=None, limit=None, detailed=False, qm=None, qs=None):
         '''
         @see: IMessageService.getPluginMessages
         '''
-        sqlQuery = self.session().query(Message).join(Source).filter(Source.Plugin == plugin)
-        if qm: sqlQuery = buildQuery(sqlQuery, qm, Message)
-        if qs: sqlQuery = buildQuery(sqlQuery, qs, Source)
-        sqlQuery = buildLimits(sqlQuery, offset, limit)
-        return sqlQuery.all()
+        sql = self.session().query(Message).join(Source).filter(Source.Plugin == plugin)
+        if qm: sql = buildQuery(sql, qm, Message)
+        if qs: sql = buildQuery(sql, qs, Source)
+        sqlLimit = buildLimits(sql, offset, limit)
+        if detailed: return IterPart(sqlLimit.all(), sql.count(), offset, limit)
+        return sqlLimit.all()
