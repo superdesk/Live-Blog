6b1bbe26aded9db8379a51e6079273cd27a694d0
ioan <ioan.pocol@sourcefabric.org>
2012-08-07 15:23:10 +0300

    NR-233 : Media Archive - upload functionality

--- superdesk/plugins/media-archive-image/superdesk/media_archive/impl/image_persist.py
+++ superdesk/plugins/media-archive-image/superdesk/media_archive/impl/image_persist.py
@@ -15,7 +15,7 @@ from ..meta.image_data import ImageData
 from ..meta.image_info import ImageInfo
 from ..meta.meta_data import MetaDataMapped
 from ..meta.meta_type import MetaTypeMapped
-from .meta_data import IMetaDataReferenceHandler
+from superdesk.media_archive.core.spec import IMetaDataHandler
 from ally.api.model import Content
 from ally.container import wire
 from ally.container.ioc import injected
@@ -35,7 +35,7 @@ import os
 # --------------------------------------------------------------------
 
 @injected
-class ImagePersistanceService(IImagePersistanceService, IMetaDataReferenceHandler, SessionSupport):
+class ImagePersistanceService(IImagePersistanceService, IMetaDataHandler, SessionSupport):
     '''
     Provides the service that handles the @see: IImagePersistanceService.
     '''
@@ -79,7 +79,7 @@ class ImagePersistanceService(IImagePersistanceService, IMetaDataReferenceHandle
 
         self._metaTypeId = None
 
-    def insert(self, imageInfo, image):
+    def insertAll(self, imageInfo, image):
         '''
         @see: IImagePersistanceService.insert
         '''
@@ -122,7 +122,7 @@ class ImagePersistanceService(IImagePersistanceService, IMetaDataReferenceHandle
 
     def process(self, metaData, scheme, thumbSize):
         '''
-        @see: IMetaDataReferenceHandler.process
+        @see: IMetaDataHandler.process
         '''
         assert isinstance(metaData, MetaDataMapped), 'Invalid meta data %s' % metaData
         try:
@@ -147,3 +147,11 @@ class ImagePersistanceService(IImagePersistanceService, IMetaDataReferenceHandle
                 self.session().flush((metaType,))
             self._metaTypeId = metaType.id
         return self._metaTypeId
+
+    # ----------------------------------------------------------------
+
+    def deploy(self):
+        '''
+           Deploy 
+        '''
+        pass
