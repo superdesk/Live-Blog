7b8d82d4458386ac1d9a91bff154de8808e95d3e
Mihai Nistor <Mihai.Nistor@sourcefabric.org>
2012-08-08 20:11:48 +0300

    NR-224: Core implementation of MVC framework - part 2
    Added set event,
    read event on collection send newly added models
    minor ie issues fixed

--- start.bat
+++ start.bat
@@ -1,4 +1,5 @@
 @ECHO OFF
 setlocal
 set PYTHONPATH=superdesk/plugins/livedesk;plugins/gui-core;plugins/test-dev;plugins/gui-action;components/ally-authentication-http/;
+del superdesk\distribution\workspace\superdesk.db
 python.exe superdesk/distribution/application.py
