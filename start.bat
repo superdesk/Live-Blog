868660084883b1620c19ccac26f473b6892fe4ef
Mihai Nistor <Mihai.Nistor@sourcefabric.org>
2012-08-13 18:51:45 +0300

    NR-224: Core implementation of MVC framework - part 2
    Eggs Layed

--- start.bat
+++ start.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
 setlocal
-set PYTHONPATH=superdesk/plugins/livedesk;plugins/gui-core;plugins/test-dev;plugins/gui-action;components/ally-authentication-http/;superdesk/plugins/media-archive;
+set PYTHONPATH=superdesk/plugins/livedesk;plugins/gui-core;plugins/test-dev;plugins/gui-action;components/ally-authentication-http/;components/support-cdm;superdesk/plugins/media-archive;
 del superdesk\distribution\workspace\superdesk.db
 python.exe superdesk/distribution/application.py
