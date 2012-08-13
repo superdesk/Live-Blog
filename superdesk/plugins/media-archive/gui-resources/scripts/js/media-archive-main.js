bc9a4dbd14e2608303c6059a545b6ad8e0476552
Mihai Balaceanu <gizm0bill@gmail.com>
2012-08-13 16:29:04 +0300

    NR-35 : Media Archive user interface implementation
    Initial actions and script files

---
+++ superdesk/plugins/media-archive/gui-resources/scripts/js/media-archive-main.js
@@ -0,0 +1,9 @@
+define([], function()
+{
+//    var ItemView,
+//        MainView,
+//        SideView,
+//        ScratchpadView,
+    var MediaArchive = function(){ console.log('hello media archive'); }
+    return MediaArchive;
+});
