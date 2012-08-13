9eb92b9b8b81487e060e2432f3c33f5175de1e36
Mihai Nistor <Mihai.Nistor@sourcefabric.org>
2012-08-13 17:48:13 +0300

    NR-194: Adding links to published posts not possible
    Embed changes

--- superdesk/plugins/livedesk/gui-resources/scripts/js/embed/livedeskembed.js
+++ superdesk/plugins/livedesk/gui-resources/scripts/js/embed/livedeskembed.js
@@ -1,159 +1,150 @@
-function isOnly(data,key) {
-	var count = 0;
-	for(i in data) {
-		count++;
-		if(count>1) return false;
-	};
-	return (data !== undefined) && (data[key] !== undefined) && (count == 1);
-}
-
 var livedeskEmbed = {
-    init : function(theBlog) {
-        var self = this;
-        if (typeof jQuery == 'undefined') {
-            self.loadScript('http://code.jquery.com/jquery-1.7.2.min.js', function(){
-                if (typeof $.gizmo == 'undefined') {
-                    self.loadScript('gizmo.js', function(){
-                        self.startLoading();
-                    })
-                }
-            })
-        } else {
-            if (typeof $.gizmo == 'undefined') {
-                self.loadScript('gizmo.js', function(){
-                    self.startLoading();
-                })
-            } else {
-                self.startLoading();
-            }
-        }
-    },
-    
-    loadScript : function (src, callback) {
-        var script = document.createElement("script")
-        script.type = "text/javascript";
-        if (script.readyState) { //IE
-            script.onreadystatechange = function () {
-                if (script.readyState == "loaded" || script.readyState == "complete") {
-                    script.onreadystatechange = null;
-                    callback();
-                }
-            };
-        } else { //Others
-            script.onload = function () {
-                callback();
-            };
-        }
-        script.src = src;
-        document.getElementsByTagName("head")[0].appendChild(script);
-    },
-    startLoading : function() {
-        var 
-        User = $.gizmo.Model.extend({}),
+	init : function() {
+		var self = this;
+		if (typeof jQuery == 'undefined') {
+			self.loadScript('http://code.jquery.com/jquery-1.7.2.min.js', function(){
+				if (typeof $.gizmo == 'undefined') {
+					self.loadScript('gizmo.js', function(){
+						self.startLoading();
+					})
+				}
+			})
+		} else {
+			if (typeof $.gizmo == 'undefined') {
+				self.loadScript('gizmo.js', function(){
+					self.startLoading();
+				})
+			} else {
+				self.startLoading();
+			}
+		}
+	},
+	
+	loadScript : function (src, callback) {
+		var script = document.createElement("script")
+		script.type = "text/javascript";
+		if (script.readyState) { //IE
+			script.onreadystatechange = function () {
+				if (script.readyState == "loaded" || script.readyState == "complete") {
+					script.onreadystatechange = null;
+					callback();
+				}
+			};
+		} else { //Others
+			script.onload = function () {
+				callback();
+			};
+		}
+		script.src = src;
+		document.getElementsByTagName("head")[0].appendChild(script);
+	},
+	startLoading : function() {
+		var 
+		User = $.gizmo.Model.extend({}),
 /*		PostType = $.gizmo.Model.extend({}),
 		AuthorPerson = $.gizmo.Model.extend({}),
 		PostBlog = $.gizmo.Model.extend({}),
 		Author = $.gizmo.Model.extend({}),
 */		
-        Post = $.gizmo.Model.extend
-        ({
-            defaults:
-            {
-                Creator: User
-            },
-            services: {
-                'flickr': true,
-                'google': true,
-                'twitter': true,
-                'facebook': true,
-                'youtube': true
-            },
-            /**
-            * Get css class based on type
-            *
-            * @return {string}
-            */
-            getClass: function() {
-                switch (this.get('Type').Key) {
-                    case 'wrapup':
-                        return 'wrapup';
-                        break;
+		Post = $.gizmo.Model.extend
+		({
+			defaults:
+			{
+				Creator: User
+			},
+			services: {
+				'flickr': true,
+				'google': true,
+				'twitter': true,
+				'facebook': true,
+				'youtube': true
+			},
+			/**
+			* Get css class based on type
+			*
+			* @return {string}
+			*/
+			getClass: function() {
+				switch (this.get('Type').Key) {
+					case 'wrapup':
+						return 'wrapup';
+						break;
 
-                    case 'quote':
-                        return 'quotation';
-                        break;
+					case 'quote':
+						return 'quotation';
+						break;
 
-                    case 'advertisement':
-                        return 'advertisement';
-                        break;
+					case 'advertisement':
+						return 'advertisement';
+						break;
 
-                    default:
-                        if (this.isService()) {
-                            return 'service';
-                        }
+					default:
+						if (this.isService()) {
+							return 'service';
+						}
 
-                        return 'tw';
-                }
-            },
-            /**
-            * Test if post is from service
-            *
-            * @return {bool}
-            */
-            isService: function() {
-                return this.get('AuthorName') in this.services;
-            },
+						return 'tw';
+				}
+			},
+			/**
+			* Test if post is from service
+			*
+			* @return {bool}
+			*/
+			isService: function() {
+				return this.get('AuthorName') in this.services;
+			},
 
-            /**
-            * Test if post is quote
-            *
-            * @return {bool}
-            */
-            isQuote: function() {
-                return this.getClass() == 'quotation';
-            },
-            twitter: {
-                link: {
-                    anchor: function(str) 
-                    {
-                        return str.replace(/[A-Za-z]+:\/\/[A-Za-z0-9-_]+\.[A-Za-z0-9-_:%&\?\/.=]+/g, function(m) 
-                        {
-                            m = m.link(m);
-                            m = m.replace('href="','target="_blank" href="');
-                            return m;
-                        });
-                    },
-                    user: function(str) 
-                    {
-                        return str.replace(/[@]+[A-Za-z0-9-_]+/g, function(us) 
-                        {
-                            var username = us.replace("@","");
+			/**
+			* Test if post is quote
+			*
+			* @return {bool}
+			*/
+			isQuote: function() {
+				return this.getClass() == 'quotation';
+			},
+			twitter: {
+				link: {
+					anchor: function(str) 
+					{
+						return str.replace(/[A-Za-z]+:\/\/[A-Za-z0-9-_]+\.[A-Za-z0-9-_:%&\?\/.=]+/g, function(m) 
+						{
+							m = m.link(m);
+							m = m.replace('href="','target="_blank" href="');
+							return m;
+						});
+					},
+					user: function(str) 
+					{
+						return str.replace(/[@]+[A-Za-z0-9-_]+/g, function(us) 
+						{
+							var username = us.replace("@","");
 				
-                            us = us.link("http://twitter.com/"+username);
-                            us = us.replace('href="','target="_blank" onclick="loadProfile(\''+username+'\');return(false);"  href="');
-                            return us;
-                        });
-                    },
-                    tag: function(str) 
-                    {
-                        return str.replace(/[#]+[A-Za-z0-9-_]+/g, function(t) 
-                        {
-                            var tag = t.replace(" #"," %23");
-                            t = t.link("http://summize.com/search?q="+tag);
-                            t = t.replace('href="','target="_blank" href="');
-                            return t;
-                        });
-                    },
-                    all: function(str)
-                    {
-                        str = this.anchor(str);
-                        str = this.user(str);
-                        str = this.tag(str);
-                        return str;
-                    }
-                }
-            }
-        }),
+							us = us.link("http://twitter.com/"+username);
+							us = us.replace('href="','target="_blank" onclick="loadProfile(\''+username+'\');return(false);"  href="');
+							return us;
+						});
+					},
+					tag: function(str) 
+					{
+						return str.replace(/[#]+[A-Za-z0-9-_]+/g, function(t) 
+						{
+							var tag = t.replace(" #"," %23");
+							t = t.link("http://summize.com/search?q="+tag);
+							t = t.replace('href="','target="_blank" href="');
+							return t;
+						});
+					},
+					all: function(str)
+					{
+						str = this.anchor(str);
+						str = this.user(str);
+						str = this.tag(str);
+						return str;
+					}
+				}
+			}
+		}),
 		AutoCollection = $.gizmo.Collection.extend({
 			timeInterval: 10000,
 			idInterval: 0,
@@ -187,27 +178,27 @@ var livedeskEmbed = {
 				this.auto().pause().setIdInterval(function(){self.auto();});
 			}
 		});
-        Posts = AutoCollection.extend({
-            model: Post
-        }),
-        
-        Blog = $.gizmo.Model.extend
-        ({
-            defaults: 
-            {
-                Post: Posts,
-                PostPublished: Posts,
-                PostUnpublished: Posts
-            }
-        });
-        
-        var i=0,
-        PostItemView = $.gizmo.View.extend
-        ({
-            init: function()
-            {
+		Posts = AutoCollection.extend({
+			model: Post
+		}),
+		
+		Blog = $.gizmo.Model.extend
+		({
+			defaults: 
+			{
+				Post: Posts,
+				PostPublished: Posts,
+				PostUnpublished: Posts
+			}
+		});
+		
+		var i=0,
+		PostItemView = $.gizmo.View.extend
+		({
+			init: function()
+			{
 				var self = this;
-                self.post
+				self.post
 					.on('read', self.render, self)
 					.on('update', function(evt, data){
 						if(isOnly(data, 'CId')) {
@@ -218,7 +209,7 @@ var livedeskEmbed = {
 					})
 					.on('delete', self.remove, self)
 					.sync();
-            },
+			},
 			remove: function()
 			{
 				var self = this;
@@ -233,8 +224,8 @@ var livedeskEmbed = {
 				if(this.prev !== undefined)
 					this.prev.next = this.next;				
 			},			
-            render: function()
-            {
+			render: function()
+			{
 				var self = this, order = parseFloat(self.post.get('Order'));
 				if ( !isNaN(self.order) && (order != self.order)) {
 					var actions = { prev: 'insertBefore', next: 'insertAfter' }, ways = { prev: 1, next: -1}, anti = { prev: 'next', next: 'prev'}
@@ -252,45 +243,47 @@ var livedeskEmbed = {
 					self.el[actions[dir]](cursor.el);
 				}
 				self.order = order;
-                var content = self.post.get('Content');
+				var content = self.post.get('Content');
 
-                var style= '';                
-                if (self.post.getClass() == 'wrapup') {
-                    style += ' open';
-                }
-                if (self.post.isService()) {
-                    style += ' ' + self.post.get('AuthorName');
-                    if (self.post.get('AuthorName') == 'flickr') {
-                        var paddedContent = '<span>' + content + '</span>';
-                        var jqo = $(paddedContent);
-                        jqo.find('img').attr('src', jqo.find('a').attr('href'));
-                        content = jqo.html();
-                    } else if (self.post.get('AuthorName') == 'twitter') {
-                        content = self.post.twitter.link.all(content);
-                    }
-                }
-                var template = '<li class="'+ style +'">' + content + '</li>';
-                self.setElement( template );
-            }
-        }),
-        TimelineView = $.gizmo.View.extend
-        ({
-            events:
-            {
-                '[uberclick="ceva-click-shucar"]': {
-                    'click': 'altceva'
-                }
-            },
-            init: function()
-            {
-                this.blog = new Blog(theBlog);
-                var self = this;
-                this.blog.on('read', function()
-                { 
-                    self.blog.get('PostPublished').on('read', self.render, self).xfilter('CId').sync();
-                });
-                this.blog.sync();
-            },
+				var style= '';                
+				if (self.post.getClass() == 'wrapup') {
+					style += ' open';
+				}
+				if (self.post.isService()) {
+					style += ' ' + self.post.get('AuthorName');
+					if (self.post.get('AuthorName') == 'flickr') {
+						var paddedContent = '<span>' + content + '</span>';
+						var jqo = $(paddedContent);
+						jqo.find('img').attr('src', jqo.find('a').attr('href'));
+						content = jqo.html();
+					} else if (self.post.get('AuthorName') == 'twitter') {
+						content = self.post.twitter.link.all(content);
+					}
+				}
+				var template = '<li class="'+ style +'">' + content + '</li>';
+				self.setElement( template );
+			}
+		}),
+		TimelineView = $.gizmo.View.extend
+		({
+			el: '#livedesk-root',
+			events:
+			{
+				'[uberclick="ceva-click-shucar"]': {
+					'click': 'altceva'
+				}
+			},
+			init: function()
+			{
+				var self = this;
+				if($.type(self.url) === 'string')
+					self.blog = new Blog(self.url);				
+				self.blog.on('read', function()
+				{ 
+					self.blog.get('PostPublished').on('read', self.render, self).xfilter('CId').sync();
+				})
+				.sync();
+			},
 			addOne: function(model)
 			{
 				current = new PostItemView({post: model, _parent: this});
@@ -309,22 +302,19 @@ var livedeskEmbed = {
 					this.addOne(data[i]);
 				}				
 			},			
-            render: function(evt, data)
-            {
+			render: function(evt, data)
+			{
 				if ( data === undefined)
 					data = this.blog.get('PostPublished')._list;
 				var next = this._latest, current, model, i = data.length;
 				while(i--) {
 					this.addOne(data[i]);
 				}
-            }
-            
-        });
-        
-        new TimelineView({
-            el: $('#livedesk-root')
-        });
-    },
+			}
+			
+		});
+		livedeskEmbed.TimelineView = TimelineView;
+		window.livedeskCallback(livedeskEmbed);
+	},
 };
 livedeskEmbed.init();
-
