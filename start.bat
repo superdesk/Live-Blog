20aa68d7217a1732e668d472b4b8bffc151b2f5e
Mihai Nistor <Mihai.Nistor@sourcefabric.org>
2012-08-13 16:04:29 +0300

    NR-194: Adding links to published posts not possible
    Egg adding

--- start.bat
+++ start.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
 setlocal
-set PYTHONPATH=superdesk/plugins/livedesk;plugins/gui-core;plugins/test-dev;plugins/gui-action;components/ally-authentication-http/;
+set PYTHONPATH=superdesk/plugins/livedesk;plugins/gui-core;plugins/test-dev;plugins/gui-action;components/ally-authentication-http/;superdesk/plugins/media-archive;
 del superdesk\distribution\workspace\superdesk.db
 python.exe superdesk/distribution/application.py
