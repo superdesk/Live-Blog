35a60bbc2fed452cea89973a7ee7297cde910576
ioan <ioan.pocol@sourcefabric.org>
2012-08-16 13:13:40 +0300

    NR-233 : Media Archive - upload functionality

--- superdesk/plugins/media-archive-image/__plugin__/media_archive_image/service.py
+++ superdesk/plugins/media-archive-image/__plugin__/media_archive_image/service.py
@@ -9,41 +9,31 @@ Created on Apr 25, 2012
 Contains the services setups for media image archive.
 '''
 
-from ..media_archive.service import cdmArchive, cdmThumbnail
 from ally.container import ioc
-from cdm.spec import ICDM
-from cdm.support import ExtendPathCDM
+from ..superdesk.db_superdesk import createTables
 from superdesk.media_archive.api.image_data import IImageDataService
 from superdesk.media_archive.api.image_persist import IImagePersistanceService
 from superdesk.media_archive.impl.image_data import ImageDataServiceAlchemy
 from superdesk.media_archive.impl.image_persist import ImagePersistanceService
 from superdesk.media_archive.core.spec import IMetaDataHandler
-from __plugin__.media_archive.service import thumbnail_sizes
 
 # --------------------------------------------------------------------
 
 @ioc.entity
-def cdmArchiveImage() -> ICDM:
-    return ExtendPathCDM(cdmArchive(), 'image/%s')
-
-@ioc.entity
-def cdmArchiveThumbnailImage() -> ICDM:
-    return ExtendPathCDM(cdmThumbnail(), 'image/%s')
-
-@ioc.entity
-def imagePersistance() -> IImagePersistanceService:
+def imagePersistanceService() -> IImagePersistanceService:
     b = ImagePersistanceService()
-    b.thumbnailSizes = thumbnail_sizes()
-    b.cdmImages = cdmArchiveImage()
-    b.cdmThumbnails = cdmArchiveThumbnailImage()
     return b
 
 @ioc.entity
 def imageDataHandler() -> IMetaDataHandler:
-    return imagePersistance()
+    return imagePersistanceService()
 
 @ioc.entity
 def imageData() -> IImageDataService:
     b = ImageDataServiceAlchemy()
     b.handler = imageDataHandler()
     return b
+
+@ioc.after(createTables)
+def deploy():
+    imagePersistanceService().deploy()
