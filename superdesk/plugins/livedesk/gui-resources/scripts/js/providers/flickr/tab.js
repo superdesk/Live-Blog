86f3378142be22aee44de466df591177188f6295
Mihai Balaceanu <gizm0bill@gmail.com>
2012-08-24 18:23:47 +0300

    NR-195 : Annotation field for third party content
    wip on flickr - broken

--- superdesk/plugins/livedesk/gui-resources/scripts/js/providers/flickr/tab.js
+++ superdesk/plugins/livedesk/gui-resources/scripts/js/providers/flickr/tab.js
@@ -1,11 +1,52 @@
-define('providers/flickr/tab', ['providers'], function(providers) {
-	providers.flickr = {
+define( 'providers/flickr/tab', 
+        ['providers', 'tmpl!livedesk>providers/flickr/post'], 
+function(providers) 
+{
+	providers.flickr = 
+	{
 		className: 'big-icon-flickr',		
-		init: function() {				
+		init: function() 
+		{				
 			require(['providers','providers/flickr'], function(providers) {
 				providers.flickr.init();
 			});
-		}
+		},
+	
+	    timeline: 
+	    {
+	        preData: $.noop,
+	        init: $.noop,
+	        save: $.noop,
+	        edit: $.noop,
+	        render: function()
+	        {
+	            var self = this,
+	                feed = {Post: this.model.feed()};
+	            
+	            try
+	            {
+	                feed.Post.Meta = JSON.parse(feed.Post.Meta);
+	                feed.Post.Meta.annotation = feed.Post.Meta.annotation[0];
+	            }
+	            catch(e)
+	            {
+	                eval('feed.Post.Meta = '+feed.Post.Meta);
+	            }
+	            console.log(feed);
+	            $.tmpl('livedesk>providers/flickr/post', {Post: feed}, function(e, o)
+                {
+                    self.setElement(o).el.find('.editable')
+                        ;
+                    
+                    $(self.el).on('click', '.btn.publish', function()
+                    {
+                    });
+                    $(self.el).on('click', '.btn.cancel', function()
+                    {
+                    });
+                });
+	        }
+	    }
 	};
 	return providers;
 });
