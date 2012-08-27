4a987180da7484c12fdbd4d527a71035ebe2f9e0
ioan <ioan.pocol@sourcefabric.org>
2012-08-27 11:38:19 +0300

    NR-232 : Media Archive - Video plugin

---
+++ superdesk/plugins/media-archive-video/__plugin__/media_archive_video/__init__.py
@@ -0,0 +1,20 @@
+'''
+Created on Aug 23, 2012
+
+@package: superdesk media archive video
+@copyright: 2012 Sourcefabric o.p.s.
+@license: http://www.gnu.org/licenses/gpl-3.0.txt
+@author: Ioan v. Pocol
+
+Contains the setup files for superdesk media archive handling for video media.
+'''
+
+# --------------------------------------------------------------------
+
+NAME = 'Media archive video'
+GROUP = 'Superdesk media archive'
+VERSION = '1.0'
+DESCRIPTION = \
+'''
+This plugin handles the video in the media archive. 
+'''
