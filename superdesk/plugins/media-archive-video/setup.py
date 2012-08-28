c647f9bbcc4fc6c74dbd5f2e424bc73c13b4eeb7
Mugur Rus <mugur.rus@sourcefabric.org>
2012-08-28 15:57:14 +0300

    NR-244 : Eggs are missing from superdesk distribution

--- superdesk/plugins/media-archive-video/setup.py
+++ superdesk/plugins/media-archive-video/setup.py
@@ -1,10 +1,10 @@
 '''
-Created on Aug 23, 2012
+Created on June 14, 2012
 
-@package: superdesk media archive video
+@package: Newscoop
 @copyright: 2012 Sourcefabric o.p.s.
 @license: http://www.gnu.org/licenses/gpl-3.0.txt
-@author: Ioan v. Pocol
+@author: Mugur Rus
 '''
 
 # --------------------------------------------------------------------
@@ -17,16 +17,19 @@ setup(
     name="media_archive_video",
     version="1.0",
     packages=find_packages(),
-    install_requires=['ally_api >= 1.0', 'ally_core_sqlalchemy >= 1.0', 'support_cdm >= 1.0',
-                      'internationalization >= 1.0', 'superdesk >= 1.0', 'media_archive >= 1.0'],
+    install_requires=['ally_api >= 1.0', 'ally_core_sqlalchemy >= 1.0', 'gui_action >= 1.0',
+                      'gui_core >= 1.0', 'internationalization >= 1.0'],
     platforms=['all'],
     zip_safe=True,
+    package_data={
+        '': ['*.gif', '*.png', '*.jpg', '*.jpeg', '*.js', '*.css', '*.less', '*.dust', '*.html'],
+    },
 
     # metadata for upload to PyPI
-    author="Ioan v. Pocol",
-    author_email="ioan.pocol@sourcefabric.org",
-    description="Video media archive plugin",
-    long_description='Implementation of the video media archive plugin',
+    author="Gabriel Nistor",
+    author_email="gabriel.nistor@sourcefabric.org",
+    description="Media archive video plugin",
+    long_description='Video files management functionality',
     license="GPL v3",
     keywords="Ally REST framework plugin Livedesk",
     url="http://www.sourcefabric.org/en/superdesk/", # project home page
