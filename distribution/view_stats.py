32126b5d161ac996110252e2b2280ee7c439f575
Mugur Rus <mugur.rus@sourcefabric.org>
2012-08-30 14:34:21 +0300

    updated package names

--- distribution/view_stats.py
+++ distribution/view_stats.py
@@ -1,7 +1,7 @@
 '''
 Created on Dec 20, 2011
 
-@package: Newscoop
+@package: Superdesk
 @copyright: 2011 Sourcefabric o.p.s.
 @license: http://www.gnu.org/licenses/gpl-3.0.txt
 @author: Gabriel Nistor
