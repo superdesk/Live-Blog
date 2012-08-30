621f26e94b2386011796f6243dbe9b9e2920da22
Mugur Rus <mugur.rus@sourcefabric.org>
2012-08-30 12:13:49 +0300

    NR-246 : Eggs should be rebuilt only on source changes

--- superdesk/plugins/copy_eggs.py
+++ superdesk/plugins/copy_eggs.py
@@ -8,9 +8,9 @@ Created on Aug 28, 2012
 '''
 
 from glob import glob
-from os.path import join, dirname, abspath, isdir
+from os.path import join, dirname, abspath, isdir, basename
 from shutil import copy
-from os import makedirs
+from os import makedirs, stat
 
 # --------------------------------------------------------------------
 
@@ -22,5 +22,8 @@ if __name__ == '__main__':
         dstDir = join(destDist, dir)
         if not isdir(dstDir): makedirs(dstDir)
         for egg in glob(join(sourceDist, dir, '*.egg')):
+            if stat(egg).st_mtime <= stat(join(dstDir, basename(egg))).st_mtime:
+                print('Egg is up to date: %s' % egg)
+                continue
             print('Copying %s\n     to %s' % (egg, dstDir))
             copy(egg, dstDir)
