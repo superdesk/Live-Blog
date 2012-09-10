8c6fd127c8dba1a07715b30fbde843d8f490f6a5
Mihai Balaceanu <gizm0bill@gmail.com>
2012-09-10 18:05:46 +0300

    NR-248 : URL management
    added some concepts and handlers in superdesk.navigation,
    made a custom implementation in livedesk submenu

--- superdesk/plugins/livedesk/__plugin__/livedesk/actions.py
+++ superdesk/plugins/livedesk/__plugin__/livedesk/actions.py
@@ -18,8 +18,8 @@ from ..gui_core.gui_core import getPublishedGui
 
 @ioc.entity   
 def menuAction():
-    return Action('livedesk', Parent=defaults.menuAction(), Label=NC_('Menu', 'Live Blogs'),
-                  ScriptPath=getPublishedGui('livedesk/scripts/js/menu-live-blogs.js'))
+    return Action('livedesk', Parent=defaults.menuAction(), Label=NC_('Menu', 'Live Blogs') ) # ,
+                  #ScriptPath=getPublishedGui('livedesk/scripts/js/menu-live-blogs.js'))
 
 def menuTestAction():
     return Action('livedesk-test', Parent=defaults.menuAction(), Label=NC_('Menu', 'Test'),
