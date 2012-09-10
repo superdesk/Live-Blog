8cbbc399228a49d845f353c97a3f737654a0a7e6
Mihai Nistor <Mihai.Nistor@sourcefabric.org>
2012-09-10 17:20:51 +0300

    NR-224: Core implementation of MVC framework - part 2
    Eggs layed

--- superdesk/plugins/livedesk/build/lib/gui-resources/scripts/js/providers/google/tab.js
+++
@@ -1,1 +0,0 @@
-define('providers/google/tab', ['providers'], function(providers) {	providers.google = {		className: 'big-icon-google',				init: function() {							require(['providers','providers/google'], function(providers) {				providers.google.init();			});		}	};	return providers;});
