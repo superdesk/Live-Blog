9cdb197ec7af01dc5db625bfc63067579950df74
Gabriel Nistor <gabriel.nistor@sourcefabric.org>
2012-09-12 11:03:14 +0300

    Bug fix

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 setlocal
-set PYTHONPATH=distribution/libraries/distribute-0.6.27-py3.2.egg;
+set PYTHONPATH=distribution/tools/distribute-0.6.27-py3.2.egg;
 del distribution\components\*.egg
 python.exe components\clean_egg.py
 python.exe components\build_egg.py
