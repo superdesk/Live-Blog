5c06bcb4420cba64214d2fa6d80e2494186929f2
Mugur Rus <mugur.rus@sourcefabric.org>
2012-09-14 13:20:55 +0300

    NR-250 : Latest fixes for 1.0 beta release

--- superdesk/plugins/media-archive/superdesk/media_archive/core/impl/thumbnail_processor_ffmpeg.py
+++ superdesk/plugins/media-archive/superdesk/media_archive/core/impl/thumbnail_processor_ffmpeg.py
@@ -12,12 +12,10 @@ Thumbnail processor class definition.
 from ally.container import wire
 from ally.container.ioc import injected
 from ally.container.support import setup
-from ally.support.util_io import synchronizeURIToDir
-from ally.support.util_sys import pythonPath
 from genericpath import exists
 from os.path import join, abspath, dirname
 from os import makedirs
-from subprocess import Popen
+from subprocess import Popen, PIPE, STDOUT
 from superdesk.media_archive.core.spec import IThumbnailProcessor
 import logging
 import os
@@ -40,18 +38,13 @@ class ThumbnailProcessor(IThumbnailProcessor):
     The command used to transform the thumbnails''')
     command_resize = '"%(ffmpeg)s" -i "%(source)s" -s %(width)ix%(height)i "%(destination)s"'
     wire.config('command_resize', doc='''The command used to resize the thumbnails''')
-    ffmpeg_dir_path = join('workspace', 'tools', 'ffmpeg'); wire.config('ffmpeg_dir_path', doc='''
-    The path where the ffmpeg is placed in order to be used, if empty will not place the contained ffmpeg''')
-    ffmpeg_path = join(ffmpeg_dir_path, 'bin', 'ffmpeg'); wire.config('ffmpeg_path', doc='''
+    ffmpeg_path = join('workspace', 'tools', 'ffmpeg', 'bin', 'ffmpeg'); wire.config('ffmpeg_path', doc='''
     The path where the ffmpeg is found''')
 
     def __init__(self):
         assert isinstance(self.command_transform, str), 'Invalid command transform %s' % self.command_transform
         assert isinstance(self.command_resize, str), 'Invalid command resize %s' % self.command_resize
-        assert isinstance(self.ffmpeg_dir_path, str), 'Invalid ffmpeg directory %s' % self.ffmpeg_dir_path
         assert isinstance(self.ffmpeg_path, str), 'Invalid ffmpeg path %s' % self.ffmpeg_path
-
-        if self.ffmpeg_dir_path: synchronizeURIToDir(join(pythonPath(), 'resources', 'ffmpeg'), self.ffmpeg_dir_path)
 
     def processThumbnail(self, source, destination, width=None, height=None):
         '''
@@ -72,7 +65,7 @@ class ThumbnailProcessor(IThumbnailProcessor):
         destDir = dirname(destination)
         if not exists(destDir): makedirs(destDir)
         try:
-            p = Popen(shlex.split(command))
+            p = Popen(shlex.split(command), stdin=PIPE, stdout=PIPE, stderr=STDOUT)
             error = p.wait() != 0
         except Exception as e:
             log.exception('Problems while executing command:\n%s \n%s' % (command, e))
