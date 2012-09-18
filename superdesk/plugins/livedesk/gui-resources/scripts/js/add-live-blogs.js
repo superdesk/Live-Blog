e17384efca0c12869997c61668c3fa42ea1388e9
Mihai Nistor <Mihai.Nistor@sourcefabric.org>
2012-09-18 17:41:03 +0300

    NR-250: Latest fixes for 1.0 beta release
    removed title and added styles

---
+++ superdesk/plugins/livedesk/gui-resources/scripts/js/add-live-blogs.js
@@ -0,0 +1,13 @@
+define([
+	'providers/enabled',
+	'jquery',
+	'jquery/superdesk',
+	'jquery/rest',
+	'jquery/utils',
+	'jqueryui/texteditor',
+	'tmpl!livedesk>add'
+], function(providers, $, superdesk) {
+
+
+    return AddApp;
+});
