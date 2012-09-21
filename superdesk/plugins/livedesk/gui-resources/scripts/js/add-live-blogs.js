0545ee0ecc764c22a29b9ad17c2d0bf72bd0f191
Mihai Nistor <Mihai.Nistor@sourcefabric.org>
2012-09-21 10:52:03 +0300

    NR-220: Blog types
    Minor

--- superdesk/plugins/livedesk/gui-resources/scripts/js/add-live-blogs.js
+++ superdesk/plugins/livedesk/gui-resources/scripts/js/add-live-blogs.js
@@ -1,13 +1,23 @@
 define([
 	'providers/enabled',
+	'gizmo/superdesk',
 	'jquery',
 	'jquery/superdesk',
 	'jquery/rest',
 	'jquery/utils',
 	'jqueryui/texteditor',
+	config.guiJs('livedesk', 'models/blog'),
+	config.guiJs('livedesk', 'models/language'),
 	'tmpl!livedesk>add'
-], function(providers, $, superdesk) {
-
-
-    return AddApp;
+], function(providers, Gizmo, $) {
+	/*!
+	 * This is the main view of add Liveblog
+	 */
+	var AddView = Gizmo.View.extend({
+		
+	});
+	return function()
+	{
+	
+	}
 });
