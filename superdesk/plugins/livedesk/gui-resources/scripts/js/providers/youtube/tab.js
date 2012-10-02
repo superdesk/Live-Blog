2b80e0285c564bd5410b9b98b247c554d0bf4df8
Vlad Nicoara <vlad.nicoara@sourcefabric.org>
2012-10-02 12:33:18 +0200

    NR-257 : Add Youtube as a content provider for Livedesk
    First commit with the new files and basic work

---
+++ superdesk/plugins/livedesk/gui-resources/scripts/js/providers/youtube/tab.js
@@ -0,0 +1,68 @@
+define( 'providers/youtube/tab', 
+        ['providers', 'tmpl!livedesk>providers/youtube/post'], 
+function(providers) 
+{
+	providers.youtube = 
+	{
+		className: 'big-icon-youtube',		
+		init: function() 
+		{				
+			require(['providers','providers/youtube'], function(providers) {
+				providers.youtube.init();
+			});
+		},
+	
+	    timeline: 
+	    {
+	        preData: $.noop,
+	        init: $.noop,
+	        save: $.noop,
+	        edit: function()
+	        {
+	            this.el.find('.actions').removeClass('hide');
+	        },
+	        render: function(callback)
+	        {
+	            var self = this,
+	                feed = this.model.feed();
+	            
+	            try
+	            {
+	                feed.Meta = JSON.parse(feed.Meta);
+	            }
+	            catch(e)
+	            {
+	                eval('feed.Meta = '+feed.Meta);
+	            }
+	            feed.Meta.annotation = feed.Meta.annotation[0];
+	            
+	            $.tmpl('livedesk>providers/youtube/post', feed, function(e, o)
+                {
+                    self.setElement(o);
+                    $(self.el).off('click', '.btn.publish').on('click', '.btn.publish', function()
+                    {
+                        var data = 
+                        {
+                            Content: $('.youtube-full-content .result-text', self.el).html(),
+                            Meta: JSON.stringify( $.extend( feed.Meta, 
+                            {
+                                annotation: [$('.youtube-full-content .annotation:eq(0)', self.el).html(),
+                                             $('.youtube-full-content .annotation:eq(1)', self.el).html()]
+                            }))
+                        };    
+                        self.model.set(data).sync();
+                        $('.actions', self.el).addClass('hide');
+                    });
+                    
+                    $(self.el).off('click', '.btn.cancel').on('click', '.btn.cancel', function()
+                    {
+                        $('.actions', self.el).addClass('hide');
+                    });
+                    
+                    callback.call(self);
+                });
+	        }
+	    }
+	};
+	return providers;
+});
