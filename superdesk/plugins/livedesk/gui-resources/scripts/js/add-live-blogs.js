4d432444b188711a32328cd21b9e273f94ffc84e
Mihai Nistor <Mihai.Nistor@sourcefabric.org>
2012-10-10 11:18:40 +0300

    Partial

--- superdesk/plugins/livedesk/gui-resources/scripts/js/add-live-blogs.js
+++ superdesk/plugins/livedesk/gui-resources/scripts/js/add-live-blogs.js
@@ -1,6 +1,7 @@
 define([
 	'providers/enabled',
 	'gizmo/superdesk',
+	
 	'jquery',
 	'jquery/superdesk',
 	'jquery/rest',
@@ -9,15 +10,23 @@ define([
 	config.guiJs('livedesk', 'models/blog'),
 	config.guiJs('livedesk', 'models/language'),
 	'tmpl!livedesk>add'
-], function(providers, Gizmo, $) {
+], function(providers, Gizmo, BlogTypes, $) {
 	/*!
 	 * This is the main view of add Liveblog
 	 */
 	var AddView = Gizmo.View.extend({
 		
+	}),
+	BlogTypesView = Gizmo.View.extend({
+		init: function() {
+			this.collection.on('read', this.render, this).sync();
+		},
+		render: function() {
+			console.log(this.collection.feed());
+		}
 	});
 	return function()
 	{
-	
+		new BlogTypesView({ collection: new Gizmo.Register.BlogTypes }
 	}
 });
