7ab693bff1833a0dc85abbfc8f685c09cff29905
Mihai Nistor <Mihai.Nistor@sourcefabric.org>
2012-10-11 18:06:46 +0300

    NR-290: Timeline bugs
    Fixed

--- start.bat
+++ start.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
 setlocal
-set PYTHONPATH=superdesk/plugins/livedesk;plugins/gui-core;plugins/test-dev;superdesk/plugins/superdesk-article;
+set PYTHONPATH=superdesk/plugins/livedesk;plugins/gui-core;plugins/test-dev;superdesk/plugins/superdesk-article;components/support-cdm
 del superdesk\distribution\workspace\superdesk.db
 python.exe superdesk/distribution/application.py
