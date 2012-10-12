577df90bb9151dcb6c5cc7b0969d0acf791dceb4
Vlad Nicoara <vlad.nicoara@sourcefabric.org>
2012-10-12 16:24:59 +0200

    NR-295 : Admin UI -> It would be useful to have tooltips for input feeds (twitter, FB, user defined , etc.)
    Done

--- superdesk/plugins/livedesk/gui-resources/scripts/js/providers/youtube/tab.js
+++ superdesk/plugins/livedesk/gui-resources/scripts/js/providers/youtube/tab.js
@@ -5,10 +5,12 @@ function(providers)
 	providers.youtube = 
 	{
 		className: 'big-icon-youtube',		
+                tooltip: _('Youtube'),
 		init: function() 
 		{				
 			require(['providers','providers/youtube'], function(providers) {
 				providers.youtube.init();
+                                $("[rel=tooltip]").tooltip();
 			});
 		},
 	
