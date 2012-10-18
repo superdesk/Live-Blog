7721aa2966b49f1f665526536777997c44ebdc14
Mihai Nistor <Mihai.Nistor@sourcefabric.org>
2012-10-18 10:50:55 +0300

    NR-297: Admin UI -> no way to add comments to newly added Youtube item
    Null messega removed from flicker,
    Position changed

--- superdesk/plugins/livedesk/gui-resources/scripts/js/providers/flickr/adaptor.js
+++ superdesk/plugins/livedesk/gui-resources/scripts/js/providers/flickr/adaptor.js
@@ -20,8 +20,7 @@ function(providers,str, $, Gizmo)
             $(self.el).on('click', '.btn.publish', function()
             {
                 self.data.Content = $('.flickr-full-content .result-text', self.el).html();
-                self.data.Meta.annotation = [$('.flickr-full-content .annotation:eq(0)', self.el).html(), 
-                    $('.flickr-full-content .annotation:eq(1)', self.el).html()];
+                self.data.Meta.annotation = $('.flickr-full-content .annotation:eq(0)', self.el).html();
                 self.data.Meta = JSON.stringify(self.data.Meta);
                 self.parent.insert(self.data, self);
                 $('.actions', self.el).remove();
