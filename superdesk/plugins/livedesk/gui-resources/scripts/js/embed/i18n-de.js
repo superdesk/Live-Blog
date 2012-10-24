021f4529b7e81fd13e8d4114f735a86315b94bd8
Mihai Nistor <Mihai.Nistor@sourcefabric.org>
2012-10-24 17:05:32 +0300

    Internationalization for embed and changes for Tw

---
+++ superdesk/plugins/livedesk/gui-resources/scripts/js/embed/i18n-de.js
@@ -0,0 +1,17 @@
+window.livedesk.i18n = {
+	'show_current_date': 'true',
+	'HH:MM:ss': 'HH:MM',
+	'HH:MM:ss TT': 'HH:MM TT',
+	'ddd mmm dd yyyy HH:MM:ss TT': 'ddd mmm dd yyyy HH:MM TT',
+	'day_names': [
+		"So", "Mo", "Di", "Mi", "Do", "Fr", "Sa",
+		"Sonnabend", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag"
+	],
+	'month_names': [
+		"Jän", "Feb", "März", "Apr", "Mai", "Juni", "Juli", "Aug", "Sep", "Okt", "Nov", "Dez",
+		"Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"
+	],
+	'updating...': 'Wird aktualisiert...',
+	'by': 'von',
+	'updated on ': 'Zuletzt aktualisiert um '	
+};
