47693306226eacf426ac444b5e0fc94892dd5d9c
Mihai Nistor <Mihai.Nistor@sourcefabric.org>
2012-10-25 15:57:43 +0300

    Youtube look and feel changes

--- superdesk/plugins/livedesk/gui-resources/scripts/js/providers/youtube/adaptor.js
+++ superdesk/plugins/livedesk/gui-resources/scripts/js/providers/youtube/adaptor.js
@@ -19,7 +19,7 @@ function(providers,str, $, Gizmo)
             var self = this;
             $(self.el).on('click', '.btn.publish', function()
             {
-                self.data.Content = $('.youtube-full-content .result-content', self.el).html();
+                self.data.Content = $('.youtube-full-content .result-text', self.el).html();
                 
                 self.data.Meta.annotation = [$('.youtube-full-content .annotation:eq(0)', self.el).html(), 
                     $('.youtube-full-content .annotation:eq(1)', self.el).html()];
@@ -69,6 +69,7 @@ function(providers,str, $, Gizmo)
             universal: function(obj) 
             {
                 var meta =  jQuery.extend(true, {}, obj);
+				console.log(obj);
                 var returner = new AnnotateView
                 ({
                     data: 
