8402eca4595c8485e3614a2fa9c2bd3b5a572ef4
Gabriel Nistor <gabriel.nistor@sourcefabric.org>
2012-10-26 17:42:09 +0300

    NR-325 : Improve scalability on multiple process machines
    Added asyncore server

---
+++ components/ally-http-asyncore-server/__setup__/ally_http_asyncore_server/server.py
@@ -0,0 +1,33 @@
+'''
+Created on Nov 23, 2011
+
+@package: ally core http
+@copyright: 2012 Sourcefabric o.p.s.
+@license: http://www.gnu.org/licenses/gpl-3.0.txt
+@author: Gabriel Nistor
+
+Runs the asyncore py web server.
+'''
+
+from ..ally_core_http import server_type, server_version, server_host, \
+    server_port
+from ..ally_core_http.processor import pathAssemblies
+from ally.container import ioc
+from ally.core.http.server import server_asyncore
+from threading import Thread
+
+# --------------------------------------------------------------------
+
+try: from ..ally_http_proxy_server.server import proxiedServers
+except ImportError: pass  # The proxy server is not available
+else:
+    @ioc.before(proxiedServers)
+    def placeToProxy():
+        args = pathAssemblies(), server_version(), server_host()
+        proxiedServers()['asyncore'] = lambda port: server_asyncore.run(*args, port=port)
+
+@ioc.start
+def runServer():
+    if server_type() == 'asyncore':
+        args = pathAssemblies(), server_version(), server_host(), server_port()
+        Thread(name='HTTP server thread', target=server_asyncore.run, args=args).start()
