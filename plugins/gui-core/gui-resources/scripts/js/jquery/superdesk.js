d9ed923224613f45b1ee08e70604a5455f5f2c2b
Mihai Balaceanu <gizm0bill@gmail.com>
2012-10-29 13:25:25 +0200

    stupid history...

--- plugins/gui-core/gui-resources/scripts/js/jquery/superdesk.js
+++ plugins/gui-core/gui-resources/scripts/js/jquery/superdesk.js
@@ -153,12 +153,14 @@ var superdesk =
         bind: function(href, callback, title)
         {
             var History = window.History,
-                State = History.getState();
-            
-            if( $.trim(State.data.href) != '' && this._repository[State.data.href]  )
+
+            if( $.trim(href) != '' && this._repository[href]  )
             {
+                History.replaceState({href: href}, 
+                        title ? this._titlePrefix + title : null, 
+                        this._base + (!config.server_tech ? '?'+href : href));
                 callback.call();
-                return;
+                return callback;
             }
             
             this._repository[href] = callback;
