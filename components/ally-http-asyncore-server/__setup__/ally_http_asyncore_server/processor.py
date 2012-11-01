a684542e7470aed27e71fc4bddb186007f5dba0a
chupy <gabriel.nistor@sourcefabric.org>
2012-11-01 18:39:58 +0200

    NR-325 : Improve scalability on multiple process machines
    Added asyncore support for content handling.

---
+++ components/ally-http-asyncore-server/__setup__/ally_http_asyncore_server/processor.py
@@ -0,0 +1,48 @@
+'''
+Created on Nov 24, 2011
+
+@package: ally core http
+@copyright: 2011 Sourcefabric o.p.s.
+@license: http://www.gnu.org/licenses/gpl-3.0.txt
+@author: Gabriel Nistor
+
+Provides the setup for the asyncore processor.
+'''
+
+from ..ally_core.processor import assemblyResources, parser
+from ..ally_core_http import server_type
+from ..ally_core_http.processor import updateAssemblyResourcesForHTTP
+from ally.container import ioc
+from ally.core.http.impl.processor.asyncore_content import \
+    AsyncoreContentHandler
+from ally.design.processor import Handler
+from os import path
+
+# --------------------------------------------------------------------
+# Creating the processors used in handling the request
+
+@ioc.config
+def dump_requests_size():
+    '''The minimum size of the request length to be dumped on the file system in bytes'''
+    return 1024 * 1024
+
+@ioc.config
+def dump_requests_path():
+    '''The path where the requests are dumped when they are to big to keep in memory'''
+    return path.join('workspace', 'requests_buffer')
+
+# --------------------------------------------------------------------
+
+@ioc.entity
+def asyncoreContent() -> Handler:
+    b = AsyncoreContentHandler()
+    b.dumpRequestsSize = dump_requests_size()
+    b.dumpRequestsPath = dump_requests_path()
+    return b
+
+# --------------------------------------------------------------------
+
+@ioc.after(updateAssemblyResourcesForHTTP)
+def updateAssemblyResourcesForHTTPAsyncore():
+    if server_type() == 'asyncore':
+        assemblyResources().add(asyncoreContent(), before=parser())
--- components/ally-http-asyncore-server/ally/core/http/server/server_asyncore.py
+++ components/ally-http-asyncore-server/ally/core/http/server/server_asyncore.py
@@ -13,11 +13,12 @@ from ally.api.config import GET, INSERT, UPDATE, DELETE
 from ally.core.http.spec.server import METHOD_OPTIONS, RequestHTTP, ResponseHTTP, \
     RequestContentHTTP, ResponseContentHTTP
 from ally.core.spec.codes import Code
-from ally.design.processor import Processing, Chain, Assembly, ONLY_AVAILABLE, \
-    CREATE_REPORT
+from ally.design.context import optional
+from ally.design.processor import Processing, Assembly, ONLY_AVAILABLE, \
+    CREATE_REPORT, Chain
 from ally.support.util_io import IOutputStream, readGenerator
 from asyncore import dispatcher, loop
-from collections import deque
+from collections import Callable, deque
 from http.server import BaseHTTPRequestHandler
 from io import BytesIO
 from urllib.parse import urlparse, parse_qsl
@@ -32,6 +33,19 @@ log = logging.getLogger(__name__)
 # Constants used in indicating the write option. 
 WRITE_BYTES = 1
 WRITE_ITER = 2
+
+# --------------------------------------------------------------------
+
+class RequestContentHTTPAsyncore(RequestContentHTTP):
+    '''
+    The request content context.
+    '''
+    # ---------------------------------------------------------------- Optional
+    contentReader = optional(Callable, doc='''
+    @rtype: Callable
+    The content reader callable used for pushing data from the asyncore read. Once the reader is finalized it will
+    return a chain that is used for further request processing.
+    ''')
 
 # --------------------------------------------------------------------
 
@@ -58,9 +72,6 @@ class RequestHandler(dispatcher, BaseHTTPRequestHandler):
                }
     methodUnknown = -1
     # The available method for processing.
-    pathProcessing = list
-    # A list that contains tuples having on the first position a regex for matching a path, and the second value 
-    # the processing for handling the path.
 
     def __init__(self, server, request, address):
         '''
@@ -75,7 +86,6 @@ class RequestHandler(dispatcher, BaseHTTPRequestHandler):
         '''
         assert isinstance(server, AsyncServer), 'Invalid server %s' % server
         assert isinstance(address, tuple), 'Invalid address %s' % address
-        assert isinstance(self.pathProcessing, list), 'Invalid path processing %s' % self.pathProcessing
         
         dispatcher.__init__(self, request, map=server.map)
         self.server = server
@@ -85,43 +95,146 @@ class RequestHandler(dispatcher, BaseHTTPRequestHandler):
         self.request_version = 'HTTP/1.1'
         self.requestline = 0
         
+        self._stage = 1
         self.rfile = BytesIO()
         self._readCarry = None
+        self._reader = None
 
         self.wfile = BytesIO()
         self._writeq = deque()
-
-    def readable (self):
-        '''
-        Always able to read.
-        @see: dispatcher.readable
-        '''
-        return True
-
+        
+        self._next(1)
+        
     def handle_read(self):
         '''
         @see: dispatcher.handle_read
         '''
         try: data = self.recv(self.bufferSize)
         except socket.error:
-            log.exception('Exception occurred while reading the connection \'%s\'' % self.connection)
+            log.exception('Exception occurred while reading the content from \'%s\'' % self.connection)
             self.close()
             return
-        self._handleReadData(data)
+        self.handle_data(data)
+    
+    def handle_error(self):
+        log.exception('A problem occurred in the server')
+    
+    def end_headers(self):
+        '''
+        @see: BaseHTTPRequestHandler.end_headers
+        '''
+        super().end_headers()
+        self._writeq.append((WRITE_BYTES, memoryview(self.wfile.getvalue())))
+        self.wfile = None
 
+    def log_message(self, format, *args):
+        '''
+        @see: BaseHTTPRequestHandler.log_message
+        '''
+        # TODO: see for a better solution for this, check for next python release
+        # This is a fix: whenever a message is logged there is an attempt to find some sort of host name which
+        # creates a big delay whenever the request is made from a non localhost client.
+        assert log.debug(format, *args) or True
+        
     # ----------------------------------------------------------------
     
-    def writable(self):
+    def _next(self, stage):
+        '''
+        Proceed to next stage.
+        '''
+        assert isinstance(stage, int), 'Invalid stage %s' % stage
+        self.readable = getattr(self, '_%s_readable' % stage, None)
+        self.handle_data = getattr(self, '_%s_handle_data' % stage, None)
+        self.writable = getattr(self, '_%s_writable' % stage, None)
+        self.handle_write = getattr(self, '_%s_handle_write' % stage, None)
+          
+    # ----------------------------------------------------------------
+    
+    def _1_readable(self):
+        '''
+        @see: dispatcher.readable
+        '''
+        return True
+    
+    def _1_handle_data(self, data):
+        '''
+        Handle the data as being part of the request.
+        '''
+        if self._readCarry is not None: data = self._readCarry + data
+        index = data.find(self.requestTerminator)
+        requestTerminatorLen = len(self.requestTerminator)
+        
+        if index >= 0:
+            index += requestTerminatorLen 
+            self.rfile.write(data[:index])
+            self.rfile.seek(0)
+            self.raw_requestline = self.rfile.readline()
+            self.parse_request()
+            self.rfile = None
+            
+            self._process(self.methods.get(self.command, self.methodUnknown))
+            
+            if index < len(data) and self.handle_data: self.handle_data(data[index:])
+        else:
+            self._readCarry = data[-requestTerminatorLen:]
+            self.rfile.write(data[:-requestTerminatorLen])
+            
+            if self.rfile.tell() > self.maximumRequestSize:
+                self.send_response(400, 'Request to long')
+                self.end_headers()
+                self.close()
+                
+    def _1_writable(self):
+        '''
+        @see: dispatcher.writable
+        '''
+        return False
+        
+    # ----------------------------------------------------------------
+    
+    def _2_readable(self):
+        '''
+        @see: dispatcher.readable
+        '''
+        return self._reader is not None
+    
+    def _2_handle_data(self, data):
+        '''
+        Handle the data as being part of the request.
+        '''
+        assert self._reader is not None, 'No reader available'
+        chain = self._reader(data)
+        if chain is not None:
+            assert isinstance(chain, Chain), 'Invalid chain %s' % chain
+            self._reader = None
+            self._next(3)  # Now we proceed to write stage
+            chain.doAll()
+            
+    def _2_writable(self):
+        '''
+        @see: dispatcher.writable
+        '''
+        return False
+            
+    # ----------------------------------------------------------------
+
+    def _3_readable(self):
+        '''
+        @see: dispatcher.readable
+        '''
+        return False
+            
+    def _3_writable(self):
         '''
         @see: dispatcher.writable
         '''
         return bool(self._writeq)
     
-    def handle_write(self):
+    def _3_handle_write(self):
         '''
         @see: dispatcher.handle_write
         '''
-        assert self._writeq, 'Nothing to write anymore'
+        assert self._writeq, 'Nothing to write'
         
         what, content = self._writeq[0]
         assert what in (WRITE_ITER, WRITE_BYTES), 'Invalid what %s' % what
@@ -145,51 +258,13 @@ class RequestHandler(dispatcher, BaseHTTPRequestHandler):
             elif what == WRITE_BYTES: self._writeq[0] = (WRITE_BYTES, data[sent:])
         else:
             if what == WRITE_BYTES: del self._writeq[0]
-            
-    # ----------------------------------------------------------------
-    
-    def _handleReadData(self, data):
-        '''
-        Handle the data as being part of the request.
-        '''
-        if self._readCarry is not None: data = self._readCarry + data
-        index = data.find(self.requestTerminator)
-        requestTerminatorLen = len(self.requestTerminator)
         
-        if index >= 0:
-            self._handleReadData = self._handleReadDataForContent  # Now the read content is considered as content
-            self.rfile.write(data[:index + requestTerminatorLen:])
-            self.rfile.seek(0)
-            self.raw_requestline = self.rfile.readline()
-            self.parse_request()
-            
-            self.rfile.seek(0)
-            self.rfile.truncate()
-            self.rfile.write(data[index + requestTerminatorLen:])
-            self._readCarry = None
-            
-            self._process(self.methods.get(self.command, self.methodUnknown))
-        else:
-            self._readCarry = data[-requestTerminatorLen:]
-            self.rfile.write(data[:-requestTerminatorLen])
-            
-            if self.rfile.tell() > self.maximumRequestSize:
-                self.send_response(400, 'Request to long')
-                self.end_headers()
-                self.close()
-
-    def _handleReadDataForContent(self, data):
-        '''
-        Handle the data as being part of the request content.
-        '''
-        if data: self.rfile.write(data)
-
     # ----------------------------------------------------------------
     
     def _process(self, method):
         url = urlparse(self.path)
         path = url.path.lstrip('/')
-        for regex, processing in self.pathProcessing:
+        for regex, processing in self.server.pathProcessing:
             match = regex.match(path)
             if match:
                 uriRoot = path[:match.end()]
@@ -198,9 +273,9 @@ class RequestHandler(dispatcher, BaseHTTPRequestHandler):
                 assert isinstance(processing, Processing), 'Invalid processing %s' % processing
                 req, reqCnt = processing.contexts['request'](), processing.contexts['requestCnt']()
                 rsp, rspCnt = processing.contexts['response'](), processing.contexts['responseCnt']()
-                chain = Chain(processing)
+
                 assert isinstance(req, RequestHTTP), 'Invalid request %s' % req
-                assert isinstance(reqCnt, RequestContentHTTP), 'Invalid request content %s' % reqCnt
+                assert isinstance(reqCnt, RequestContentHTTPAsyncore), 'Invalid request content %s' % reqCnt
                 assert isinstance(rsp, ResponseHTTP), 'Invalid response %s' % rsp
                 assert isinstance(rspCnt, ResponseContentHTTP), 'Invalid response content %s' % rspCnt
 
@@ -215,44 +290,36 @@ class RequestHandler(dispatcher, BaseHTTPRequestHandler):
 
         req.method = method
         req.headers = dict(self.headers)
-        reqCnt.source = self.rfile
 
-        chain.process(request=req, requestCnt=reqCnt, response=rsp, responseCnt=rspCnt)
-        assert isinstance(rsp.code, Code), 'Invalid response code %s' % rsp.code
-
-        if ResponseHTTP.headers in rsp:
-            for name, value in rsp.headers.items(): self.send_header(name, value)
-
-        if ResponseHTTP.text in rsp: self.send_response(rsp.code.code, rsp.text)
-        else: self.send_response(rsp.code.code)
-
-        self.end_headers()
-
-        if rspCnt.source is not None:
-            if isinstance(rspCnt.source, IOutputStream): source = readGenerator(rspCnt.source, self.bufferSize)
-            else: source = rspCnt.source
-
-            self._writeq.append((WRITE_ITER, iter(source)))
-
-    # ----------------------------------------------------------------
+        def respond():
+            assert isinstance(rsp.code, Code), 'Invalid response code %s' % rsp.code
     
-    def end_headers(self):
-        '''
-        @see: BaseHTTPRequestHandler.end_headers
-        '''
-        super().end_headers()
-        self._writeq.append((WRITE_BYTES, memoryview(self.wfile.getvalue())))
-        self.wfile = None
-
-
-    def log_message(self, format, *args):
-        '''
-        @see: BaseHTTPRequestHandler.log_message
-        '''
-        # TODO: see for a better solution for this, check for next python release
-        # This is a fix: whenever a message is logged there is an attempt to find some sort of host name which
-        # creates a big delay whenever the request is made from a non localhost client.
-        assert log.debug(format, *args) or True
+            if ResponseHTTP.headers in rsp:
+                for name, value in rsp.headers.items(): self.send_header(name, value)
+    
+            if ResponseHTTP.text in rsp: self.send_response(rsp.code.code, rsp.text)
+            else: self.send_response(rsp.code.code)
+    
+            self.end_headers()
+    
+            if rspCnt.source is not None:
+                if isinstance(rspCnt.source, IOutputStream): source = readGenerator(rspCnt.source, self.bufferSize)
+                else: source = rspCnt.source
+    
+                self._writeq.append((WRITE_ITER, iter(source)))
+            
+        chain = Chain(processing)
+        chain.process(request=req, requestCnt=reqCnt, response=rsp, responseCnt=rspCnt)
+        chain.callBack(respond)
+        
+        while True:
+            if not chain.do():
+                self._next(3)  # Now we proceed to write stage
+                break
+            if reqCnt.contentReader is not None:
+                self._next(2)  # Now we proceed to read stage
+                self._reader = reqCnt.contentReader
+                break
 
 # --------------------------------------------------------------------
 
@@ -262,23 +329,28 @@ class AsyncServer(dispatcher):
     '''
     timeout = 10.0
     # The timeout for select loop.
-    
-    def __init__ (self, serverAddress, requestHandlerFactory):
+
+    def __init__(self, serverAddress, pathProcessing, requestHandlerFactory):
         '''
         Construct the server.
         
         @param serverAddress: tuple(string, integer)
             The server address host and port.
+        @param pathProcessing: list[tuple(regex, Processing)] 
+            A list that contains tuples having on the first position a regex for matching a path, and the second value 
+            the processing for handling the path.
         @param requestHandlerFactory: callable(AsyncServer, socket, tuple(string, integer))
             The factory that provides request handlers, takes as arguments the server, request socket
             and client address.
         '''
         assert isinstance(serverAddress, tuple), 'Invalid server address %s' % serverAddress
+        assert isinstance(pathProcessing, list), 'Invalid path processing %s' % pathProcessing
         assert callable(requestHandlerFactory), 'Invalid request handler factory %s' % requestHandlerFactory
         
         self.map = {}
         dispatcher.__init__(self, map=self.map)
         self.serverAddress = serverAddress
+        self.pathProcessing = pathProcessing
         self.requestHandlerFactory = requestHandlerFactory
         
         self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -327,23 +399,23 @@ def run(pathAssemblies, server_version, host='', port=80):
     '''
     assert isinstance(pathAssemblies, list), 'Invalid path assemblies %s' % pathAssemblies
     RequestHandler.server_version = server_version
-    RequestHandler.pathProcessing = []
+    pathProcessing = []
     for pattern, assembly in pathAssemblies:
         assert isinstance(pattern, str), 'Invalid pattern %s' % pattern
         assert isinstance(assembly, Assembly), 'Invalid assembly %s' % assembly
 
         processing, report = assembly.create(ONLY_AVAILABLE, CREATE_REPORT,
-                                             request=RequestHTTP, requestCnt=RequestContentHTTP,
+                                             request=RequestHTTP, requestCnt=RequestContentHTTPAsyncore,
                                              response=ResponseHTTP, responseCnt=ResponseContentHTTP)
 
         log.info('Assembly report for pattern \'%s\':\n%s', pattern, report)
-        RequestHandler.pathProcessing.append((re.compile(pattern), processing))
-    
+        pathProcessing.append((re.compile(pattern), processing))
+        
     try:
-        server = AsyncServer((host, port), RequestHandler)
+        server = AsyncServer((host, port), pathProcessing, RequestHandler)
         print('=' * 50, 'Started Async REST API server...')
         # import profile
-        # profile.runctx('server.serve_limited(1000)', globals(), locals(), 'profiler.data') # TODO: remove
+        # profile.runctx('server.serve_limited(1000)', globals(), locals(), 'profiler.data')
         server.serve_forever()
     except KeyboardInterrupt:
         print('=' * 50, '^C received, shutting down server')
