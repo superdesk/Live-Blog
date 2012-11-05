e5ddc5d0d8d52454df5578861103cc5054883931
Mihai Balaceanu <gizm0bill@gmail.com>
2012-11-05 17:07:05 +0200

    upload component fix for url

--- superdesk/plugins/media-archive/gui-resources/scripts/js/upload.js
+++ superdesk/plugins/media-archive/gui-resources/scripts/js/upload.js
@@ -14,7 +14,7 @@ function()
                 fd.append(filename || 'upload_file', file);
                 var xhr = new XMLHttpRequest();
                 // replace or add format we want as response in url // path = path.search(/(((\..+)?\?))/) != -1 ? path.replace(/(((\..+)?\?))/,'.xml?') : path+'.xml';
-                xhr.open('POST', (path+' ').replace(/(((\..+)?\?)|(\s$))/,'.xml$1'), true);
+                xhr.open('POST', (path+' ').replace(/(((\.[\w\d-]+)?\?)|(\s$))/,'.xml$1'), true);
                 xhr.setRequestHeader('X-Filter', 'Content');
                 startCb && startCb.apply(this);
                 xhr.send(fd);
