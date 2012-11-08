3b8fcf3871b4496b73ba131d46e3a1f560ff48b3
Mihai Balaceanu <gizm0bill@gmail.com>
2012-11-08 14:41:49 +0200

    NR-441 :  LiveBlog->ConfigureBlog->Internal collaborators -> Edit button does not work
    moved to a new version with refactoring todo

--- superdesk/plugins/livedesk/gui-resources/scripts/js/configure-blog.js
+++ superdesk/plugins/livedesk/gui-resources/scripts/js/configure-blog.js
@@ -1,3 +1,4 @@
+// TODO refactor
 define([ 'jquery', 'jquery/rest', 'jqueryui/texteditor', 'jquery/avatar',
          'tmpl!livedesk>layouts/livedesk', 'tmpl!livedesk>layouts/blog', 
          'tmpl!livedesk>configure', 'tmpl!livedesk>configure/internal-colabs' ],
