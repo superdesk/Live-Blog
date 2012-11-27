2ca7d62a1a2d03946b9ba2fb2ddcb185c26a2f55
Mihai Balaceanu <gizm0bill@gmail.com>
2012-11-27 19:03:03 +0200

    NR-501 : Media Archive user interface implementation version 1.0
    add media functionallity

--- plugins/media-archive/gui-resources/scripts/js/upload.js
+++ plugins/media-archive/gui-resources/scripts/js/upload.js
@@ -8,13 +8,22 @@ function()
     var origImageCtrl = $.ui.texteditor.prototype.plugins.controls.image,
         uploadComponent = 
         {
-            upload: function(file, filename, path, startCb)
+            /*!
+             * upload one file to server
+             * @param {object} file The object to append to FormData (form.files[i])
+             * @param {string} filename The key of the file in post data
+             * @param {string} path Server path to upload to
+             * @param {function} startCb Callback for upload start, falls back to format if string and !format 
+             * @param {string} format The format to append to path example ".xml"
+             */
+            upload: function(file, filename, path, startCb, format)
             {
-                var fd = new FormData();
+                var fd = new FormData(),
+                    format = typeof startCb == 'string' && !format ? startCb : (format ? format : 'xml');
                 fd.append(filename || 'upload_file', file);
                 var xhr = new XMLHttpRequest();
                 // replace or add format we want as response in url // path = path.search(/(((\..+)?\?))/) != -1 ? path.replace(/(((\..+)?\?))/,'.xml?') : path+'.xml';
-                xhr.open('POST', (path+' ').replace(/(((\.[\w\d-]+)?\?)|(\s$))/,'.xml$1'), true);
+                xhr.open('POST', (path+' ').replace(/(((\.[\w\d-]+)?\?)|(\s$))/,'.'+format+'$1'), true);
                 xhr.setRequestHeader('X-Filter', 'Content');
                 startCb && startCb.apply(this);
                 xhr.send(fd);
@@ -33,10 +42,10 @@ function()
                               '     <label class="control-label" for="editor-image-text">Upload:</label>'+
                               '     <div style="position:relative" class="controls">'+
                               //'         <input type="hidden" name="Authorization" value="'+ localStorage.getItem('superdesk.login.session')+'"/>"'+
-							  '         <input type="file" name="upload_file" multiple="multiple"'+
-                              '             style="position:absolute; width:100%; opacity:0;" />'+
                               '         <input type="button" value="'+_('Browse')+'"'+
                               '             class="btn btn-primary btn-block btn-medium span3" style="position:absolute;" />'+
+                              '         <input type="file" name="upload_file" multiple="multiple"'+
+                              '             style="position:absolute; top: 0px; left: 0px; width:100%; opacity:0;" />'+
                               ' </div></fieldset></form>';
                 // build upload form html component
                 $(command.dialog).prepend(htmlCom);
