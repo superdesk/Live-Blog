559b7d356a07d614f985ab5713a31815d4ceb183
ioan <ioan.pocol@sourcefabric.org>
2012-11-29 17:57:35 +0200

    Add plugin info for query criteria

--- plugins/media-archive-audio/superdesk/media_archive/api/audio_info.py
+++ plugins/media-archive-audio/superdesk/media_archive/api/audio_info.py
@@ -9,12 +9,13 @@ Created on Oct 1, 2012
 API specifications for media archive audio info.
 '''
 
-from .domain_archive import modelArchive
-from .audio_data import AudioData, QAudioData
-from .meta_data import MetaData, QMetaData
-from .meta_info import MetaInfo, QMetaInfo, IMetaInfoService
 from ally.api.config import query, service
 from ally.api.criteria import AsLikeOrdered
+from superdesk.media_archive.api.domain_archive import modelArchive
+from superdesk.media_archive.api.meta_info import MetaInfo, QMetaInfo,\
+    IMetaInfoService
+from superdesk.media_archive.api.audio_data import AudioData, QAudioData
+from superdesk.media_archive.api.meta_data import MetaData, QMetaData
 
 # --------------------------------------------------------------------
 
