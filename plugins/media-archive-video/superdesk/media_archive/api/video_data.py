7d7b2b719967a5c93ba32000b5efba303643eeda
ioan <ioan.pocol@sourcefabric.org>
2012-11-29 17:57:35 +0200

    Add plugin info for query criteria

--- plugins/media-archive-video/superdesk/media_archive/api/video_data.py
+++ plugins/media-archive-video/superdesk/media_archive/api/video_data.py
@@ -9,10 +9,11 @@ Created on Aug 23, 2012
 API specifications for media meta data video archive.
 '''
 
-from .domain_archive import modelArchive
-from .meta_data import MetaData, QMetaData, IMetaDataService
 from ally.api.config import query, service
 from ally.api.criteria import AsEqualOrdered, AsLikeOrdered
+from superdesk.media_archive.api.meta_data import MetaData, QMetaData,\
+    IMetaDataService
+from superdesk.media_archive.api.domain_archive import modelArchive
 
 # --------------------------------------------------------------------
 
