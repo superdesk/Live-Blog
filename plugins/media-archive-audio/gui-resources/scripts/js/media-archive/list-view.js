3c7e70f530fa5f343f162ab41494f78147eee761
Mihai Balaceanu <gizm0bill@gmail.com>
2012-12-06 18:18:13 +0200

    NR-501 : Media Archive user interface implementation version 1.0
    more work on types

--- plugins/media-archive-audio/gui-resources/scripts/js/media-archive/list-view.js
+++ plugins/media-archive-audio/gui-resources/scripts/js/media-archive/list-view.js
@@ -4,20 +4,20 @@ define
     'jquery/superdesk',
     'gizmo/superdesk',
     config.guiJs('media-archive', 'types/_default/list-view'),
-    config.guiJs('media-archive-image', 'media-archive/common'),
-    'tmpl!media-archive-image>media-archive/list'
+    config.guiJs('media-archive-audio', 'media-archive/common'),
+    'tmpl!media-archive-audio>media-archive/list'
 ],
 function($, superdesk, giz, ItemView, common)
 {
     /*!
      * @see gizmo/views/list/ItemView
      */
-    ImageView = ItemView.extend
+    AudioView = ItemView.extend
     ({
-        tmpl: 'media-archive-image>media-archive/list',
+        tmpl: 'media-archive-audio>media-archive/list',
         editClass: common.edit,
         viewClass: common.view
     });
-    return ImageView;
+    return AudioView;
 });
 
