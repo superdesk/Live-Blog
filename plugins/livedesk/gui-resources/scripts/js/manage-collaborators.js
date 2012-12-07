a4525dcceed764d4e71074d94614c7d53f39975d
Mihai Nistor <Mihai.Nistor@sourcefabric.org>
2012-12-07 11:43:01 +0200

    NR-474: Refactor the collaborators setup in the blog configuration screen
    start-sources.bat now works for checking if configs are done

--- plugins/livedesk/gui-resources/scripts/js/manage-collaborators.js
+++ plugins/livedesk/gui-resources/scripts/js/manage-collaborators.js
@@ -1,16 +1,42 @@
-// TODO refactor
 define([
 	'jquery',
-    'gizmo/superdesk',
+	'gizmo/superdesk',
 	'jquery/rest',
 	'jquery/avatar',
+	config.guiJs('livedesk', 'models/collaborators'),
+	config.guiJs('livedesk', 'models/blog'),
+	config.guiJs('livedesk', 'models/collaborator'),
 	'tmpl!livedesk>layouts/livedesk',
-	'tmpl!livedesk>layouts/blog', 
+	'tmpl!livedesk>layouts/blog',
 	'tmpl!livedesk>manage-collaborators',
 	'tmpl!livedesk>manage-collaborators/internal-colabs'
-], function(, Gizmo) {
-	
-	return app = function(theBlog) {
-		console.log(theBlog);
+	], function ($, Gizmo, Collaborators) {
+
+	var CollaboratorView = Gizmo.View.extend({
+		
+	}),
+	ManageInternalCollaboratorsView = Gizmo.View.extend({
+		init: function(){
+
+		}
+	}),
+	MainManageCollaboratorsView = Gizmo.View.extend({
+		refresh: function () {
+			var self = this;
+			self.model = Gizmo.Auth(new Gizmo.Register.Blog(self.theBlog));
+			self.model
+				.on('read', self.render, self)
+				.sync();
+		},
+		render: function(evt, data) {
+
+		}
+	});
+	var mainManageCollaboratosView = new MainManageCollaboratorsView({
+		el: '#area-main'
+	});
+	return app = function (theBlog) {
+		mainManageCollaboratosView.theBlog = theBlog;
+		mainManageCollaboratosView.refresh();
 	}
 });
