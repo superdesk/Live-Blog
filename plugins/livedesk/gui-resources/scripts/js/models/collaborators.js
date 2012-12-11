572ff62c8a045c697a83c7efad556408dd448152
Mihai Nistor <Mihai.Nistor@sourcefabric.org>
2012-12-11 16:29:16 +0200

    NR-474: Refactor the collaborators setup in the blog configuration screen
    Search isn't working well yet.

--- plugins/livedesk/gui-resources/scripts/js/models/collaborators.js
+++ plugins/livedesk/gui-resources/scripts/js/models/collaborators.js
@@ -1,8 +1,30 @@
-define(['gizmo/superdesk', config.guiJs('livedesk', 'models/collaborator')], 
-function(Gizmo, Collaborator)
+define(['jquery', 'gizmo/superdesk', config.guiJs('livedesk', 'models/collaborator')], 
+function($, Gizmo, Collaborator)
 {
     return Gizmo.Collection.extend({ 
     	url: new Gizmo.Url('Superdesk/Collaborator'),
+    	add: function(data){
+           var self = this,
+				dataAdapter = function()
+				{ 
+				    return self.syncAdapter.request.apply(self.syncAdapter, arguments); 
+				}, dfd = $.Deferred();
+    		for( var i = 0, count = data.length; i < count; i++) {
+                ret = dataAdapter(this.href+data[i].get('Id')+'/Add').insert();
+    		}
+    		return ret;
+    	},
+    	remove: function(data) {
+           var self = this,
+				dataAdapter = function()
+				{ 
+				    return self.syncAdapter.request.apply(self.syncAdapter, arguments); 
+				}, dfd = $.Deferred();
+    		for( var i = 0, count = data.length; i < count; i++) {
+                ret = dataAdapter(this.href+data[i].get('Id')+'/Remove').remove();
+    		}
+    		return ret;    		
+    	},
     	model: Collaborator
     }, { register: 'Collaborators' } );
 });
