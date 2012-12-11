0a0184e0fd50646911c4ccda72692ce7c7bce85f
Mihai Nistor <Mihai.Nistor@sourcefabric.org>
2012-12-11 16:53:44 +0200

    NR-474: Refactor the collaborators setup in the blog configuration screen
    merge

--- plugins/livedesk/gui-resources/scripts/js/models/collaborators.js
+++ plugins/livedesk/gui-resources/scripts/js/models/collaborators.js
@@ -2,29 +2,29 @@ define(['jquery', 'gizmo/superdesk', config.guiJs('livedesk', 'models/collaborat
 function($, Gizmo, Collaborator)
 {
     return Gizmo.Collection.extend({ 
-    	url: new Gizmo.Url('Superdesk/Collaborator'),
-    	add: function(data){
+        url: new Gizmo.Url('Superdesk/Collaborator'),
+        add: function(data){
            var self = this,
-				dataAdapter = function()
-				{ 
-				    return self.syncAdapter.request.apply(self.syncAdapter, arguments); 
-				}, dfd = $.Deferred();
-    		for( var i = 0, count = data.length; i < count; i++) {
+                dataAdapter = function()
+                { 
+                    return self.syncAdapter.request.apply(self.syncAdapter, arguments); 
+                }, dfd = $.Deferred();
+            for( var i = 0, count = data.length; i < count; i++) {
                 ret = dataAdapter(this.href+data[i].get('Id')+'/Add').insert();
-    		}
-    		return ret;
-    	},
-    	remove: function(data) {
+            }
+            return ret;
+        },
+        remove: function(data) {
            var self = this,
-				dataAdapter = function()
-				{ 
-				    return self.syncAdapter.request.apply(self.syncAdapter, arguments); 
-				}, dfd = $.Deferred();
-    		for( var i = 0, count = data.length; i < count; i++) {
+                dataAdapter = function()
+                { 
+                    return self.syncAdapter.request.apply(self.syncAdapter, arguments); 
+                }, dfd = $.Deferred();
+            for( var i = 0, count = data.length; i < count; i++) {
                 ret = dataAdapter(this.href+data[i].get('Id')+'/Remove').remove();
-    		}
-    		return ret;    		
-    	},
-    	model: Collaborator
+            }
+            return ret;         
+        },
+        model: Collaborator
     }, { register: 'Collaborators' } );
 });
