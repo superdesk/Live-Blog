3c15348d3330815eadd6ab1575bb6771aaef25b2
Mihai Nistor <Mihai.Nistor@sourcefabric.org>
2012-12-18 16:04:57 +0200

    NR-490: LivedeskEmbed -> refactoring
    Automatic livedesk emebed

--- plugins/livedesk-embed/__plugin__/livedesk_embed/gui.py
+++ plugins/livedesk-embed/__plugin__/livedesk_embed/gui.py
@@ -1,4 +1,4 @@
-'''
+''', 
 Created on May 3rd, 2012
 
 @package: superdesk media archive
@@ -10,12 +10,41 @@ Publish the GUI resources.
 '''
 
 from ally.container import ioc
-from ..gui_core.gui_core import publishGui
+from ..gui_core.gui_lib import publish, server_url, js_core_libs_format
+from ..gui_core import publish_gui_resources
+from ..gui_core.gui_core import getGuiPath, getPublishedLib, gui_folder_format, lib_folder_format, publishGui
+from ..plugin.registry import cdmGUI
+from ally.support.util_io import openURI
+from io import BytesIO
+import logging
+
+
+# --------------------------------------------------------------------
+
+log = logging.getLogger(__name__)
 
 # --------------------------------------------------------------------
 
 @ioc.start
 def publishJS():
     publishGui('livedesk-embed')
-    
-    
+
+@ioc.config
+def ui_demo_embed_file():
+    ''' the demo client html file '''
+    return 'index.html'
+
+@ioc.after(publish)
+def updateDemoEmbedFile():
+    if not publish_gui_resources(): return  # No publishing is allowed
+    try:
+        bootPath = lib_folder_format() % 'livedesk-embed/'
+        with openURI(getGuiPath(ui_demo_embed_file())) as f:
+            out = f.read().replace(b'{server_url}', bytes(server_url(), 'utf-8'))
+            out = out.replace(b'{gui}', bytes(gui_folder_format(), 'utf-8'));
+            out = out.replace(b'{lib_core}', bytes(lib_folder_format() % 'core/', 'utf-8'));
+            cdmGUI().publishFromFile(bootPath + ui_demo_embed_file(), BytesIO(out))
+    except:
+        log.exception('Error publishing demo client file')
+    else:
+        assert log.debug('Client demo script published:', server_url() + getPublishedLib('livedesk-embed/' + ui_demo_embed_file())) or True
