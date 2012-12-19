e57f55b03f24cf03aa4c9f5687938b1de3cf6890
ioan <ioan.pocol@sourcefabric.org>
2012-12-19 15:22:52 +0200

    35

--- plugins/media-archive/superdesk/media_archive/core/impl/query_service_creator.py
+++ plugins/media-archive/superdesk/media_archive/core/impl/query_service_creator.py
@@ -230,6 +230,8 @@ class QueryServiceAlchemy(SessionSupport):
                     if sqlUnion: sqlUnion = sqlUnion.union(sql)
                     else: sqlUnion = sql
 
+        if sqlUnion is None:
+            sqlUnion = buildSubquery(self, MetaInfoMapped, MetaDataMapped, qa, qi, qd)
 
         count = sqlUnion.count()
 
@@ -280,7 +282,8 @@ def buildPartialQuery(sqlQuery, query, queryLike, mapped, pQuery, andClauses=Non
         The mapped model class to use the query on.
     '''
 
-    assert isinstance(queryLike, QMetaDataInfo), 'Invalid query %s' % queryLike
+    if queryLike:
+        assert isinstance(queryLike, QMetaDataInfo), 'Invalid query %s' % queryLike
 
     ordered, unordered = [], []
     mapper = mappingFor(mapped)
