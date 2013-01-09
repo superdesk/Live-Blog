05d5924345d3e775c8179731aaa6e9129459f087
Mihai Nistor <Mihai.Nistor@sourcefabric.org>
2013-01-09 14:59:53 +0200

    NR-220: Blog Types
    Apply layout.

--- plugins/livedesk/gui-resources/scripts/js/views/configure/blog.js
+++ plugins/livedesk/gui-resources/scripts/js/views/configure/blog.js
@@ -17,7 +17,12 @@
             
         },
         save: function(evt){
-
+            var self = this,
+                data = {
+                    Language: self.el.find('[name="Language"]').val(),
+                    Type: self.el.find('[name="blogtypeselection"]:checked').val()
+                }
+            self.model.set(data).sync();
         },
         refresh: function() {
             var self = this;
@@ -29,7 +34,8 @@
         render: function() {
             var self = this;
             $.tmpl('livedesk>configure', self.model.feed(), function(e, o){
-                self.setElement(o);
+                self.el.html(o);
+                console.log(o);
                 self.languagesView = new LanguagesView({
                     tmpl: 'livedesk>configure/languages',
                     el: self.el.find('.languages'),
@@ -41,6 +47,47 @@
                     _parent: self,
                     theBlog: self.theBlog,
                     tmplData: { selected: self.model.get('Type').get('Id') }
+                });
+                var 
+                    topSubMenu = $(this.el).find('[is-submenu]'),
+                    content = $(this.el).find('[is-content]');
+                console.log(topSubMenu);
+                $(topSubMenu)
+                .off('click'+self.getNamespace(), 'a[data-target="configure-blog"]')
+                .on('click'+self.getNamespace(), 'a[data-target="configure-blog"]', function(event)
+                {
+                    event.preventDefault();
+                    var blogHref = $(this).attr('href')
+                    $.superdesk.getAction('modules.livedesk.configure')
+                    .done(function(action)
+                    {
+                        action.ScriptPath && 
+                            require([$.superdesk.apiUrl+action.ScriptPath], function(app){ new app(blogHref); });
+                    });
+                })
+                .off(self.getEvent('click'), 'a[data-target="manage-collaborators-blog"]')
+                .on(self.getEvent('click'), 'a[data-target="manage-collaborators-blog"]', function(event)
+                {
+                    event.preventDefault();
+                    var blogHref = $(this).attr('href')
+                    $.superdesk.getAction('modules.livedesk.manage-collaborators')
+                    .done(function(action)
+                    {
+                        action.ScriptPath && 
+                            require([$.superdesk.apiUrl+action.ScriptPath], function(app){ new app(blogHref); });
+                    });
+                })
+                .off('click'+self.getNamespace(), 'a[data-target="edit-blog"]')
+                .on('click'+self.getNamespace(), 'a[data-target="edit-blog"]', function(event)
+                {
+                    event.preventDefault();
+                    var blogHref = $(this).attr('href');
+                    $.superdesk.getAction('modules.livedesk.edit')
+                    .done(function(action)
+                    {
+                        action.ScriptPath && 
+                            require([$.superdesk.apiUrl+action.ScriptPath], function(EditApp){ EditApp(blogHref); });
+                    });
                 });
             });
         }
