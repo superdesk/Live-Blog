bf19156c533269a86bdeea7492f67042777b4bc2
Mihai Nistor <Mihai.Nistor@sourcefabric.org>
2013-01-09 13:49:15 +0200

    NR-220: Blog Types
    Configure on the way.

--- plugins/livedesk/gui-resources/scripts/js/models/blogtype.js
+++ plugins/livedesk/gui-resources/scripts/js/models/blogtype.js
@@ -7,6 +7,15 @@ define([
     	url: new Gizmo.Url('LiveDesk/BlogType'),
     	defaults: { 
     		PostPosts: Gizmo.Register.Posts
+    	},
+    	addSync: function(data){
+    		var self = this,
+    			ret = self.xfilter('Id,PostPosts').set(data).sync();
+    		ret.done(function(data){
+                self._parseHash(data);
+    			self.Class.triggerHandler('add', self);
+    		});
+    		return ret;
     	}
 	}, { register: 'BlogType' });
 });
--- plugins/livedesk/gui-resources/scripts/js/views/blogtypes.js
+++ plugins/livedesk/gui-resources/scripts/js/views/blogtypes.js
@@ -1,14 +1,17 @@
  define([
     'jquery', 
     'gizmo/superdesk',
-    config.guiJs('livedesk', 'views/blogtype'),    
+    config.guiJs('livedesk', 'views/blogtype'),
+    config.guiJs('livedesk', 'views/add-blogtype'),
     config.guiJs('livedesk', 'models/blogtypes'),
     
-    'tmpl!livedesk>blogtype/blogtypes',
-], function( $, Gizmo, BlogTypeView ) {
-   
+    'tmpl!livedesk>blogtype/blogtypes'
+], function( $, Gizmo, BlogTypeView, AddBlogTypeView ) {  
    return Gizmo.View.extend({
         tmplData: {},
+        events: {
+            '[name="add-blogtype-modal"]': { 'click': 'addBlogTypeModal' }
+        },
         init: function(){
             var self = this;
             if( !self.collection ) {
@@ -18,19 +21,24 @@
                 .on('read update', self.render, self)
                 .xfilter('Id,Name,PostPosts')
                 .sync();
+            self.collection.model.on('add',self.addOne, self);
         },
-        addOne: function(model){
-            var blogTypeView = new BlogTypeView({ model: model, tmplData: this.tmplData });
+        addOne: function(evt, model){
+            var blogTypeView = new BlogTypeView({ _parent: this, model: model, tmplData: this.tmplData });
             this.el.find('.blogtype-list').append(blogTypeView.el)
         },
         addAll: function(evt, data) {
             data = (data === undefined) ? this.collection._list : data;
             for( var i = 0, count = data.length; i < count; i++ ){
-                this.addOne(data[i]);
+                this.addOne(evt, data[i]);
             }
         },
         render: function(evt, data){
+            if(!$('#add-blogtype').length) {
+                $('body').append('<div id="content-add-blogtype"></div>');
+            }
             var self = this;
+            self.configBlogType = new AddBlogTypeView({ el: '#content-add-blogtype'});
             self.el
                 .off(self.getEvent('click'), 'input[name="blogtypeselection"]')
                 .on(self.getEvent('click'), 'input[name="blogtypeselection"]', function(){
@@ -45,8 +53,14 @@
                         
                     });
               });
-            self.el.tmpl('livedesk>blogtype/blogtypes', this.tmplData);
-            this.addAll(evt, data);
+
+            self.el.tmpl('livedesk>blogtype/blogtypes', this.tmplData, function(){
+                self.addAll(evt, data);
+            });
+        },
+        addBlogTypeModal: function(evt) {
+            var self = this;
+            self.configBlogType.switchModal(evt, 0);
         }
     });
 });
