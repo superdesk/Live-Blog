4596a304d641479dd528b5c5d13173faf8426dc6
Vlad Nicoara <vlad.nicoara@sourcefabric.org>
2013-01-18 16:02:30 +0200

    NR-595 : Update the welcome page
    Commiting early progress

--- plugins/livedesk/gui-resources/scripts/js/dashboard.js
+++ plugins/livedesk/gui-resources/scripts/js/dashboard.js
@@ -5,7 +5,8 @@ define
     config.guiJs('livedesk', 'models/blog'),
 	config.guiJs('livedesk', 'models/posttype'),
     config.guiJs('livedesk', 'models/post'),
-    'tmpl!livedesk>ceva'
+    config.guiJs('livedesk', 'models/liveblogs'),
+    'tmpl!livedesk>layouts/dashboard'
  ], 
 function(Gizmo, $) 
 {
@@ -14,7 +15,29 @@ function(Gizmo, $)
     DashboardApp = Gizmo.View.extend
     ({
         init: function(){
-            $(this.el).addClass(' myclass ')
+            $(this.el).addClass(' myclass ');
+            $(this.el).html('supernebunia');
+            //this.render();
+
+
+
+            this.collection = new Gizmo.Register.LiveBlogs;
+            this.collection.on('read update', this.render, this).
+                //xfilter('Content').sync();
+                sync();
+            //console.log(this.collection);
+        },
+        render: function(){
+            var self = this;
+            //console.log(this.collection);
+            
+            self.collection.each(function(){
+                console.log(this);
+            })
+
+            $.tmpl('livedesk>layouts/dashboard', {}, function(e,o) {
+                $(self.el).append(o);
+            });
         }
     }),
     dashboardApp = new DashboardApp();
@@ -22,6 +45,7 @@ function(Gizmo, $)
     return {
         init: function(element)
         { 
+            console.log(element);
             $(element).append( dashboardApp.el );
             return dashboardApp; 
         }
