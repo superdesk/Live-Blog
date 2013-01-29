e6d9018d0234e02f9133adb843ae24cee71ddb7c
Mihai Balaceanu <gizm0bill@gmail.com>
2013-01-29 13:37:17 +0200

    NR-617 : Test and fix media archive because of security changes / refactoring
    fixes

--- plugins/media-archive/gui-resources/scripts/js/models/languages.js
+++ plugins/media-archive/gui-resources/scripts/js/models/languages.js
@@ -1,5 +1,5 @@
 define([ 'gizmo/superdesk', config.guiJs('media-archive', 'models/language') ],
 function(giz, Language)
 {
-    return giz.Collection.extend({ model: Language, href: new giz.Url('Superdesk/Language') });
+    return giz.Collection.extend({ model: Language, href: new giz.Url('Localization/Language') });
 });
