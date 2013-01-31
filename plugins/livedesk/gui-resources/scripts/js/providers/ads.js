9e56e201361f6bf61d3ffac57461124fbe5a5e88
Mihai Nistor <Mihai.Nistor@sourcefabric.org>
2013-01-31 18:00:26 +0200

    NR-624: Timeline bugs
    Advertiments fixed.

--- plugins/livedesk/gui-resources/scripts/js/providers/ads.js
+++ plugins/livedesk/gui-resources/scripts/js/providers/ads.js
@@ -19,9 +19,27 @@ define('providers/ads', [
             getAds = function()
             {
                 posts = Gizmo.Auth(new Gizmo.Register.Posts(theBlog+'/PostType/advertisement/Post/Unpublished'));
-                posts.on('read update', function(evt,data){
-                    console.log('evt: ',evt)
-                }).sync();
+                posts
+                    .on('read update', function(evt,data){
+                        $.tmpl('livedesk>providers/ads/items', {Posts: posts.feed()}, function(e, o)
+                        {  
+                            $('.search-result-list', self.el).prepend(o);
+                            Action.get('modules.livedesk.blog-post-publish').done(function(action) {
+                                $('.search-result-list li', self.el).draggable
+                                ({
+                                    helper: 'clone',
+                                    appendTo: 'body',
+                                    zIndex: 2700,
+                                    start: function() 
+                                    {
+                                        $(this).data('data', self.adaptor.universal($(this)));
+                                    }
+                                });
+                            });
+                        });
+                    })
+                    .xfilter('CreatedOn,Content,PublishedOn,Type,Id,CId')
+                    .sync();
             };
             $(self.el).tmpl('livedesk>providers/ads', getAds);
         }
