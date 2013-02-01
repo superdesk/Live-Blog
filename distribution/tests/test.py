aefd9092d758691542fddd0b312003f05b7f204b
Gabriel Nistor <gabriel.nistor@sourcefabric.org>
2013-02-01 21:04:44 +0200

    NR-610 : Complete the security gateway
    Small changes.

--- distribution/tests/test.py
+++ distribution/tests/test.py
@@ -5,18 +5,15 @@ Created on Dec 21, 2012
 '''
 
 import re
-import timeit
+from urllib.parse import urlsplit, urlunsplit, urlencode
 
-def testperf():
-    pattern = base + re.escape('HR/User/') + '[0-9\\-]+' + re.escape('/Resource/') + '([0-9\\-]+)'
-    for k in range(100):
-        re.match(pattern, 'http://localhost:8080/resources/HR/User/1/Resource/%s' % k)
 
 if __name__ == '__main__':
-    base = re.escape('http://localhost:8080/resources/')
-    
-    pattern = base + re.escape('HR/User/') + '([0-9\\-]+)' + re.escape('/Resource/') + '([0-9\\-]+)'
-    print(pattern)
-    print(re.match(pattern, 'http://localhost:8080/resources/HR/User/1/Resource/2').groups())
-    
-    print(timeit.timeit(testperf, number=100))
+    base = re.compile('^resources(?:/|(?=\\.)|$)(.*)')
+    m = base.match('resources/LiveDesk/Blog/1/Admin/')
+    print(m.groups())
+    m = base.match('resources')
+    print(m.groups())
+    print(re.sub('[\\/]+', '/', re.sub('([^\w\\/]*)', '', '^resources(?://|(?=\\.)|$)(.*)')))
+    print(urlsplit('//localhost:8080/resources/?asa=as'))
+    print(urlunsplit(('http', 'localhost:8080', '/resources/', urlencode((('name', 'value'))), '')))
