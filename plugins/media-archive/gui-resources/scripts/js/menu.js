f8c8fd8d7a816fcb46626ecd47e4530b95de0ad9
Mihai Balaceanu <gizm0bill@gmail.com>
2013-02-04 16:39:14 +0200

    NR-617 : Test and fix media archive because of security changes / refactoring
    cleanup

--- plugins/media-archive/gui-resources/scripts/js/menu.js
+++ plugins/media-archive/gui-resources/scripts/js/menu.js
@@ -6,15 +6,6 @@ function($, superdesk, Action)
 {
     return { init: function() 
     {
-        Action.initApp('modules.media-archive.main');
-        return;
-        
-        superdesk.getAction('modules.media-archive.main')
-        .done(function(action)
-        {
-            if( !action ) return; 
-            if( action.Path == 'modules.media-archive.main' && action.Script )
-                require([action.Script.href], function(app){ app(); });
-        });
+        return Action.initApp('modules.media-archive.main');
     }};
 });
