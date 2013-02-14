53279bb4d87c42c1d48058170f944e332792427b
Mihai Balaceanu <gizm0bill@gmail.com>
2013-02-14 19:24:01 +0200

    NR-715 : 'Tweet' button links out to a back-end url only accessible to staff
    fixed

--- plugins/livedesk/gui-resources/scripts/js/edit-live-blogs.js
+++ plugins/livedesk/gui-resources/scripts/js/edit-live-blogs.js
@@ -1083,7 +1083,7 @@ function(providers, Gizmo, $, BlogAction)
 						self.el.find('[data-target="manage-collaborators-blog"]').css('display', 'none');
 					});
 					// refresh twitter share button
-					//require(['//platform.twitter.com/widgets.js'], function(){ twttr.widgets.load(); });
+					require(['//platform.twitter.com/widgets.js'], function(){ twttr.widgets.load(); });
 				    
 					var timelineCollection = Gizmo.Auth(new TimelineCollection());
 					timelineCollection.href.root(self.theBlog);
