39648783f96b9ea9d4d12bfa743047829f3b7913
Mugur Rus <mugur.rus@sourcefabric.org>
2013-02-27 12:59:07 +0200

    Merge branch devel

--- plugins/superdesk-language/test/superdesk/__init__.py
+++
@@ -1,4 +0,0 @@
-# Required in order to register the package extender whenever the unit test is run.
-if True:
-    import package_extender
-    package_extender.PACKAGE_EXTENDER.setForUnitTest(True)
