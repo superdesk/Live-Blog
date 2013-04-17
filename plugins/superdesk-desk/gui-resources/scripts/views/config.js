57276fb0b4b46afce1b2e04f7a8bef06bb29f0c0
petr.jasek <petr.jasek@sourcefabric.org>
2013-04-17 12:24:17 +0200

    Add initial AddDeskView.

--- plugins/superdesk-desk/gui-resources/scripts/views/config.js
+++ plugins/superdesk-desk/gui-resources/scripts/views/config.js
@@ -2,7 +2,8 @@ define([
     'backbone',
     'gizmo/superdesk',
     'tmpl!superdesk-desk>config',
-    'tmpl!superdesk-desk>config-desk'
+    'tmpl!superdesk-desk>config-desk',
+    'tmpl!superdesk-desk>add-desk'
 ], function(Backbone, Gizmo) {
 
     var Model = Backbone.Model.extend({
@@ -15,6 +16,8 @@ define([
     var UserCollection = Backbone.Collection.extend({
         model: User
     });
+
+    var deskHeaders = {'X-Filter': 'Id, Name, User'};
 
     var Desk = Model.extend({
         parse: function(response) {
@@ -44,9 +47,43 @@ define([
         }
     });
 
+    var AddDeskView = Backbone.View.extend({
+        events: {
+            'click .save': 'save',
+            'click .cancel': 'close'
+        },
+
+        render: function() {
+            $(this.el).tmpl('superdesk-desk>add-desk');
+            return this;
+        },
+
+        save: function(e) {
+            var data = {
+                'Name': $(this.el).find('#desk-name').val()
+            };
+
+            this.collection.create(data, {headers: deskHeaders});
+            this.close(e);
+        },
+
+        close: function(e) {
+            e.preventDefault();
+            var view = this;
+            $(this.el).find('#addDesk').modal('hide', function() {
+                view.remove();
+            });
+        }
+    });
+
     var ConfigView = Backbone.View.extend({
+        events: {
+            'click #add-desk': 'addDesk'
+        },
+
         initialize: function() {
             this.collection.on('reset', this.render, this);
+            this.collection.on('add', this.render, this);
         },
 
         render: function() {
@@ -62,7 +99,13 @@ define([
         },
 
         fetchCollection: function() {
-            this.collection.fetch({headers: {'X-Filter': 'Id, Name, User'}, reset: true});
+            this.collection.fetch({headers: deskHeaders, reset: true});
+        },
+
+        addDesk: function(e) {
+            e.preventDefault();
+            var view = new AddDeskView({collection: this.collection});
+            $(this.el).find('#modal-placeholder').html(view.render().el);
         }
     });
 
