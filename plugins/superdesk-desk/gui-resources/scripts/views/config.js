1c9153c9715377a93748822a0d4125fe7e1032ab
petr.jasek <petr.jasek@sourcefabric.org>
2013-04-17 17:36:36 +0200

    Add members list to config desks view.

--- plugins/superdesk-desk/gui-resources/scripts/views/config.js
+++ plugins/superdesk-desk/gui-resources/scripts/views/config.js
@@ -3,25 +3,64 @@ define([
     'gizmo/superdesk',
     'tmpl!superdesk-desk>config',
     'tmpl!superdesk-desk>config-desk',
-    'tmpl!superdesk-desk>add-desk'
+    'tmpl!superdesk-desk>add-desk',
+    'tmpl!superdesk-desk>edit-members',
+    'tmpl!superdesk-desk>select-user',
+    'tmpl!superdesk-desk>list-user'
 ], function(Backbone, Gizmo) {
 
     var Model = Backbone.Model.extend({
         idAttribute: 'Id'
     });
 
+    var userHeaders = {'X-Filter': 'Id, FullName, Name, EMail'};
     var User = Model.extend({
+        getData: function() {
+            return {
+                name: this.getName(),
+                email: this.get('EMail'),
+                id: this.id,
+                selected: this.get('selected')
+            };
+        },
+
+        getName: function() {
+            if (this.get('FullName')) {
+                return this.get('FullName');
+            } else {
+                return this.get('Name');
+            }
+        },
+
+        getResource: function() {
+            return {
+                Id: this.id,
+                FullName: this.get('FullName'),
+                Name: this.get('Name'),
+                EMail: this.get('Email')
+            };
+        }
     });
 
+    var url = new Gizmo.Url('HR/User');
     var UserCollection = Backbone.Collection.extend({
-        model: User
+        model: User,
+        url: url.get(),
+
+        parse: function(response) {
+            return response.UserList;
+        },
+
+        comparator: function(user) {
+            return user.getName().toLowerCase();
+        }
     });
 
     var deskHeaders = {'X-Filter': 'Id, Name, User'};
 
     var Desk = Model.extend({
         parse: function(response) {
-            this.users = new UserCollection({url: response.User.href});
+            this.users = new UserCollection([], {url: response.User.href});
             return response;
         }
     });
@@ -36,14 +75,133 @@ define([
         }
     });
 
+    var UserView = Backbone.View.extend({
+        tagName: 'li',
+
+        initialize: function() {
+            this.model.on('destroy', this.remove, this);
+        },
+
+        render: function() {
+            $(this.el).tmpl('superdesk-desk>list-user', this.model.getData());
+            return this;
+        }
+    });
+
+    var SelectUserView = Backbone.View.extend({
+        tagName: 'li',
+
+        events: {
+            'change input:checkbox': 'toggle'
+        },
+
+        render: function() {
+            $(this.el).tmpl('superdesk-desk>select-user', this.model.getData());
+            return this;
+        },
+
+        toggle: function() {
+            this.model.set('selected', !this.model.get('selected'));
+        }
+    });
+
+    var EditMembersView = Backbone.View.extend({
+        events: {
+            'click .save': 'save',
+            'click .cancel': 'close',
+            'change .toggle-all': 'toggleAll'
+        },
+
+        initialize: function() {
+            this.users = new UserCollection();
+            this.users.on('reset', this.renderUsers, this);
+        },
+
+        render: function() {
+            $(this.el).tmpl('superdesk-desk>edit-members');
+            this.users.fetch({reset: true, headers: userHeaders});
+            return this;
+        },
+
+        renderUsers: function() {
+            var selected = this.collection;
+            var list = $(this.el).find('.internal-collaborators').empty();
+            this.users.each(function(user) {
+                if (selected.findWhere({'Id': user.id})) {
+                    user.set('selected', true);
+                }
+
+                var view = new SelectUserView({model: user});
+                list.append(view.render().el);
+            });
+        },
+
+        save: function(e) {
+            var selected = new UserCollection(this.users.where({'selected': true}));
+            var members = this.collection;
+
+            console.log('selected', selected.pluck('Id'));
+            console.log('members', members.pluck('Id'));
+
+            members.each(function(user) {
+                if (!selected.findWhere({Id: user.id})) {
+                    user.destroy({wait: true});
+                }
+            });
+
+            selected.each(function(user) {
+                members.create(user.getResource());
+            });
+
+            this.close(e);
+        },
+
+        close: function(e) {
+            e.preventDefault();
+            $(this.el).find('#addMember').modal('hide', function() {
+                view.remove();
+            });
+        }
+    });
+
     var DeskView = Backbone.View.extend({
+        events: {
+            'click .edit-members': 'editMembers'
+        },
+
+        initialize: function() {
+            this.model.users.on('reset', this.renderMembers, this);
+            this.model.users.on('add', this.renderMembers, this);
+        },
+
         render: function() {
             var data = {
                 Name: this.model.get('Name')
             };
 
             $(this.el).tmpl('superdesk-desk>config-desk', data).addClass('desk-config');
+
+            this.fetchUsers();
             return this;
+        },
+
+        fetchUsers: function() {
+            this.model.users.fetch({headers: userHeaders, reset: true});
+        },
+
+        renderMembers: function() {
+            var list = $(this.el).find('.user-list').empty();
+            this.model.users.each(function(user) {
+                var view = new UserView({model: user});
+                list.append(view.render().el);
+            });
+        },
+
+        editMembers: function(e) {
+            e.preventDefault();
+            var view = new EditMembersView({collection: this.model.users});
+            $('#modal-placeholder').html(view.render().el);
+            $(this.el).find('#addMember').modal('show');
         }
     });
 
@@ -105,7 +263,7 @@ define([
         addDesk: function(e) {
             e.preventDefault();
             var view = new AddDeskView({collection: this.collection});
-            $(this.el).find('#modal-placeholder').html(view.render().el);
+            $('#modal-placeholder').html(view.render().el);
         }
     });
 
