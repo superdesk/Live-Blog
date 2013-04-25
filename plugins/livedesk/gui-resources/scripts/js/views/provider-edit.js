3e4dd8e33d1642515cd3eb05acbd7a029bf1e9d4
petr.jasek <petr.jasek@sourcefabric.org>
2013-04-25 16:33:35 +0200

    LB-499: Add provider no action
    Use xfilter for creating new providers so it gets
    data needed for rendering.

--- plugins/livedesk/gui-resources/scripts/js/views/provider-edit.js
+++ plugins/livedesk/gui-resources/scripts/js/views/provider-edit.js
@@ -1,56 +1,45 @@
 define(['jquery', 'backbone', 'tmpl!livedesk>manage-feeds-edit'], function($, Backbone) {
-    /**
-     * Edit provider view
-     */
     return Backbone.View.extend({
         events: {
             'click #edit-provider-save': 'save',
             'click #edit-provider-cancel': 'close',
-            'click .close': 'close'
+            'click .close': 'close',
+            'submit form': 'save'
         },
 
         initialize: function() {
-            this.render(); // render on init
+            if (!this.model) {
+                this.model = new this.collection.model();
+            }
         },
 
         render: function() {
-            $(this.el).tmpl('livedesk>manage-feeds-edit');
-            this.title = $(this.el).find('#edit-provider-title');
-            this.url = $(this.el).find('#edit-provider-url');
+            $(this.el).tmpl('livedesk>manage-feeds-edit', this.model.render());
             this.modal = $(this.el).find('.modal');
-
-            if (this.model) {
-                this.title.val(this.model.get('Name'));
-                this.url.val(this.model.get('URI'));
-                $(this.el).find('h3').text(_('Edit Provider ' + this.model.get('Name')));
-            } else {
-                $(this.el).find('h3').text(_('Add Provider'));
-            }
-
-            this.options.target.append(this.el);
             this.modal.modal('show');
             return this;
         },
 
-        save: function() {
+        save: function(e) {
+            e.preventDefault();
             var data = {
-                Name: this.title.val(),
-                URI: this.url.val()
+                Name: $(this.el).find('#edit-provider-title').val(),
+                URI: $(this.el).find('#edit-provider-url').val()
             };
 
-            if (!this.model) {
-                this.collection.create(data);
+            if (this.model.isNew()) {
+                this.collection.create(data, {headers: this.collection.xfilter, wait: true});
             } else {
-                this.model.save(data);
+                this.model.save(data, {patch: true});
             }
 
-            return this.close();
+            return this.close(e);
         },
 
-        close: function() {
-            this.modal.modal('hide');
-            this.remove();
-            return false;
+        close: function(e) {
+            e.preventDefault();
+            var view = this;
+            this.modal.modal('hide', function() { view.remove(); });
         }
     });
 });
