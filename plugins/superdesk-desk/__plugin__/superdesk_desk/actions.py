a8916612e4e80cdca9d3c858af0887d29f085b73
petr.jasek <petr.jasek@sourcefabric.org>
2013-04-26 13:09:29 +0200

    NR-913: Add desks in app menu

--- plugins/superdesk-desk/__plugin__/superdesk_desk/actions.py
+++ plugins/superdesk-desk/__plugin__/superdesk_desk/actions.py
@@ -4,6 +4,7 @@ from acl.right_action import RightAction
 from gui.action.api.action import Action
 from __plugin__.acl import gui
 from __plugin__.gui_core.gui_core import publishedURI
+from __plugin__.gui_action import defaults
 from __plugin__.gui_action.service import addAction
 import __plugin__.superdesk.actions as superdesk
 from superdesk.desk.api.desk import IDeskService
@@ -12,18 +13,18 @@ support.listenToEntities(Action, listeners=addAction)
 support.loadAllEntities(Action)
 
 @ioc.entity
-def configMenuAction() -> Action:
-    script=publishedURI('superdesk-desk/scripts/config.js')
-    return Action('desks', Parent=superdesk.menuAction(), Label=_('menu', 'Desks'), NavBar='config/desks', Script=script)
+def desksMenuAction() -> Action:
+    return Action('desks', Parent=defaults.menuAction(), Label=_('menu', 'Desks'))
 
 @ioc.entity
-def deskConfigView() -> RightAction:
-    return gui.actionRight(_('security', 'Desk Config View'), _('security', 'Allows desks configuration.'))
+def desksListMenuAction() -> Action:
+    script = publishedURI('superdesk-desk/scripts/actions/desks.js')
+    return Action('desks:list', Parent=desksMenuAction(), Script=script)
 
 @ioc.entity
 def configMenuAction() -> Action:
-    return Action('config:desks', Parent=superdeskActions.menuConfigAction(), Label=_('menu', 'Desks'),\
-                  NavBar='config/desks', Script=publishedURI('superdesk-desk/scripts/actions/config.js'))
+    script = publishedURI('superdesk-desk/scripts/actions/config.js')
+    return Action('config:desks', Parent=superdesk.menuAction(), Label=_('menu', 'Desks'), NavBar='config/desks', Script=script)
 
 @ioc.entity
 def deskView() -> RightAction:
@@ -37,4 +38,10 @@ def deskConfigView() -> RightAction:
 def registerConfigView():
     r = deskConfigView()
     r.addActions(configMenuAction())
+    r.allGet(IDeskService)
+
+@gui.setup
+def registerDeskView():
+    r = deskView()
+    r.addActions(desksMenuAction(), desksListMenuAction())
     r.allGet(IDeskService)
