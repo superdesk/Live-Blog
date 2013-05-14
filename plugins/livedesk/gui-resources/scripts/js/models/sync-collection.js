a87856c1e7b7eab4ff67ec6f3f4bce2286e91b08
petr.jasek <petr.jasek@sourcefabric.org>
2013-05-14 17:03:05 +0300

    LB-392: Autopublish - chaining blogs
    Add chaining blog autopublish control.

---
+++ plugins/livedesk/gui-resources/scripts/js/models/sync-collection.js
@@ -0,0 +1,20 @@
+define(['backbone', 'gizmo/superdesk', config.guiJs('livedesk', 'models/sync')],
+function(Backbone, Gizmo, SyncModel) {
+    return Backbone.Collection.extend({
+        model: SyncModel,
+        xfilter: {'X-Filter': 'Id, Auto, Source.Id'},
+
+        parse: function(response) {
+            return response.SyncList;
+        },
+
+        findSource: function(sourceId) {
+            return this.findWhere({SourceId: sourceId});
+        },
+
+        isAuto: function(sourceId) {
+            var sync = this.findSource(sourceId);
+            return Boolean(sync) && sync.get('Auto') === 'True';
+        }
+    });
+});
