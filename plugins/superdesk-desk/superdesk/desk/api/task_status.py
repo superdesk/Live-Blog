f77af60e60ee954311e05894a0081a57f3ef1590
Ioan Pocol <ioan.pocol@sourcefabric.org>
2013-05-21 18:51:22 +0300

    NR-961 : Cards (Kanban) plugin

--- plugins/superdesk-desk/superdesk/desk/api/task_status.py
+++ plugins/superdesk-desk/superdesk/desk/api/task_status.py
@@ -10,7 +10,7 @@ API specifications for task statuses.
 '''
 
 from ally.api.config import service
-from ally.support.api.entity  import Entity, IEntityGetService, IEntityFindService
+from ally.support.api.entity  import Entity, IEntityService
 from superdesk.api.domain_superdesk import modelDesk
 
 # --------------------------------------------------------------------
@@ -28,7 +28,7 @@ class TaskStatus(Entity):
 # --------------------------------------------------------------------
 
 @service((Entity, TaskStatus))
-class ITaskStatusService(IEntityGetService, IEntityFindService):
+class ITaskStatusService(IEntityService):
     '''
     Provides the service methods for the task statuses.
     '''
