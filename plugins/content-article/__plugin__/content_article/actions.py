c52e64b8d7587a2bfe450ff78bda771ab3ca0e6e
Mugur Rus <mugur.rus@sourcefabric.org>
2013-05-24 19:20:27 +0300

    Set the article edit rights

--- plugins/content-article/__plugin__/content_article/actions.py
+++ plugins/content-article/__plugin__/content_article/actions.py
@@ -1,7 +1,7 @@
 '''
 Created on May 3rd, 2012
 
-@package: Livedesk 
+@package: Livedesk
 @copyright: 2011 Sourcefabric o.p.s.
 @license:  http://www.gnu.org/licenses/gpl-3.0.txt
 @author: Mihai Balaceanu
@@ -18,7 +18,7 @@ from ally.container import ioc, support
 from ally.internationalization import NC_
 from gui.action.api.action import Action
 from content.article.api.article import IArticleService
-    
+
 # --------------------------------------------------------------------
 
 support.listenToEntities(Action, listeners=addAction)
@@ -26,24 +26,24 @@ support.loadAllEntities(Action)
 
 # --------------------------------------------------------------------
 
-@ioc.entity   
+@ioc.entity
 def menuAction() -> Action:
     return Action('article', Parent=defaults.menuAction(), Label=NC_('menu', 'Article'), NavBar='/article',
                   Script=publishedURI('superdesk/article/scripts/js/menu.js'))
 
-@ioc.entity   
+@ioc.entity
 def modulesAction() -> Action:
     return Action('article', Parent=defaults.modulesAction())
 
-@ioc.entity   
+@ioc.entity
 def modulesAddAction() -> Action:
     return Action('add', Parent=modulesAction(), Script=publishedURI('superdesk/article/scripts/js/add.js'))
 
-@ioc.entity   
+@ioc.entity
 def modulesEditAction() -> Action:
     return Action('edit', Parent=modulesAction(), Script=publishedURI('superdesk/article/scripts/js/edit.js'))
 
-@ioc.entity   
+@ioc.entity
 def modulesMainAction() -> Action:
     return Action('list', Parent=modulesAction(), Script=publishedURI('superdesk/article/scripts/js/list.js'))
 
@@ -54,10 +54,21 @@ def rightArticleView() -> RightAction:
     return gui.actionRight(NC_('security', 'Article view'), NC_('security', '''
     Allows read only access to Article.'''))
 
+@ioc.entity
+def rightArticleEdit() -> RightAction:
+    return gui.actionRight(NC_('security', 'Article edit'), NC_('security', '''
+    Allows read/write access to Article.'''))
+
 # --------------------------------------------------------------------
-    
+
 @gui.setup
-def registerAclMediaArchiveView():
+def registerAclArticleView():
     r = rightArticleView()
     r.addActions(menuAction(), modulesAction(), modulesMainAction(), modulesAddAction(), modulesEditAction())
     r.allGet(IArticleService)
+
+@gui.setup
+def registerAclArticleEdit():
+    r = rightArticleEdit()
+    r.addActions(menuAction(), modulesAction(), modulesMainAction(), modulesAddAction(), modulesEditAction())
+    r.all(IArticleService)
