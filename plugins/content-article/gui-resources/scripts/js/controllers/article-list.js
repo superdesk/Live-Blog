b7f32666571e5aafd8e26264346ed69600f51c8f
Ozan Ozbek <ozan.ozbek@sourcefabric.org>
2013-06-13 12:43:00 +0200

    work in progress

--- plugins/content-article/gui-resources/scripts/js/controllers/article-list.js
+++ plugins/content-article/gui-resources/scripts/js/controllers/article-list.js
@@ -3,6 +3,12 @@ function(angular) {
     'use strict';
 
     return function($scope, $q, ArticleListLoader) {
+        $('body').removeClass().addClass('article-list'); //
+
         $scope.articles = ArticleListLoader();
+        
+        $q.when($scope.articles).then(function(articles){
+            console.log(articles);
+        });
     };
 });
