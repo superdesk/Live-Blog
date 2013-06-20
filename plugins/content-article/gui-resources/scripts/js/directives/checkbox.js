ac5758e9ff39a7840c7df9a42edcb635e6db7fc6
Ozan Ozbek <ozan.ozbek@sourcefabric.org>
2013-06-20 15:45:04 +0200

    work in progress.

--- plugins/content-article/gui-resources/scripts/js/directives/checkbox.js
+++ plugins/content-article/gui-resources/scripts/js/directives/checkbox.js
@@ -9,8 +9,29 @@ function(angular, $) {
     module.directive('sdCheckbox', function() {
         return {
             restrict: 'A',
+            requires: 'ngModel',
             link: function(scope, element, attrs) {
-                console.log(element);
+                var isChecked = '';
+                if ($(element).attr('checked') == 'checked') {
+                    isChecked = 'sf-checked';
+                }
+                $(element).wrap('<span class="sf-checkbox-custom ' + isChecked + '"></span>');
+                $(element).hide();
+
+                var setBg = $(element).attr('set-bg'); 
+                if (typeof setBg !== undefined && setBg !== false && $(element).attr('checked') == 'checked') {
+                    $(this).parents().eq(setBg).toggleClass('active-bg');
+                }
+
+                element.parent().on('click', function(e){
+                    e.preventDefault();
+                    $(this).toggleClass('sf-checked');
+
+                    
+                    
+                    return false;
+                });
+                //*/
             }
         };
     });
