d8cf7cd1fed9073426540ecb5e79fce5f5dfc76b
Mihai Nistor <Mihai.Nistor@sourcefabric.org>
2013-07-08 13:44:46 +0300

    LB-291: Embed: Implement a Dynamic Plugin system for application

--- plugins/livedesk-embed/gui-resources/scripts/js/main.js
+++ plugins/livedesk-embed/gui-resources/scripts/js/main.js
@@ -1,17 +1,8 @@
 require(['livedesk-embed/concat.min'], function(){
 	require([
-		'jquery',
-		'livedesk-embed/views/timeline',
-        'livedesk-embed/views/user-comments-popup',
-		'livedesk-embed/plugins',
-		'jquery/cookie',
-		'i18n!livedesk_embed'
-	], function( $, TimelineView, UserCommentsPopupView ){
+		'jquery'
+	], function( $ ){
 		var data = { url: livedesk.blog };
-/*		if(livedesk.language) {
-			$.cookie('superdesk.langcode',livedesk.language);
-		}
-*/
 		if( livedesk.el !== undefined) {
 			if($(livedesk.el).length !== 0)
 				data.el = livedesk.el;
@@ -19,21 +10,7 @@ require(['livedesk-embed/concat.min'], function(){
 				console.log('Element: ',livedesk.el,' not found!');
 		}
 
-        var timeline = new TimelineView(data);
-        $.dispatcher.on('after-render', function() {
-        	var embedConfig = false;
-        	try {
-        		embedConfig = JSON.parse(this.model.get('EmbedConfig'));
-        	} catch(e){}
-            if (embedConfig && embedConfig.UserComments) {
-                new UserCommentsPopupView({
-                    el: '#liveblog-header', 
-                    timeline: timeline,
-                    model: timeline.model
-                });
-            } else {
-            	this.el.find('#comment-btn,.comment-box').hide();
-            }
-        });
+		var timeline = new TimelineView(data);
+
 	});
 });
---
+++ plugins/livedesk-embed/gui-resources/scripts/js/views/blog.js
@@ -0,0 +1,12 @@
+define([
+	'jquery',
+	'gizmo/view-events',
+	'livedesk-embed/dispatcher',
+	'jquery/tmpl',
+	'livedesk-embed/models/blog',
+	'jquery/xdomainrequest'
+], function($, Gizmo) {
+	return Gizmo.View.extend({
+
+	});
+});
