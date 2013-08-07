daf013732a71d99e749e298ace75c1415169ddc5
Ozan Ozbek <ozan.ozbek@sourcefabric.org>
2013-08-07 14:07:22 +0200

    LB-845: Refactor user management by using AngularJS
    Completed listing users.

--- plugins/superdesk-user/gui-resources/scripts/js/controllers/user-list.js
+++ plugins/superdesk-user/gui-resources/scripts/js/controllers/user-list.js
@@ -4,7 +4,62 @@ function(angular) {
 
     return function($scope, $q, User, UserListLoader) {
         
-        
+        $scope.initialize = function() {
+            $scope.loadSettings();
+
+            $scope.pageMax = 1;
+            $scope.page = 1;
+
+            $scope.$watch('page', function(page) {
+                $scope.loadUsers();
+            });
+            $scope.$watch('settings', function(settings, oldSettings) {
+                $scope.saveSettings();
+                if (settings.itemsPerPage !== oldSettings.itemsPerPage) {
+                    $scope.loadUsers();
+                }
+            }, true);
+            $scope.$watch('users', function(users){
+                console.log(users);
+                if (users !== undefined) {
+                    $scope.pageMax = Math.ceil(users.count / $scope.settings.itemsPerPage);
+                    if ($scope.pageMax === 0) {
+                        $scope.pageMax = 1;
+                    }
+                    delete users.count;
+                    $scope.users = users;
+                }
+            });
+            $scope.$watch('searchTerm', function(searchTerm){
+                if (searchTerm === undefined) {
+                    searchTerm = '';
+                }
+                $scope.loadUsers();
+            });
+        };
+
+        $scope.saveSettings = function() {
+            localStorage.setItem('superdesk.userList.settings', angular.toJson($scope.settings));
+        };
+        $scope.loadSettings = function() {
+            $scope.settings = angular.fromJson(localStorage.getItem('superdesk.userList.settings'));
+            if ($scope.settings === null) {
+                $scope.settings = {
+                    itemsPerPage: 25
+                };
+            }
+        };
+
+        $scope.loadUsers = function() {
+            var limit = $scope.settings.itemsPerPage;
+            var offset = ($scope.page - 1) * limit;
+
+            $scope.users = UserListLoader(offset, limit, $scope.searchTerm);
+        };
+
+        //
+
+        $scope.initialize();
 
     };
 });
--- plugins/superdesk-user/gui-resources/scripts/js/resources.js
+++ plugins/superdesk-user/gui-resources/scripts/js/resources.js
@@ -7,8 +7,8 @@ define([
     var resources = angular.module('users.resources', ['ngResource']);
 
     resources.factory('User', ['$resource', '$q', function($resource, $q) {
-        return $resource('/resources/Content/Article/:Id/:Action', {Id: '@Id', Action: '@Action'}, {
-            query: {method: 'GET', params: {'X-Filter': '*,Author.*'}},
+        return $resource('/resources/HR/User/:Id/:Action', {Id: '@Id', Action: '@Action'}, {
+            query: {method: 'GET', params: {'X-Filter': '*'}},
             update: {method: 'PUT'},
             save: {method: 'POST', params: {'X-Filter': 'Id'}}
         });
@@ -21,18 +21,20 @@ define([
             if (searchTerm !== undefined && searchTerm !== '') {
                 parameters.search = searchTerm;
             }
-            Article.query(parameters, function(response) {
-                for (var i = 0; i < response.ArticleList.length; i = i + 1) {
-                    response.ArticleList[i].checked = false;
-                    response.ArticleList[i].Content = angular.fromJson(response.ArticleList[i].Content);
+            User.query(parameters, function(response) {
+                /*
+                for (var i = 0; i < response.UserList.length; i = i + 1) {
+                    response.UserList[i].checked = false;
+                    response.UserList[i].Content = angular.fromJson(response.UserList[i].Content);
                     if (response.ArticleList[i].IsPublished === 'True') {
                         response.ArticleList[i].IsPublished = true;
                     } else {
                         response.ArticleList[i].IsPublished = false;
                     }
                 }
-                response.ArticleList.count = response.total;
-                delay.resolve(response.ArticleList);
+                */
+                response.UserList.count = response.total;
+                delay.resolve(response.UserList);
             });
             return delay.promise;
         };
