2dc780fc788fe599cb93c8dca8d17105121875cd
Nikola Nikolic <nikolic_nikola@live.com>
2013-09-05 13:37:05 +0200

    Custom checkbox - directive
    Custom checkbox directive written, and added on article list screen.

--- plugins/content-article/gui-resources/scripts/js/directives/checkbox.js
+++ plugins/content-article/gui-resources/scripts/js/directives/checkbox.js
@@ -1,18 +1,28 @@
-define([
-    'angular',
-    'jquery'
-],
-function(angular, $) {
-    
-    var module = angular.module('directives', ['resources']);
+define(['angular'],
+function(angular) {
 
-    module.directive('sdCheckbox', function() {
+    var module = angular.module('directives', []);
+
+    module.directive('sfcheck', function() {
         return {
-            restrict: 'A',
-            requires: 'ngModel',
-            scope: false,
-            link: function(scope, element, attrs) {
-                
+            restrict: 'E',
+            replace: true,
+            template : '<span class="sf-checkbox-custom" ng-class="{\'sf-checked\':check}" ></span>',
+            scope: {
+                check : "=",
+                clickevent : "&"
+            },
+            link: function(scope, element, attrs, controller) {
+                var scopeCaller = false;
+                element.bind('click', function() {
+                    scope.$apply(function(){
+                        scope.check = !scope.check;
+                        scopeCaller=true;
+                    });
+                });
+                scope.$watch('check', function(value) {
+                    scopeCaller ? (scopeCaller = false, scope.clickevent()) : '';
+                });
             }
         };
     });
