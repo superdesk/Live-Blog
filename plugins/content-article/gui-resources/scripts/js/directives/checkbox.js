184d1a96a9d80380fd039c96a69201fa7ba0c249
Nikola Nikolic <nikolic_nikola@live.com>
2013-09-09 12:50:03 +0200

    Custom toggle - directive
    Custom toggle element added
    Use of toggle demonstrated thorugh article publish - unpublish
    functionality

--- plugins/content-article/gui-resources/scripts/js/directives/checkbox.js
+++ plugins/content-article/gui-resources/scripts/js/directives/checkbox.js
@@ -3,6 +3,7 @@ function(angular) {
 
     var module = angular.module('directives', []);
 
+    //use <sfcheck check="modelVariable" clickevent="OnClickEventListener" />
     module.directive('sfcheck', function() {
         return {
             restrict: 'E',
@@ -11,6 +12,33 @@ function(angular) {
             scope: {
                 check : "=",
                 clickevent : "&"
+            },
+            link: function(scope, element, attrs, controller) {
+                var scopeCaller = false;
+                element.bind('click', function() {
+                    scope.$apply(function(){
+                        scope.check = !scope.check;
+                        scopeCaller=true;
+                    });
+                });
+                scope.$watch('check', function(value) {
+                    scopeCaller ? (scopeCaller = false, scope.clickevent()) : '';
+                });
+            }
+        };
+    });
+
+    //use <sftoggle check="modelVariable" clickevent="OnClickEventListener" />
+    //for on/off type of toggle use attribute  >>  onoff="true" 
+    module.directive('sftoggle', function() {
+        return {
+            restrict: 'E',
+            replace: true,
+            template : '<div class="sf-toggle-custom" ng-class="{\'sf-checked\':check, \'on-off-toggle\' : onoff}"><div class="sf-toggle-custom-inner"></div></div>',
+            scope: {
+                check : "=",
+                clickevent : "&",
+                onoff : "="
             },
             link: function(scope, element, attrs, controller) {
                 var scopeCaller = false;
