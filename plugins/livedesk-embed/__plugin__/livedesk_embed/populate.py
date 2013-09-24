bbc3a7bb72aef3bdecc9b7425b79a219f9234791
Vlad Nicoara <vlad.nicoara@sourcefabric.org>
2013-09-24 17:42:35 +0300

    LB-1097: create 2 new themes for liveblog embed
    done

--- plugins/livedesk-embed/__plugin__/livedesk_embed/populate.py
+++ plugins/livedesk-embed/__plugin__/livedesk_embed/populate.py
@@ -26,7 +26,7 @@ log = logging.getLogger(__name__)
 def insertThemes():
     s = entityFor(IBlogThemeService)
     assert isinstance(s, IBlogThemeService)
-    for name in ('default', 'tageswoche', 'stt', 'genapp', 'big-screen', 'zeit', 'ctkepr', 'ctkeu', 'ctkih', 'ctkno', 'ctkr', 'aamulehti', 'satakansa', 'okfn', 'sasa', 'sasa-light'):
+    for name in ('default', 'tageswoche', 'stt', 'genapp', 'big-screen', 'zeit', 'ctkepr', 'ctkeu', 'ctkih', 'ctkno', 'ctkr', 'aamulehti', 'satakansa', 'okfn', 'sasa', 'sasa-light', 'ksml', 'ksml-light'):
         q = QBlogTheme()
         q.name = name
         l = s.getAll(q=q)
---
+++ plugins/livedesk-embed/gui-themes/themes/ksml.js
@@ -0,0 +1,25 @@
+requirejs.config({
+	urlArgs: 'v=40',
+	paths: {
+		'theme': 'livedesk-embed/themes/ksml'
+	}
+});
+require(['ksml.min'], function() {
+	require(['../scripts/js/config'], function(){
+		var name;
+		for(name in livedesk.theme) {
+			define('tmpl!theme/'+name, ['dust/compiler'], function(dust){
+				dust.loadSource(dust.compile(livedesk.theme[name],'theme/'+name));
+			});		
+		}
+		function loadCss(url) {
+			var link = document.createElement("link");
+			link.type = "text/css";
+			link.rel = "stylesheet";
+			link.href = url;
+			document.getElementsByTagName("head")[0].appendChild(link);
+		}
+		loadCss(require.toUrl('theme/livedesk.css'));//'css!theme/livedesk', 
+		require(['livedesk-embed/main']);
+	});
+});
