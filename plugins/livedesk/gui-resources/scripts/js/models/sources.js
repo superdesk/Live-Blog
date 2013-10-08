fe5500f04afda902f1dbef025fe4c418f6b07dc2
Mihai Nistor <Mihai.Nistor@sourcefabric.org>
2013-10-08 12:40:03 +0300

    LB-879: Add new functionality to Live Blog posts needed for the Citizen Desk verification workflow - assign post, change status
    Added users collection.

--- plugins/livedesk/gui-resources/scripts/js/models/sources.js
+++ plugins/livedesk/gui-resources/scripts/js/models/sources.js
@@ -1,7 +1,10 @@
-define(['gizmo/superdesk', config.guiJs('livedesk', 'models/sources')], function(Gizmo)
-{
+define([
+	'gizmo/superdesk', 
+	config.guiJs('livedesk', 'models/source')
+], function(Gizmo) {
+
     return Gizmo.Collection.extend({
-    	url: new Gizmo.Url('Data/Source'), 
+    	url: new Gizmo.Url('Data/Source'),
     	model: Gizmo.Register.Source
     }, { register: 'Sources' } );
 });
