bcddb21e6cedb5af3c6a66d6f029e181f9c27b73
Patricia Garcia <pat@patriciagarcia.me>
2013-10-09 11:21:57 +0200

    VLB-4: Add first version of new theme

---
+++ plugins/livedesk-embed/gui-themes/themes/live-dashboard.min.js
@@ -0,0 +1,10 @@
+define([
+	'plugins/twitter-widgets',
+	'css!theme/liveblog',
+  'tmpl!theme/container'
+], function(){
+	return {
+		//enviroments: [ 'mobile', 'desktop', 'quirks' ],
+		plugins: []
+	}
+});
