8459d653f8ed02abefea0bbca857c65e39cb66d3
Patricia Garcia <pat@patriciagarcia.me>
2013-10-10 15:08:43 +0200

    VLB-14 Display embed tweets on slider
    twitter_widgets plugin is not working for the live dashboard, because it only displays twitter cards on scrolling.
    Temporary plugin added, remove when regular plugin fixed.

---
+++ plugins/livedesk-embed/gui-resources/scripts/js/plugins/dashboard-twitter-widgets.js
@@ -0,0 +1,23 @@
+requirejs.config({
+    paths: { "twitterWidgets": "//platform.twitter.com/widgets" }
+});
+define([
+	'jquery',
+	'plugins',
+  'twitterWidgets',
+	'dispatcher'
+], function($, plugins){
+	return plugins["dashboard-twitter-widgets"] = function(config){
+		$.dispatcher.on('post-view.rendered-after-/item/source/twitter', function(){
+			var self = this;
+      window.twttr.widgets.createTweet(
+        self.templateData.Meta.id_str,
+        self.el.find('.post-content-full').get(0),
+        function(){
+          self.el.find('.post-core-content').remove();
+        },
+        { cards: 'all' }
+      );
+		});
+	}
+});
--- plugins/livedesk-embed/gui-themes/themes/live-dashboard.js
+++ plugins/livedesk-embed/gui-themes/themes/live-dashboard.js
@@ -1,12 +1,12 @@
 define([
-  'plugins/twitter-widgets',
   'plugins/live-dashboard-sliders',
+  'plugins/dashboard-twitter-widgets',
 	'css!theme/liveblog',
   'css!theme/jquery.bxslider',
   'tmpl!theme/container'
 ], function(){
 	return {
 		//enviroments: [ 'mobile', 'desktop', 'quirks' ],
-		plugins: ['twitter-widgets', 'live-dashboard-sliders']
+    plugins: ['dashboard-twitter-widgets', 'live-dashboard-sliders']
 	}
 });
