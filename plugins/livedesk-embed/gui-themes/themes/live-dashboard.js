3d86ac4e8f018a6e869a8fcba72d2901dba91c66
FranLlull <trizztz@gmail.com>
2013-10-14 00:06:26 +0200

    VLB-10: Styling of the app

--- plugins/livedesk-embed/gui-themes/themes/live-dashboard.js
+++ plugins/livedesk-embed/gui-themes/themes/live-dashboard.js
@@ -3,7 +3,8 @@ define([
   'plugins/dashboard-twitter-widgets',
 	'css!theme/liveblog',
   'css!theme/jquery.bxslider',
-  'tmpl!theme/container'
+  'tmpl!theme/container',
+  'tmpl!theme/item/base'
 ], function(){
 	return {
 		//enviroments: [ 'mobile', 'desktop', 'quirks' ],
