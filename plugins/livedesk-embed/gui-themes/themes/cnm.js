8bd86bde28c68c374f0ecb6ff5123422877ee708
nistormihai <mihai.nistor@sourcefabric.org>
2013-11-21 17:37:34 +0200

    Update cnm.js

--- plugins/livedesk-embed/gui-themes/themes/cnm.js
+++ plugins/livedesk-embed/gui-themes/themes/cnm.js
@@ -1,5 +1,5 @@
 requirejs.config({
-	urlArgs: 'v=100',
+	urlArgs: 'v=110',
 	paths: {
 		'theme': 'livedesk-embed/themes/cnm'
 	}
@@ -22,4 +22,13 @@ require(['cnm.min'], function() {
 		loadCss(require.toUrl('theme/livedesk.css'));//'css!theme/livedesk', 
 		require(['livedesk-embed/main']);
 	});
+	var _gaq = _gaq || [];
+	_gaq.push(['_setAccount', 'UA-45928067-1']);
+	_gaq.push(['_trackPageview']);
+	
+	(function() {
+	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	})();
 });
