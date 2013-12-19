a458cc29f5a80bfde57da28dd2d509153a88cc70
Patricia Garcia <pat@patriciagarcia.me>
2013-12-19 12:17:50 +0100

    LB-1339 Embed not loading if adblock is turned on

--- plugins/livedesk-embed/gui-resources/scripts/js/plugins/twitter-widgets.js
+++ plugins/livedesk-embed/gui-resources/scripts/js/plugins/twitter-widgets.js
@@ -7,44 +7,48 @@ define([
 	'dust',
 	'jquery/waypoints',
 	'dispatcher',
-	'twitterWidgets'
 ], function($, plugins, dust, waypoints){
 	return plugins["twitter-widgets"] = function(config){
-		$.dispatcher.on('rendered-after.post-view', function(){
-      if(this.shortItem != '/item/source/twitter')
-				return;
-      if(!this._parent._twitterPosts)
-        this._parent._twitterPosts = [];
-      this._parent._twitterPosts.push(this);
-		});
-    $.dispatcher.on('added-pending.posts-view', function(){
-      addWaypoints(this);
-    });
-    $.dispatcher.on('add-all.posts-view', function(){
-      addWaypoints(this);
-    });
-
-    var addWaypoints = function(self){
-      if(!self._twitterPosts)
-        return;
-      $.each(self._twitterPosts, function(index, post){
-        post.el.waypoint(function(){
-          window.twttr.widgets.createTweet(
-            post.templateData.Meta.id_str,
-            post.el.find('.post-content-full').get(0),
-            function(){
-              post.el.find('.post-core-content').remove();
-            },
-            { cards: 'all' }
-          );
-        },
-        {
-          triggerOnce: true,
-          offset: '120%',
-          context: self.el
-        });
+    require(['twitterWidgets'], function(){
+      $.dispatcher.on('rendered-after.post-view', function(){
+        if(this.shortItem != '/item/source/twitter')
+          return;
+        if(!this._parent._twitterPosts)
+          this._parent._twitterPosts = [];
+        this._parent._twitterPosts.push(this);
       });
-      self._twitterPosts = [];
-    }
+      $.dispatcher.on('added-pending.posts-view', function(){
+        addWaypoints(this);
+      });
+      $.dispatcher.on('add-all.posts-view', function(){
+        addWaypoints(this);
+      });
+
+      var addWaypoints = function(self){
+        if(!self._twitterPosts)
+          return;
+        $.each(self._twitterPosts, function(index, post){
+          post.el.waypoint(function(){
+            window.twttr.widgets.createTweet(
+              post.templateData.Meta.id_str,
+              post.el.find('.post-content-full').get(0),
+              function(){
+                post.el.find('.post-core-content').remove();
+              },
+              { cards: 'all' }
+            );
+          },
+          {
+            triggerOnce: true,
+            offset: '120%',
+            context: self.el
+          });
+        });
+        self._twitterPosts = [];
+      }
+    }, function(err){
+      // twitterWidgets dependency failed to load, probably blocked by user
+      return;
+    });
 	}
 });
