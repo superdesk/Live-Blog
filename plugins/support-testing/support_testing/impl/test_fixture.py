5961deb1426330f4ebfa73ef3cc35c01016f45a2
Ioan v. Pocol <ioan.pocol@sourcefabric.org>
2014-04-17 09:47:17 +0300

    LB-1533 : Write REST API tests for embed
    Added embed tests on superdesk/rest_tests

--- plugins/support-testing/support_testing/impl/test_fixture.py
+++ plugins/support-testing/support_testing/impl/test_fixture.py
@@ -82,8 +82,11 @@ class TestFixtureService(ITestFixtureService, SessionSupport):
         ''' 
         
         #TODO - generate the truncate script:
-        # SELECT Concat('TRUNCATE TABLE ',table_schema,'.',TABLE_NAME, ';')  
-        # FROM INFORMATION_SCHEMA.TABLES where  table_schema = 'lb';
+        # SELECT Concat('DELETE FROM ', TABLE_NAME, ';')  
+        # FROM INFORMATION_SCHEMA.TABLES where table_schema = 'lb';
+        
+        # SELECT Concat('ALTER TABLE ', TABLE_NAME, ' AUTO_INCREMENT = 1;')  
+        # FROM INFORMATION_SCHEMA.TABLES where table_schema = 'lb';
         
         #TODO - generate the populate script:
         #mysqldump -uroot -proot --no-create-info --compact --add-locks=FALSE lb > mysql_add.sql
