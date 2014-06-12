71e3cb42827f2d48569ca5704f18cfaeebf2e39a
nistormihai <mihai.nistor@sourcefabric.org>
2014-06-12 12:45:11 +0300

    New theme 'brasil247' added
    Added needed pagination.

--- plugins/livedesk-embed/gui-themes/themes/brasil247.js
+++ plugins/livedesk-embed/gui-themes/themes/brasil247.js
@@ -2,6 +2,7 @@ define([
 	'plugins/wrappup-toggle',
 	'plugins/permanent-link',
 	'plugins/twitter-widgets',
+    'plugins/button-pagination',
 	'css!theme/liveblog',
 	'tmpl!theme/container',
 	'tmpl!theme/item/base'
@@ -10,7 +11,8 @@ define([
 		//enviroments: [ 'mobile', 'desktop', 'quirks' ],
 		plugins: [ 'wrappup-toggle', 
 					'permanent-link',
-					'twitter-widgets'
+					'twitter-widgets',
+                    'button-pagination'
 				]
 	}
 });
