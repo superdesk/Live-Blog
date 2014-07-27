1b074629b264b311508fac0a99f4a1744a257ab5
vladnicoara <vlad.nicoara@sourcefabric.org>
2014-07-27 17:15:19 +0300

    LB-1892 Multiple SEO configs

--- plugins/livedesk/gui-resources/scripts/js/seo-config.js
+++ plugins/livedesk/gui-resources/scripts/js/seo-config.js
@@ -96,12 +96,12 @@ define([
 			}
 		}]);
 
-		seoconf.controller('seoInterface', function($scope, $http, seoInterfaceData) {
+		seoconf.controller('seoInterface', function($scope, $http, seoInterfaceData, $q) {
 			$scope.cleanConfig = {
         		Id: 0,
         		SeoTheme: '1',
-        		CallbackActive: 'False',
-        		RefreshActive: 'False',
+        		CallbackActive: 'True',
+        		RefreshActive: 'True',
         		CallbackURL: '',
         		RefreshInterval: 60,
         		MaxPosts: 10,
@@ -115,6 +115,7 @@ define([
             //initializing some vars
             $scope.addSeoConfig = function() {
             	var config = $scope.completeConfigData($.extend(true,{},$scope.cleanConfig));
+            	$scope.RefreshActive = 'True';
             	$scope.configs.push(config);
             };
             $scope.getThemes = function() {
@@ -123,6 +124,7 @@ define([
 	            });
             };
             $scope.getSeoConfigs = function() {
+            	var deffered = $q.defer();
             	seoInterfaceData.getSeoConfigs(getGizmoUrl('my/LiveDesk/Blog/' + $scope.BlogId + '/Seo')).then(function(data) {
 	            	//init the configs var
 	            	
@@ -133,10 +135,14 @@ define([
 	            		config = $scope.completeConfigData(config);
 	            		//add the rest of the data
 	            		$scope.configs.push(config);
-
+	            		deffered.resolve(data);
 	            	} else {
+	            		var dl = data.length;
+	            		//done counter
+	            		var dc = 0;
 	            		for (var i = 0; i < data.length; i ++) {
 	            			seoInterfaceData.getSeoConfig(data[i].href).then(function(data){
+	            				
 	            				//create new config object
 	            				var config = {};
 	            				config.Id = data.Id;
@@ -152,12 +158,21 @@ define([
 				            	config = $scope.completeConfigData(config);
 				            	//add the rest of the data
 	            				$scope.configs.push(config);
+	            				dc ++;
+	            				if ( dc == dl ) {
+	            					//finished loading all the configs
+	            					deffered.resolve(data);
+	            				}
 	            			});
 	            		}
 	            	}
 	            });
+	            return deffered.promise;
             };
             $scope.init = function(BlogId) {
+            	//variable for the main HTML active button
+            	//initialy false until data is read
+            	$scope.htmlActive = false;
             	$scope.BlogId = BlogId;
             	//getting the available themes
             	//for new seo configs
@@ -168,8 +183,26 @@ define([
             			$scope.defaultConfig[data[i].Key] = data[i].Value;
             		}
             		$scope.cleanConfig = $.extend(true, $scope.cleanConfig, $scope.defaultConfig);
-            		$scope.getSeoConfigs();
+            		$scope.getSeoConfigs().then(function(){
+            			$scope.getMasterHtmlActive();
+            		});
             	});
+            };
+            $scope.getMasterHtmlActive = function() {
+            	var startValue = "True";
+            	for (var i = 0; i < $scope.configs.length; i ++) {
+            		if ( $scope.configs[i].RefreshActive == "False") {
+            			startValue = "False";
+            			break;
+            		}
+            	};
+            	$scope.htmlActive = startValue;
+            };
+            $scope.toggleHtmlActive = function() {
+            	for (var j = 0; j < $scope.configs.length; j ++) {
+            		$scope.configs[j].RefreshActive = $scope.htmlActive;
+            	}
+            	$scope.saveAllConfigs();
             };
             $scope.showRemove = function(i) {
             	return i ? true : false;
@@ -192,23 +225,24 @@ define([
             	config.extraInfoText = extraInfoText.toString();
             	config.HtmlWaitSwitch = false;
             	//on-off switches for data that may or may not be present
-            	config.HtmlURLSwitch = false;
+            	config.HtmlURLSwitch = true;
             	config.CallbackStatusSwitch = false;
             	//vars to keep the text info
-            	config.HtmlURLText = '';
+            	config.HtmlURLText = _('URL');
             	config.CallbackStatusText = '';
             	//general info on the html created
             	if (config.Id > 0 && config.HtmlURL && config.LastSync) {
                     config.HtmlWaitSwitch = false;
             		config.HtmlURLSwitch = true;
             		config.HtmlURL = relativeToAbsolute(config.HtmlURL);
-            		config.HtmlURLText =  _('SEO html');
+            		config.HtmlURLText =  _('URL');
             		//add timeinfo to text if it exists
             		if ( config.Id > 0 && config.LastSync ) {
             			var LastSync = new Date( config.LastSync );
-            			config.HtmlURLText += _(' generated on ');
-            			config.HtmlURLText += LastSync.format('yyyy-mm-dd HH:MM:ss');
             		}
+            	} else {
+            		config.HtmlURL = _('Please select a theme and press Generate HTML');
+            		config.HtmlURL = config.HtmlURL.toString();
             	}
             	config.HtmlURLText = config.HtmlURLText.toString();
             	//info on callback status if it exists
@@ -282,6 +316,9 @@ define([
             $scope.isLast = function(i) {
             	return $scope.configs.length - 1 == i;
             }
+            $scope.showNewHtml = function() {
+            	return $scope.configs.length < 4;
+            };
             $scope.saveAllConfigs = function() {
             	for (var i = 0; i < $scope.configs.length; i ++) {
             		$scope.save($scope.configs[i], i);
@@ -292,7 +329,7 @@ define([
             		BlogTheme: config.SeoTheme.toString(),
 	            	CallbackActive: config.CallbackActive,
 	            	CallbackURL: config.CallbackURL,
-	            	RefreshActive: config.RefreshActive,
+	            	RefreshActive: $scope.htmlActive,
 	            	RefreshInterval: config.RefreshInterval.toString(),
 	            	MaxPosts: config.MaxPosts.toString()	
             	}
@@ -313,7 +350,7 @@ define([
             };
             $scope.toggleSave = function(index) {
             	$scope.save($scope.configs[index], index);
-            }
+            };
         });
 	
 		seoconf.directive('seoToggler', function() {
@@ -328,6 +365,14 @@ define([
 					if ( scope.toggle == attrs['trueValue'] ) {
 						element.find('.sf-toggle-custom').addClass('sf-checked');
 					}
+					scope.$watch('toggle', function(){
+						//console.log('true vlue ',attrs['trueValue']);
+						if ( scope.toggle == attrs['trueValue'] ) {
+							element.find('.sf-toggle-custom').addClass('sf-checked');
+						} else {
+							element.find('.sf-toggle-custom').removeClass('sf-checked');
+						}
+					}, true);
 					element.find('.sf-toggle-custom').on('click', function(){
 						$(this).toggleClass('sf-checked');
 						if ( $(this).hasClass('sf-checked') ) {
