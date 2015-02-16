b15c03303a3da0da68234414982e100b0807572f
Nistor Mihai <Mihai.Nistor@sourcefabric.org>
2015-02-16 22:21:40 +0200

    Squashed 'plugins/embed/' changes from a88330b..b190e8f
    b190e8f Merge pull request #269 from nistormihai/master
    3c9b76c chore: refactored font-awesome location.
    
    git-subtree-dir: plugins/embed
    git-subtree-split: b190e8f97a94861a389ef07ed2e124fc57e13705

--- gui-themes/themes/aachener/desktop.js
+++ gui-themes/themes/aachener/desktop.js
@@ -6,7 +6,7 @@ require.config({
 });
 define([
     'css!theme/liveblog',
-    'css!themeBase/vendor/font-awesome/4.3.0/css/font-awesome.min',
+    'css!font-awesome',
     'plugins/button-pagination',
     'plugins/twitter-widgets',
     'plugins/post-hash',
