d75ee35bd35e3dd7c8427bfb06bf9b1770f312e0
Nistor Mihai <Mihai.Nistor@sourcefabric.org>
2015-09-14 23:14:28 +0300

    Squashed 'plugins/embed/' changes from 2ff217c..e551aaf
    e551aaf chore: replace zeit themes with the new ones.
    
    git-subtree-dir: plugins/embed
    git-subtree-split: e551aafba9d48dac9e709bc888c518c025a941f7

--- gui-themes/themes/zeit.js
+++ gui-themes/themes/zeit.js
@@ -1,9 +1,29 @@
-define(function() {
+define([
+    // 'plugins/scroll-pagination',
+    'plugins/button-pagination',
+    'plugins/twitter-widgets',
+    'plugins/post-hash',
+    'plugins/social-share',
+    'plugins/status',
+    'plugins/wrappup-toggle',
+    'plugins/predefined-types',
+    'css!theme/liveblog',
+    'tmpl!theme/container',
+    'tmpl!theme/item/base',
+    'tmpl!theme/plugins/social-share',
+    'tmpl!theme/item/predefined/scorecard'
+], function(dust, gt, moment) {
     'use strict';
+    liveblog.hashmark = '?';
+    liveblog.hashaddition = '#livedesk-root';
     return {
-        environments: {
-            'desktop': 'desktop',
-            'default': 'desktop'
-        }
+        plugins: [
+            // 'scroll-pagination',
+            'button-pagination',
+            'twitter-widgets',
+            'social-share',
+            'status',
+            'wrappup-toggle'
+        ]
     };
 });
